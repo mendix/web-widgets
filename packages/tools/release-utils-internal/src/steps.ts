beecafcebebc1691031c5ddf9ef3b2f32feb293c
Illia Obukhau <vyusfire@gmail.com>
2022-10-17 19:35:39 +0200

    refactor(release-utils-internal): add runSteps for module and widget

--- packages/tools/release-utils-internal/src/steps.ts
+++ packages/tools/release-utils-internal/src/steps.ts
@@ -1,8 +1,15 @@
 import { WidgetChangelogFileWrapper } from "./changelog-parser";
-import { PackageInfo } from "./package-info";
-import { exec, find, mkdir, cp } from "./shell";
+import { ModuleInfo, PackageInfo, WidgetInfo } from "./package-info";
+import { exec, find, mkdir, cp, rm } from "./shell";
 import { gh } from "./github";
 import { listPackages } from "./monorepo";
+import {
+    CommonBuildConfig,
+    getModuleConfigs,
+    getWidgetConfigs,
+    ModuleBuildConfig,
+    WidgetBuildConfig
+} from "./build-config";
 
 export async function updateChangelogsAndCreatePR(
     packageInfo: PackageInfo,
@@ -41,4 +48,79 @@ export async function copyMpkFiles(packageNames: string[], dest: string): Promis
     const paths = [...find(packages.map(p => `${p.path}/dist/${p.version}/*.mpk`))];
     mkdir("-p", dest);
     cp(paths, dest);
+}
+
+type CommonStepParams = {
+    info: PackageInfo;
+    config: CommonBuildConfig;
+};
+
+type ModuleStepParams = {
+    info: ModuleInfo;
+    config: ModuleBuildConfig;
+};
+
+export async function removeDist({ config }: CommonStepParams): Promise<void> {
+    console.info("Remove dist");
+    rm("-rf", config.paths.dist);
+}
+
+export async function copyThemesourceToProject({ config }: ModuleStepParams): Promise<void> {
+    console.info("Remove module themesource in targetProject");
+    rm("-rf", config.output.dirs.themesource);
+    console.info("Copy module themesource to targetProject");
+    mkdir("-p", config.output.dirs.themesource);
+    cp("-R", `${config.paths.themesource}/*`, config.output.dirs.themesource);
+}
+
+export async function copyWidgetsToProject({ config }: ModuleStepParams): Promise<void> {
+    console.info("Copy module widgets to targetProject");
+    await copyMpkFiles(config.dependencies, config.output.dirs.widgets);
+}
+
+type Step<Info, Config> = (params: { info: Info; config: Config }) => Promise<void>;
+
+export async function runSteps<Info, Config>(params: {
+    packageInfo: Info;
+    config: Config;
+    steps: Array<Step<Info, Config>>;
+}): Promise<void> {
+    for (const step of params.steps) {
+        await step({ info: params.packageInfo, config: params.config });
+    }
+}
+
+type RunWidgetStepsParams = {
+    dependencies: string[];
+    packagePath: string;
+    steps: Array<Step<WidgetInfo, WidgetBuildConfig>>;
+};
+
+export async function runWidgetSteps(params: RunWidgetStepsParams): Promise<void> {
+    const [packageInfo, config] = await getWidgetConfigs(params);
+
+    await runSteps({
+        packageInfo,
+        config,
+        steps: params.steps
+    });
+}
+
+type RunModuleStepsParams = {
+    dependencies: string[];
+    packagePath: string;
+    steps: Array<Step<ModuleInfo, ModuleBuildConfig>>;
+};
+
+export async function runModuleSteps(params: RunModuleStepsParams): Promise<void> {
+    const [packageInfo, config] = await getModuleConfigs(params);
+
+    // console.dir(packageInfo, { depth: 10 });
+    // console.dir(config, { depth: 10 });
+
+    await runSteps({
+        packageInfo,
+        config,
+        steps: params.steps
+    });
 }
