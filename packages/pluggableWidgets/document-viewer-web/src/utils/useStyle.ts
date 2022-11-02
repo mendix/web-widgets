f4c9564726ad7b99bd437dfb75d7bef3df7a9504
Illia Obukhau <vyusfire@gmail.com>
2022-11-02 12:33:33 +0100

    refactor: remove document-viewer-web

--- packages/pluggableWidgets/document-viewer-web/src/utils/useStyle.ts
+++
@@ -1,9 +0,0 @@
-import { CSSProperties, useMemo } from "react";
-
-import { Dimensions, getDimensions } from "@mendix/pluggable-widgets-commons";
-
-export function useStyle(dimensions: Dimensions): CSSProperties {
-    return useMemo(() => {
-        return getDimensions(dimensions);
-    }, [dimensions]);
-}
