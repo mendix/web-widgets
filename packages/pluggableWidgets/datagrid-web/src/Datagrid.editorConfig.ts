c12f2a062b9c571dc1e5cb86576116522c70e510
Roman Vyakhirev <roman.vyakhirev@mendix.com>
2022-11-30 12:54:00 +0100

    chore(datagrid-web): use container helper in structure preview

--- packages/pluggableWidgets/datagrid-web/src/Datagrid.editorConfig.ts
+++ packages/pluggableWidgets/datagrid-web/src/Datagrid.editorConfig.ts
@@ -1,5 +1,5 @@
 import {
-    ContainerProps,
+    container,
     DropZoneProps,
     RowLayoutProps,
     StructurePreviewProps,
@@ -158,37 +158,32 @@ export const getPreview = (values: DatagridPreviewProps, isDarkMode: boolean): S
     const columns: RowLayoutProps = {
         type: "RowLayout",
         columnSize: "fixed",
-        children: columnProps.map(
-            column =>
-                ({
-                    type: "Container",
-                    borders: true,
-                    grow: column.width === "manual" && column.size ? column.size : 1,
-                    backgroundColor:
-                        values.columnsHidable && column.hidable === "hidden"
-                            ? isDarkMode
-                                ? "#3E3E3E"
-                                : "#F5F5F5"
-                            : undefined,
-                    children: [
-                        column.showContentAs === "customContent"
-                            ? {
-                                  type: "DropZone",
-                                  property: column.content
-                              }
-                            : {
-                                  type: "Container",
-                                  padding: 8,
-                                  children: [
-                                      text({ fontSize: 10 })(
-                                          column.showContentAs === "dynamicText"
-                                              ? column.dynamicText ?? "Dynamic text"
-                                              : `[${column.attribute ? column.attribute : "No attribute selected"}]`
-                                      )
-                                  ]
-                              }
-                    ]
-                } as ContainerProps)
+        children: columnProps.map(column =>
+            container({
+                borders: true,
+                grow: column.width === "manual" && column.size ? column.size : 1,
+                backgroundColor:
+                    values.columnsHidable && column.hidable === "hidden"
+                        ? isDarkMode
+                            ? "#3E3E3E"
+                            : "#F5F5F5"
+                        : undefined
+            })(
+                column.showContentAs === "customContent"
+                    ? {
+                          type: "DropZone",
+                          property: column.content
+                      }
+                    : container({
+                          padding: 8
+                      })(
+                          text({ fontSize: 10 })(
+                              column.showContentAs === "dynamicText"
+                                  ? column.dynamicText ?? "Dynamic text"
+                                  : `[${column.attribute ? column.attribute : "No attribute selected"}]`
+                          )
+                      )
+            )
         )
     };
     const titleHeader: RowLayoutProps = {
@@ -198,11 +193,9 @@ export const getPreview = (values: DatagridPreviewProps, isDarkMode: boolean): S
         borders: true,
         borderWidth: 1,
         children: [
-            {
-                type: "Container",
-                padding: 4,
-                children: [text({ fontColor: isDarkMode ? "#6DB1FE" : "#2074C8" })("Data grid 2")]
-            }
+            container({
+                padding: 4
+            })(text({ fontColor: isDarkMode ? "#6DB1FE" : "#2074C8" })("Data grid 2"))
         ]
     };
     const headerFilters = {
@@ -222,8 +215,7 @@ export const getPreview = (values: DatagridPreviewProps, isDarkMode: boolean): S
         columnSize: "fixed",
         children: columnProps.map(column => {
             const isColumnHidden = values.columnsHidable && column.hidable === "hidden";
-            const content: ContainerProps = {
-                type: "Container",
+            const content = container({
                 borders: true,
                 grow:
                     values.columns.length > 0
@@ -237,47 +229,41 @@ export const getPreview = (values: DatagridPreviewProps, isDarkMode: boolean): S
                         : "#DCDCDC"
                     : isDarkMode
                     ? "#3E3E3E"
-                    : "#F5F5F5",
-                children: [
-                    {
-                        type: "Container",
-                        padding: 8,
-                        children: [
-                            text({
-                                bold: true,
-                                fontSize: 10,
-                                fontColor: column.header
-                                    ? undefined
-                                    : isColumnHidden
-                                    ? isDarkMode
-                                        ? "#4F4F4F"
-                                        : "#DCDCDC"
-                                    : isDarkMode
-                                    ? "#3E3E3E"
-                                    : "#F5F5F5"
-                            })(column.header ? column.header : "Header")
-                        ]
-                    },
-                    ...(hasColumns && values.columnsFilterable
-                        ? [
-                              {
-                                  type: "DropZone",
-                                  property: column.filter,
-                                  placeholder: "Place filter widget here"
-                              } as DropZoneProps
-                          ]
-                        : [])
-                ]
-            };
+                    : "#F5F5F5"
+            })(
+                container({
+                    padding: 8
+                })(
+                    text({
+                        bold: true,
+                        fontSize: 10,
+                        fontColor: column.header
+                            ? undefined
+                            : isColumnHidden
+                            ? isDarkMode
+                                ? "#4F4F4F"
+                                : "#DCDCDC"
+                            : isDarkMode
+                            ? "#3E3E3E"
+                            : "#F5F5F5"
+                    })(column.header ? column.header : "Header")
+                ),
+                ...(hasColumns && values.columnsFilterable
+                    ? [
+                          {
+                              type: "DropZone",
+                              property: column.filter,
+                              placeholder: "Place filter widget here"
+                          } as DropZoneProps
+                      ]
+                    : [])
+            );
             return values.columns.length > 0
                 ? {
                       type: "Selectable",
                       object: column,
                       grow: column.width === "manual" && column.size ? column.size : 1,
-                      child: {
-                          type: "Container",
-                          children: [content]
-                      }
+                      child: container()(content)
                   }
                 : content;
         })
@@ -299,16 +285,13 @@ export const getPreview = (values: DatagridPreviewProps, isDarkMode: boolean): S
                   } as RowLayoutProps
               ]
             : [];
-    return {
-        type: "Container",
-        children: [
-            titleHeader,
-            ...(values.showHeaderFilters && values.filterList.length > 0 ? [headerFilters] : []),
-            headers,
-            ...Array.from({ length: 5 }).map(() => columns),
-            ...footer
-        ]
-    };
+    return container()(
+        titleHeader,
+        ...(values.showHeaderFilters && values.filterList.length > 0 ? [headerFilters] : []),
+        headers,
+        ...Array.from({ length: 5 }).map(() => columns),
+        ...footer
+    );
 };
 
 export function check(values: DatagridPreviewProps): Problem[] {
