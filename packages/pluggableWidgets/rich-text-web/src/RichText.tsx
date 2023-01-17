428ee04187ea1904f5003f52d65b521b5547b15c
Leonardo de Souza <leonardo.de.souza@mendix.com>
2023-01-17 16:53:07 +0100

    fix(rich-text-web): add missing plugins for the indent feature

--- packages/pluggableWidgets/rich-text-web/src/RichText.tsx
+++ packages/pluggableWidgets/rich-text-web/src/RichText.tsx
@@ -40,7 +40,7 @@ export default function RichText(props: RichTextContainerProps): ReactNode {
             return createCustomToolbar(groupItems, toolbarConfig === "basic");
         }
     };
-    const plugins = ["openlink"];
+    const plugins = ["openlink", "indent", "indentlist"];
     if (props.codeHighlight) {
         plugins.push("codesnippet");
     }
