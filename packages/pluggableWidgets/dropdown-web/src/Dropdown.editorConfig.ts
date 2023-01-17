7f692e736620b6663659d1121c884eb122931467
Rahman <rahmanunver@gmail.com>
2023-01-17 16:41:01 +0100

    feat(dropdown-web): initial build

---
+++ packages/pluggableWidgets/dropdown-web/src/Dropdown.editorConfig.ts
@@ -0,0 +1,32 @@
+import { StructurePreviewProps } from "@mendix/pluggable-widgets-commons";
+import { BadgePreviewProps } from "../typings/BadgeProps";
+
+export function getPreview(values: BadgePreviewProps, isDarkMode: boolean): StructurePreviewProps {
+    return {
+        type: "RowLayout",
+        columnSize: "grow",
+        children: [
+            {
+                type: "Container",
+                children: [
+                    {
+                        type: "Container",
+                        children: [
+                            {
+                                type: "Text",
+                                content: values.value,
+                                fontColor: "#FFF",
+                                bold: true,
+                                fontSize: 8
+                            }
+                        ],
+                        padding: values.value ? 8 : 18
+                    }
+                ],
+                backgroundColor: isDarkMode ? "#579BF9" : "#264AE5",
+                borderRadius: values.type === "badge" ? 22 : 8
+            },
+            { type: "Container", grow: 2 }
+        ]
+    };
+}
