ca5ebb93dc1d8a50ad4c558bb44f58875e75898e
Rahman <rahmanunver@gmail.com>
2023-01-17 16:41:01 +0100

    feat(dropdown-web): initial build

---
+++ packages/pluggableWidgets/dropdown-web/src/Dropdown.editorPreview.tsx
@@ -0,0 +1,12 @@
+// import { parseStyle } from "@mendix/pluggable-widgets-commons";
+import { createElement, ReactElement } from "react";
+
+import Dropdown from "./Dropdown";
+
+export const preview = (): ReactElement => {
+    // TODO: Change PIW preview props typing (class -> className) generation to remove the ts-ignore below
+    // @ts-ignore
+    // const { className, style, type, value } = props;
+
+    return <Dropdown />;
+};
