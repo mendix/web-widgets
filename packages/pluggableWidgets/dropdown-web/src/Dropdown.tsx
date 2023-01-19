96df78079d867bbeddcdc2ea46f33d08a649ee1e
Rahman <rahmanunver@gmail.com>
2023-01-19 16:58:38 +0100

    feat(dropdown-web): basic dropdown, autocomplete, no styling

--- packages/pluggableWidgets/dropdown-web/src/Dropdown.tsx
+++ packages/pluggableWidgets/dropdown-web/src/Dropdown.tsx
@@ -1,6 +1,55 @@
 import { ReactNode, createElement } from "react";
+import Downshift from "downshift";
 // import { executeAction } from "@mendix/pluggable-widgets-commons";
 
 export default function Dropdown(): ReactNode {
-    return <div>YOLO</div>;
+    const items = [{ value: "apple" }, { value: "pear" }, { value: "orange" }, { value: "grape" }, { value: "banana" }];
+
+    return (
+        <div>
+            <Downshift itemToString={item => (item ? item.value : "")}>
+                {({
+                    getInputProps,
+                    getItemProps,
+                    getLabelProps,
+                    getMenuProps,
+                    isOpen,
+                    inputValue,
+                    highlightedIndex,
+                    selectedItem,
+                    getRootProps
+                }) => (
+                    <div>
+                        <label {...getLabelProps()}>Enter a fruit</label>
+                        <div style={{ display: "inline-block" }} {...getRootProps({}, { suppressRefError: true })}>
+                            <input {...getInputProps()} />
+                        </div>
+                        <ul {...getMenuProps()}>
+                            {isOpen
+                                ? items
+                                      .filter(item => !inputValue || item.value.includes(inputValue))
+                                      .map((item, index) => (
+                                          // eslint-disable-next-line react/jsx-key
+                                          <li
+                                              {...getItemProps({
+                                                  key: item.value,
+                                                  index,
+                                                  item,
+                                                  style: {
+                                                      backgroundColor:
+                                                          highlightedIndex === index ? "lightgray" : "white",
+                                                      fontWeight: selectedItem === item ? "bold" : "normal"
+                                                  }
+                                              })}
+                                          >
+                                              {item.value}
+                                          </li>
+                                      ))
+                                : null}
+                        </ul>
+                    </div>
+                )}
+            </Downshift>
+        </div>
+    );
 }
