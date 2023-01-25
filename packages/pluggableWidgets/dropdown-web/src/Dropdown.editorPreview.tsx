abec0271db6822d3eb2fbf2f27bd228f29a3cb89
Rahman <rahmanunver@gmail.com>
2023-01-25 18:11:34 +0100

    feat(dropdown-web): cleanup

--- packages/pluggableWidgets/dropdown-web/src/Dropdown.editorPreview.tsx
+++ packages/pluggableWidgets/dropdown-web/src/Dropdown.editorPreview.tsx
@@ -6,7 +6,5 @@ import Dropdown from "./Dropdown";
 export const preview = (): ReactElement => {
     // TODO: Change PIW preview props typing (class -> className) generation to remove the ts-ignore below
     // @ts-ignore
-    // const { className, style, type, value } = props;
-
     return <Dropdown />;
 };
