6a0f820893d9ba1c7c166b13978ccca5074dbf7a
Illia Obukhau <vyusfire@gmail.com>
2023-01-25 11:23:23 +0100

    feat(datagrid-dropdown-filter-web): use "or" statement in filter condition to match enum behavior

--- packages/shared/pluggable-widgets-commons/src/builders/ConditionUtils.ts
+++ packages/shared/pluggable-widgets-commons/src/builders/ConditionUtils.ts
@@ -1,6 +1,6 @@
 import { ListReferenceSetValue, ListReferenceValue, ObjectItem } from "mendix";
-import { ContainsCondition, EqualsCondition } from "mendix/filters";
-import { association, literal, equals, contains, empty } from "mendix/filters/builders";
+import { ContainsCondition, EqualsCondition, FilterCondition } from "mendix/filters";
+import { association, literal, equals, contains, empty, or } from "mendix/filters/builders";
 
 export function referenceEqualsCondition(associationValue: ListReferenceValue, value: ObjectItem): EqualsCondition {
     return equals(association(associationValue.id), literal(value));
@@ -12,4 +12,24 @@ export function referenceSetContainsCondition(
 ): ContainsCondition {
     const v = value.length ? literal(value.slice()) : empty();
     return contains(association(associationValue.id), v);
+}
+
+export function referenceEqualsOneOf(association: ListReferenceValue, values: ObjectItem[]): FilterCondition {
+    const expressions = values.map(value => referenceEqualsCondition(association, value));
+
+    if (expressions.length > 1) {
+        return or(...expressions);
+    }
+
+    return expressions[0];
+}
+
+export function referenceSetContainsOneOf(association: ListReferenceSetValue, values: ObjectItem[]): FilterCondition {
+    const expressions = values.map(value => referenceSetContainsCondition(association, [value]));
+
+    if (expressions.length > 1) {
+        return or(...expressions);
+    }
+
+    return expressions[0];
 }
