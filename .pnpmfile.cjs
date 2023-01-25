272f2141fae97ad55e44c9ddd011f7660edc993b
Illia Obukhau <vyusfire@gmail.com>
2023-01-25 14:02:25 +0100

    feat: enable strict-peer-dependencies

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -3,6 +3,11 @@ function readPackage(pkg, context) {
         delete pkg.dependencies["react-native"];
     }
 
+    if (pkg && pkg.name === "react-big-calendar") {
+        pkg.peerDependencies["react"] = "^15.3.0 || ^16.0.0 || ^17.0.0";
+        pkg.peerDependencies["react-dom"] = "^15.3.0 || ^16.0.0 || ^17.0.0";
+    }
+
     return pkg;
 }
 
