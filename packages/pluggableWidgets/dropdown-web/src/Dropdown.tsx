e01305e86adb8b6a72b73c3db27828a93b126a7c
Roman Vyakhirev <roman.vyakhirev@mendix.com>
2023-02-15 10:59:03 +0100

    tmp

--- packages/pluggableWidgets/dropdown-web/src/Dropdown.tsx
+++ packages/pluggableWidgets/dropdown-web/src/Dropdown.tsx
@@ -1,46 +1,155 @@
-import { ReactNode, createElement } from "react";
+import { createElement, ReactNode, useRef } from "react";
 import Downshift from "downshift";
 import { DropdownContainerProps } from "../typings/DropdownProps";
-import { matchSorter } from "match-sorter";
+import { AssociationSingleSelector } from "./helpers/Association/AssociationSingleSelector";
+import { EnumBooleanSingleSelector } from "./helpers/EnumBool/EnumBoolSingleSelector";
+import { SingleSelector } from "./helpers/types";
 // import { executeAction } from "@mendix/pluggable-widgets-commons";
 
-export default function Dropdown(props: DropdownContainerProps): ReactNode {
-    console.log("props", props.dataSource);
-    const items = [{ value: "apple" }, { value: "pear" }, { value: "orange" }, { value: "grape" }, { value: "banana" }];
+// attribute options
+// enum options - needs no other properties, value and caption are from universe
+// custom options
+
+function getSelector(props: DropdownContainerProps): SingleSelector {
+    if (props.optionsSourceType === "enumerationOrBoolean") {
+        return new EnumBooleanSingleSelector();
+    } else if (props.optionsSourceType === "association") {
+        return new AssociationSingleSelector();
+    } else {
+        throw new Error(`'optionsSourceType' of type '${props.optionsSourceType}' is not supported`);
+    }
+}
+
+export default function Dropdown(props: DropdownContainerProps): ReactNode | null {
+    const selectorRef = useRef<SingleSelector | undefined>(undefined);
+    const inputRef = useRef<HTMLInputElement>(null);
+    if (!selectorRef.current) {
+        selectorRef.current = getSelector(props);
+    }
+    const selector = selectorRef.current!;
+    selector.updateProps(props);
+    if (selector.status === "unavailable") {
+        // show some placeholder, no data at all
+        return (
+            <div className="widget-dropdown" style={{ flexGrow: 1 }}>
+                <div className="form-control" style={{ backgroundColor: "transparent" }}>
+                    &nbsp;
+                </div>
+            </div>
+        );
+    }
+
+    console.log("props", props);
+    console.log("provider", selector);
+
     return (
-        <Downshift itemToString={item => (item ? item.value : "")}>
+        <Downshift
+            itemToString={v => selector.caption.get(v)}
+            onChange={v => selector.setValue(v)}
+            onInputValueChange={v => selector.options.setSearchTerm(v)}
+            onStateChange={state => {
+                if (state.type === Downshift.stateChangeTypes.clickButton && state.isOpen) {
+                    inputRef.current?.focus();
+                }
+            }}
+            selectedItem={selector.currentValue}
+            stateReducer={(_state, changes) => {
+                switch (changes.type) {
+                    case Downshift.stateChangeTypes.clickButton:
+                        return {
+                            ...changes,
+                            // highlightedIndex: state.highlightedIndex,
+                            // isOpen: true,
+                            inputValue: ""
+                        };
+                    case Downshift.stateChangeTypes.keyDownEnter:
+                    case Downshift.stateChangeTypes.clickItem:
+                        return {
+                            ...changes,
+                            // highlightedIndex: state.highlightedIndex,
+                            // isOpen: true,
+                            inputValue: ""
+                        };
+
+                    default:
+                        return changes;
+                }
+            }}
+        >
             {({
+                selectItem,
                 getInputProps,
                 getItemProps,
-                getLabelProps,
                 getMenuProps,
                 isOpen,
-                inputValue,
+                // inputValue,
                 highlightedIndex,
                 selectedItem,
-                getRootProps
+                getRootProps,
+                getToggleButtonProps
             }) => (
-                <div>
-                    <label {...getLabelProps()}>Enter a fruit {props.customName}</label>
-                    <div style={{ display: "inline-block" }} {...getRootProps({}, { suppressRefError: true })}>
-                        <input {...getInputProps()} />
+                <div
+                    className="widget-dropdown"
+                    style={{ flexGrow: 1 }}
+                    {...getRootProps({}, { suppressRefError: true })}
+                >
+                    <div
+                        style={{ display: "flex", border: isOpen ? "1px solid #264ae5" : undefined }}
+                        className="form-control"
+                        {...getToggleButtonProps()}
+                    >
+                        <input
+                            ref={inputRef}
+                            {...getInputProps()}
+                            placeholder={selector.caption.get(selector.currentValue)}
+                            style={{
+                                border: "none"
+                            }}
+                        />
+
+                        <div
+                            style={{
+                                flexGrow: 1,
+                                width: !isOpen ? undefined : "2px",
+                                opacity: selector.currentValue === null ? "0.7" : undefined
+                            }}
+                        >
+                            {selector.caption.get(selector.currentValue)}
+                        </div>
+
+                        {selector.clearable && selector.currentValue !== null && (
+                            <button
+                                style={{
+                                    border: "none",
+                                    background: "transparent"
+                                }}
+                                onClick={e => {
+                                    e.preventDefault();
+                                    e.stopPropagation();
+                                    selectItem(null);
+                                }}
+                            >
+                                ❌
+                            </button>
+                        )}
+                        <div>⬇</div>
                     </div>
-                    <ul {...getMenuProps()}>
+
+                    <ul {...getMenuProps()} style={{ display: !isOpen ? "none" : undefined }}>
                         {isOpen
-                            ? matchSorter(items, inputValue || "", { keys: ["value"] }).map((item, index) => (
-                                  // eslint-disable-next-line react/jsx-key
+                            ? selector.options.getAll().map((item, index) => (
                                   <li
                                       {...getItemProps({
-                                          key: item.value,
+                                          key: JSON.stringify(item),
                                           index,
-                                          item,
+                                          item: item,
                                           style: {
                                               backgroundColor: highlightedIndex === index ? "lightgray" : "white",
                                               fontWeight: selectedItem === item ? "bold" : "normal"
                                           }
                                       })}
                                   >
-                                      {item.value}
+                                      {selector.caption.render(item)}
                                   </li>
                               ))
                             : null}
--- packages/pluggableWidgets/dropdown-web/typings/DropdownProps.d.ts
+++ packages/pluggableWidgets/dropdown-web/typings/DropdownProps.d.ts
@@ -3,36 +3,28 @@
  * WARNING: All changes made to this file will be overwritten
  * @author Mendix Widgets Framework Team
  */
-import { ComponentType, CSSProperties, ReactNode } from "react";
-import { ActionValue, DynamicValue, ListValue, ReferenceValue, ReferenceSetValue } from "mendix";
+import { ActionValue, DynamicValue, EditableValue, ListValue, ListAttributeValue, ListExpressionValue, ReferenceValue, ReferenceSetValue } from "mendix";
 
-export type TypeaheadEnum = "typeaheadNo" | "typeaheadStartWith" | "typeaheadContains";
+export type OptionsSourceTypeEnum = "enumerationOrBoolean" | "association" | "custom";
 
-export type TypeEnum = "typeEnumeration" | "typeAssociation" | "typeCustom";
+export type OptionsSourceAssociationCaptionTypeEnum = "attribute" | "expression" | "custom";
 
-export type CustomCaptionEnum = "captionEnumeration" | "captionAssociation" | "captionCustom";
-
-export type OptionCaptionEnum = "textTemplate" | "custom";
+export type TypeaheadEnum = "no" | "startsWith" | "contains";
 
 export interface DropdownContainerProps {
     name: string;
-    class: string;
-    style?: CSSProperties;
     tabIndex?: number;
-    captionText: DynamicValue<string>;
+    id: string;
+    optionsSourceType: OptionsSourceTypeEnum;
+    attributeEnumerationOrBoolean?: EditableValue<string | boolean>;
+    attributeAssociation?: ReferenceValue | ReferenceSetValue;
+    optionsSourceAssociationDataSource?: ListValue;
+    optionsSourceAssociationCaptionType: OptionsSourceAssociationCaptionTypeEnum;
     emptyOptionText: DynamicValue<string>;
+    optionsSourceAssociationCaptionAttribute?: ListAttributeValue<string>;
+    optionsSourceAssociationCaptionExpression?: ListExpressionValue<string>;
     typeahead: TypeaheadEnum;
     clearable: boolean;
-    showLabel: boolean;
-    labelCaption: DynamicValue<string>;
-    type: TypeEnum;
-    customAttribute?: ReferenceValue | ReferenceSetValue;
-    customCaption: CustomCaptionEnum;
-    dataSource?: ListValue;
-    customName: string;
-    optionCaption: OptionCaptionEnum;
-    optionTextTemplate: DynamicValue<string>;
-    optionCustom?: ReactNode;
     onClickEvent?: ActionValue;
     onEnterEvent?: ActionValue;
     onLeaveEvent?: ActionValue;
@@ -40,28 +32,17 @@ export interface DropdownContainerProps {
 }
 
 export interface DropdownPreviewProps {
-    /**
-     * @deprecated Deprecated since version 9.18.0. Please use class property instead.
-     */
-    className: string;
-    class: string;
-    style: string;
-    styleObject?: CSSProperties;
     readOnly: boolean;
-    captionText: string;
+    optionsSourceType: OptionsSourceTypeEnum;
+    attributeEnumerationOrBoolean: string;
+    attributeAssociation: string;
+    optionsSourceAssociationDataSource: {} | { type: string } | null;
+    optionsSourceAssociationCaptionType: OptionsSourceAssociationCaptionTypeEnum;
     emptyOptionText: string;
+    optionsSourceAssociationCaptionAttribute: string;
+    optionsSourceAssociationCaptionExpression: string;
     typeahead: TypeaheadEnum;
     clearable: boolean;
-    showLabel: boolean;
-    labelCaption: string;
-    type: TypeEnum;
-    customAttribute: string;
-    customCaption: CustomCaptionEnum;
-    dataSource: {} | { type: string } | null;
-    customName: string;
-    optionCaption: OptionCaptionEnum;
-    optionTextTemplate: string;
-    optionCustom: { widgetCount: number; renderer: ComponentType<{ caption?: string }> };
     onClickEvent: {} | null;
     onEnterEvent: {} | null;
     onLeaveEvent: {} | null;
