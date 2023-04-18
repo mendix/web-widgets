af276756c81471c96f6fb1dfb82f322865fd282f
Illia Obukhau <vyusfire@gmail.com>
2023-04-18 17:06:14 +0200

    test(datagrid-text-filter-web): update unit tests

--- packages/pluggableWidgets/datagrid-text-filter-web/src/components/__tests__/FilterComponent.spec.tsx
+++ packages/pluggableWidgets/datagrid-text-filter-web/src/components/__tests__/FilterComponent.spec.tsx
@@ -1,4 +1,4 @@
-import { render, shallow } from "enzyme";
+import { render, mount } from "enzyme";
 import { createElement } from "react";
 import { FilterComponent } from "../FilterComponent";
 
@@ -35,7 +35,7 @@ describe("Filter component", () => {
 
     it("calls updateFilters when value changes", () => {
         const updateFiltersHandler = jest.fn();
-        const component = shallow(
+        const component = mount(
             <FilterComponent
                 adjustable
                 initialFilterType="contains"
@@ -46,13 +46,14 @@ describe("Filter component", () => {
 
         const input = component.find("input");
         input.simulate("change", { target: { value: "test" } });
-
-        expect(updateFiltersHandler).toBeCalled();
+        expect(updateFiltersHandler).toBeCalledTimes(0);
+        jest.advanceTimersByTime(500);
+        expect(updateFiltersHandler).toBeCalledTimes(1);
     });
 
     it("debounces calls for updateFilters when value changes", () => {
         const updateFiltersHandler = jest.fn();
-        const component = shallow(
+        const component = mount(
             <FilterComponent
                 adjustable
                 initialFilterType="contains"
@@ -61,21 +62,23 @@ describe("Filter component", () => {
             />
         );
 
-        // Initial call with default filter
-        expect(updateFiltersHandler).toBeCalledTimes(1);
+        expect(updateFiltersHandler).toBeCalledTimes(0);
 
         const input = component.find("input");
         input.simulate("change", { target: { value: "test" } });
         jest.advanceTimersByTime(499);
+        expect(updateFiltersHandler).toBeCalledTimes(0);
+
         input.simulate("change", { target: { value: "test2" } });
         input.simulate("change", { target: { value: "test3" } });
         jest.advanceTimersByTime(500);
-
-        expect(updateFiltersHandler).toBeCalledTimes(2);
+        expect(updateFiltersHandler).toBeCalledTimes(1);
+        expect(updateFiltersHandler).toHaveBeenCalledWith("test3", "contains");
 
         input.simulate("change", { target: { value: "test" } });
         jest.advanceTimersByTime(500);
 
-        expect(updateFiltersHandler).toBeCalledTimes(3);
+        expect(updateFiltersHandler).toBeCalledTimes(2);
+        expect(updateFiltersHandler).toHaveBeenCalledWith("test", "contains");
     });
 });
