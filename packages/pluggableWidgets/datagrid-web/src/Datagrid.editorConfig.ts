eb86975f4a3c29edc2fdd151670ba1b77c591517
Roman Vyakhirev <roman.vyakhirev@mendix.com>
2023-04-26 16:12:12 +0200

    feat(datagrid-web): better names in structure preview

--- packages/pluggableWidgets/datagrid-web/src/Datagrid.editorConfig.ts
+++ packages/pluggableWidgets/datagrid-web/src/Datagrid.editorConfig.ts
@@ -210,7 +210,7 @@ export const getPreview = (
             )
         )
     );
-    const titleHeader = rowLayout({
+    const gridTitle = rowLayout({
         columnSize: "fixed",
         backgroundColor: modeColor("#3B5C8F", "#DAEFFB"),
         borders: true,
@@ -220,7 +220,7 @@ export const getPreview = (
             padding: 4
         })(text({ fontColor: modeColor("#6DB1FE", "#2074C8") })("Data grid 2"))
     );
-    const headerFilters = rowLayout({
+    const gridHeaderWidgets = rowLayout({
         columnSize: "fixed",
         borders: true
     })(
@@ -230,7 +230,7 @@ export const getPreview = (
         )(values.filtersPlaceholder)
     );
 
-    const headers = rowLayout({
+    const columnHeaders = rowLayout({
         columnSize: "fixed"
     })(
         ...columnProps.map(column => {
@@ -274,7 +274,7 @@ export const getPreview = (
                 : content;
         })
     );
-    const footer =
+    const customEmptyMessageWidgets =
         values.showEmptyPlaceholder === "custom"
             ? [
                   rowLayout({
@@ -290,12 +290,12 @@ export const getPreview = (
             : [];
 
     return container()(
-        titleHeader,
+        gridTitle,
         ...(canHideDataSourceHeader ? [datasource(values.datasource)()] : []),
-        headerFilters,
-        headers,
+        gridHeaderWidgets,
+        columnHeaders,
         ...Array.from({ length: 5 }).map(() => columns),
-        ...footer
+        ...customEmptyMessageWidgets
     );
 };
 
