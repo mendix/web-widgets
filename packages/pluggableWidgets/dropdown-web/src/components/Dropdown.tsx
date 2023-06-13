c9102817bd7520f9c01c56f295bd5bbdc2a6fcb3
Rahman <rahmanunver@gmail.com>
2023-06-13 17:50:43 +0200

    fix(dropdown-web): fix placeholder appearing gray

--- packages/pluggableWidgets/dropdown-web/src/components/Dropdown.tsx
+++ packages/pluggableWidgets/dropdown-web/src/components/Dropdown.tsx
@@ -16,7 +16,6 @@ export function Dropdown(props: DropdownContainerProps): ReactElement {
     const downshiftProps = useDownshiftProps(selector, inputRef.current, props.emptyOptionText?.value);
     const actionEvents = useActionEvents(props);
     const readOnly = props.attributeEnumerationOrBoolean?.readOnly ?? props.attributeAssociation?.readOnly;
-    console.log("PROPS", downshiftProps);
 
     if (selector.status === "unavailable") {
         return <Placeholder />;
