3144c6c64bedb7c2d9a72b0f1cb6c0acd9bd96d3
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2023-06-19 14:36:16 +0200

    test(data-widgets): fix imports in unit tests

--- packages/pluggableWidgets/datagrid-number-filter-web/src/features/__tests__/initialize.spec.ts
+++ packages/pluggableWidgets/datagrid-number-filter-web/src/features/__tests__/initialize.spec.ts
@@ -1,4 +1,4 @@
-import { renderHook } from "@testing-library/react-hooks";
+import { renderHook } from "@testing-library/react";
 import { useSetInitialConditionEffect } from "../initialize";
 import { Big } from "big.js";
 
