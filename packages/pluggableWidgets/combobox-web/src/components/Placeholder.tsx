c22d66139fc60e09e4d43605059c15ab202a3c6d
Rahman <rahmanunver@gmail.com>
2023-06-20 17:27:11 +0200

    fix(combobox-web): fixes disabled state in case of unavailable selector

--- packages/pluggableWidgets/combobox-web/src/components/Placeholder.tsx
+++ packages/pluggableWidgets/combobox-web/src/components/Placeholder.tsx
@@ -1,9 +1,15 @@
 import { createElement, ReactElement } from "react";
+import { DownArrow } from "../assets/icons";
 
 export function Placeholder(): ReactElement {
     return (
         <div className="widget-combobox">
-            <div className="widget-combobox-placeholder form-control">&nbsp;</div>
+            <div className="form-control widget-combobox-placeholder">
+                &nbsp;
+                <div className="widget-combobox-placeholder-down-arrow">
+                    <DownArrow />
+                </div>
+            </div>
         </div>
     );
 }
