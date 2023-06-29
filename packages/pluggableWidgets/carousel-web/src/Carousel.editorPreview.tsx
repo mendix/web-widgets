d0fe32fd7681a72e7c24b6668e4777ff5f0267c2
= <=>
2023-06-29 06:14:57 +0200

    fix: editor preview

--- packages/pluggableWidgets/carousel-web/src/Carousel.editorPreview.tsx
+++ packages/pluggableWidgets/carousel-web/src/Carousel.editorPreview.tsx
@@ -1,31 +1,33 @@
-import { createElement, ReactNode, useId } from "react";
+import { createElement, ReactElement } from "react";
 import { CarouselPreviewProps } from "../typings/CarouselProps";
 import { Carousel } from "./components/Carousel";
 import { GUID } from "mendix";
+import { generateUUID } from "@mendix/pluggable-widgets-commons/components/web";
 
 export function getPreviewCss(): string {
     return require("./ui/Carousel.scss");
 }
 
-export function preview(props: CarouselPreviewProps): ReactNode {
-    const id = useId();
+export function CarouselPreviewComponent(props: CarouselPreviewProps): ReactElement {
     return (
         <Carousel
-            id={id}
+            id={generateUUID().toString()}
             className={props.className}
             navigation={props.navigation}
             pagination={props.showPagination}
             loop={false}
-            items={[
-                {
-                    id: "1" as GUID,
-                    content: (
-                        <props.content.renderer caption="Carousel item content">
-                            <div />
-                        </props.content.renderer>
-                    )
-                }
-            ]}
+            items={["1", "2", "3"].map(item => ({
+                id: item as GUID,
+                content: (
+                    <props.content.renderer caption={`Carousel item content ${item}`}>
+                        <div>{`Carousel item content ${item}`}</div>
+                    </props.content.renderer>
+                )
+            }))}
         />
     );
+}
+
+export function preview(props: CarouselPreviewProps): ReactElement {
+    return <CarouselPreviewComponent {...props} />;
 }
