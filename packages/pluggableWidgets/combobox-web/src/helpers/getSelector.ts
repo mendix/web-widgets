0574dbded160b5da4a7a0853db1ecc55433145ef
Rahman <rahmanunver@gmail.com>
2023-07-06 17:08:13 +0200

    feat(combobox-web): reusing by duplicating logic of SingleSelection, WIP

--- packages/pluggableWidgets/combobox-web/src/helpers/getSelector.ts
+++ packages/pluggableWidgets/combobox-web/src/helpers/getSelector.ts
@@ -1,13 +1,21 @@
 import { ComboboxContainerProps } from "../../typings/ComboboxProps";
 import { AssociationSingleSelector } from "./Association/AssociationSingleSelector";
+import { AssociationMultiSelector } from "./Association/AssociationMultiSelector";
 import { EnumBooleanSingleSelector } from "./EnumBool/EnumBoolSingleSelector";
-import { SingleSelector } from "./types";
+import { SingleSelector, MultiSelector } from "./types";
 
 export function getSelector(props: ComboboxContainerProps): SingleSelector {
     if (props.optionsSourceType === "enumeration" || props.optionsSourceType === "boolean") {
         return new EnumBooleanSingleSelector();
     } else if (props.optionsSourceType === "association") {
         return new AssociationSingleSelector();
+    } else {
+        throw new Error(`'optionsSourceType' of type '${props.optionsSourceType}' is not supported`);
+    }
+}
+export function getMultiSelector(props: ComboboxContainerProps): MultiSelector {
+    if (props.attributeAssociation.type === "ReferenceSet") {
+        return new AssociationMultiSelector();
     } else {
         throw new Error(`'optionsSourceType' of type '${props.optionsSourceType}' is not supported`);
     }
