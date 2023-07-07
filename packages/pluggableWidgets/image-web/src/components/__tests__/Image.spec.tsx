f3d46c87fad82ba0da7787fcdc694e67d2ed1b2a
hedwig.doets <hedwig.doets@mendix.com>
2023-07-07 14:40:23 +0200

    test(image-web): add tests that check for tabindex when there is an onclick action

--- packages/pluggableWidgets/image-web/src/components/__tests__/Image.spec.tsx
+++ packages/pluggableWidgets/image-web/src/components/__tests__/Image.spec.tsx
@@ -105,6 +105,21 @@ describe("Image", () => {
             expect(onClickMock).toHaveBeenCalled();
         });
 
+        it("has tabindex if there is an action with OnClick", () => {
+            const onClickMock = jest.fn();
+            const imageRender = mount(<Image {...imageProps} onClick={onClickMock} onClickType="action" />);
+            const image = imageRender.find("img");
+
+            expect(image.prop("tabIndex")).not.toBeUndefined();
+        });
+
+        it("has no tabindex if there is no action with OnClick", () => {
+            const imageRender = mount(<Image {...imageProps} />);
+            const image = imageRender.find("img");
+
+            expect(image.prop("tabIndex")).toBeUndefined();
+        });
+
         it("calls the onClick when clicking on a glyph icon", () => {
             const onClickMock = jest.fn();
             const imageRender = mount(<Image {...glyphiconProps} onClick={onClickMock} onClickType="action" />);
