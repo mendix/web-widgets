0278971993b4c89348a6c34ff7cd5ef49cb1b522
Rahman <rahmanunver@gmail.com>
2023-07-13 09:26:59 +0200

    style(combobox-web): basic rename

--- packages/pluggableWidgets/combobox-web/src/Combobox.tsx
+++ packages/pluggableWidgets/combobox-web/src/Combobox.tsx
@@ -1,7 +1,7 @@
 import { createElement, ReactElement } from "react";
 
 import { ComboboxContainerProps } from "../typings/ComboboxProps";
-import { Combobox as ComboboxComponent } from "./components/Combobox";
+import { Combobox as SingleSelection } from "./components/Combobox";
 import { MultiSelection } from "./components/MultiSelection/MultiSelection";
 
 import "./ui/Combobox.scss";
@@ -10,6 +10,6 @@ export default function Combobox(props: ComboboxContainerProps): ReactElement {
     return props.attributeAssociation?.type === "ReferenceSet" ? (
         <MultiSelection {...props} />
     ) : (
-        <ComboboxComponent {...props} />
+        <SingleSelection {...props} />
     );
 }
