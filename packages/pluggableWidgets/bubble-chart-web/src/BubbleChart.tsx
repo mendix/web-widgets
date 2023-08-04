cc56879fa54571d948a526bbbba119d4d22de299
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2023-08-04 15:39:13 +0200

    feat(bubble-chart-web): link onClick action to datasource

--- packages/pluggableWidgets/bubble-chart-web/src/BubbleChart.tsx
+++ packages/pluggableWidgets/bubble-chart-web/src/BubbleChart.tsx
@@ -1,9 +1,10 @@
-import { createElement, ReactElement, useCallback } from "react";
-import { ChartWidget, ChartWidgetProps } from "@mendix/shared-charts";
+import { createElement, ReactElement, useCallback, memo } from "react";
+import { ChartWidget, ChartWidgetProps, traceEqual } from "@mendix/shared-charts";
 import classNames from "classnames";
 import { BubbleChartContainerProps, LinesType } from "../typings/BubbleChartProps";
 import { usePlotChartDataSeries, getPlotChartDataTransforms } from "@mendix/shared-charts/dist/hooks";
 import { calculateSizeRef } from "./utils";
+import { flatEqual, defaultEqual } from "@mendix/pluggable-widgets-commons/dist/utils/flatEqual";
 import Big from "big.js";
 
 const bubbleChartLayoutOptions: ChartWidgetProps["layoutOptions"] = {
@@ -25,77 +26,85 @@ const bubbleChartConfigOptions: ChartWidgetProps["configOptions"] = {
 };
 const bubbleChartSeriesOptions: ChartWidgetProps["seriesOptions"] = {};
 
-export function BubbleChart(props: BubbleChartContainerProps): ReactElement {
-    const getSizes = (line: LinesType): number[] => {
-        const sizes: number[] = [];
-        let dataset: LinesType["staticDataSource"] | LinesType["dynamicDataSource"] | undefined;
-        let sizeAttribute: LinesType["staticSizeAttribute"] | LinesType["dynamicSizeAttribute"];
-        if (line.dataSet === "static") {
-            dataset = line.staticDataSource;
-            sizeAttribute = line.staticSizeAttribute;
-        } else {
-            dataset = line.dynamicDataSource;
-            sizeAttribute = line.dynamicSizeAttribute;
-        }
-        if (dataset?.items) {
-            for (const item of dataset.items) {
-                const size = sizeAttribute?.get(item);
-                const value = size?.value instanceof Big ? Number(size.value.toString()) : size?.value;
-                if (value) {
-                    sizes.push(value);
+export const BubbleChart = memo(
+    // disable eslint rule to have nice component name in component tree at devtools
+    // eslint-disable-next-line prefer-arrow-callback
+    function BubbleChart(props: BubbleChartContainerProps): ReactElement {
+        const getSizes = (line: LinesType): number[] => {
+            const sizes: number[] = [];
+            let dataset: LinesType["staticDataSource"] | LinesType["dynamicDataSource"] | undefined;
+            let sizeAttribute: LinesType["staticSizeAttribute"] | LinesType["dynamicSizeAttribute"];
+            if (line.dataSet === "static") {
+                dataset = line.staticDataSource;
+                sizeAttribute = line.staticSizeAttribute;
+            } else {
+                dataset = line.dynamicDataSource;
+                sizeAttribute = line.dynamicSizeAttribute;
+            }
+            if (dataset?.items) {
+                for (const item of dataset.items) {
+                    const size = sizeAttribute?.get(item);
+                    const value = size?.value instanceof Big ? Number(size.value.toString()) : size?.value;
+                    if (value) {
+                        sizes.push(value);
+                    }
                 }
             }
-        }
-        return sizes;
-    };
+            return sizes;
+        };
 
-    const chartBubbles = usePlotChartDataSeries(
-        props.lines,
-        useCallback((line, dataPoints) => {
-            const size = getSizes(line);
-            const markerOptions = calculateSizeRef(
-                line,
-                { size },
-                {
-                    width: props.width,
-                    widthUnit: props.widthUnit,
-                    height: props.height,
-                    heightUnit: props.heightUnit
-                }
-            );
-            return {
-                type: "scatter",
-                mode: "markers",
-                marker: {
-                    color: line.markerColor?.value,
-                    symbol: ["circle"],
-                    size,
-                    ...markerOptions
-                },
-                transforms: getPlotChartDataTransforms(line.aggregationType, dataPoints)
-            };
-        }, [])
-    );
-    return (
-        <ChartWidget
-            type="BubbleChart"
-            className={classNames("widget-bubble-chart", props.class)}
-            data={chartBubbles ?? []}
-            width={props.width}
-            widthUnit={props.widthUnit}
-            height={props.height}
-            heightUnit={props.heightUnit}
-            showLegend={props.showLegend}
-            xAxisLabel={props.xAxisLabel?.value}
-            yAxisLabel={props.yAxisLabel?.value}
-            gridLinesMode={props.gridLines}
-            showSidebarEditor={props.enableDeveloperMode}
-            customLayout={props.customLayout}
-            customConfig={props.customConfigurations}
-            layoutOptions={bubbleChartLayoutOptions}
-            configOptions={bubbleChartConfigOptions}
-            seriesOptions={bubbleChartSeriesOptions}
-            enableThemeConfig={props.enableThemeConfig}
-        />
-    );
-}
+        const chartBubbles = usePlotChartDataSeries(
+            props.lines,
+            useCallback((line, dataPoints) => {
+                const size = getSizes(line);
+                const markerOptions = calculateSizeRef(
+                    line,
+                    { size },
+                    {
+                        width: props.width,
+                        widthUnit: props.widthUnit,
+                        height: props.height,
+                        heightUnit: props.heightUnit
+                    }
+                );
+                return {
+                    type: "scatter",
+                    mode: "markers",
+                    marker: {
+                        color: line.markerColor?.value,
+                        symbol: ["circle"],
+                        size,
+                        ...markerOptions
+                    },
+                    transforms: getPlotChartDataTransforms(line.aggregationType, dataPoints)
+                };
+            }, [])
+        );
+        return (
+            <ChartWidget
+                type="BubbleChart"
+                className={classNames("widget-bubble-chart", props.class)}
+                data={chartBubbles ?? []}
+                width={props.width}
+                widthUnit={props.widthUnit}
+                height={props.height}
+                heightUnit={props.heightUnit}
+                showLegend={props.showLegend}
+                xAxisLabel={props.xAxisLabel?.value}
+                yAxisLabel={props.yAxisLabel?.value}
+                gridLinesMode={props.gridLines}
+                showSidebarEditor={props.enableDeveloperMode}
+                customLayout={props.customLayout}
+                customConfig={props.customConfigurations}
+                layoutOptions={bubbleChartLayoutOptions}
+                configOptions={bubbleChartConfigOptions}
+                seriesOptions={bubbleChartSeriesOptions}
+                enableThemeConfig={props.enableThemeConfig}
+            />
+        );
+    },
+    (prev, next) =>
+        flatEqual(prev, next, (oldProp, newProp, propName) => {
+            return propName === "lines" ? flatEqual(oldProp, newProp, traceEqual) : defaultEqual(oldProp, newProp);
+        })
+);
