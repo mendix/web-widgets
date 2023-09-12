c02fbafb3a4c23306f8d4dc0cb0b7e448eb4c14c
gjulivan <grandjulivan@gmail.com>
2023-09-12 14:38:05 +0200

    feat(combo-box): update combobox design mode

--- packages/pluggableWidgets/combobox-web/src/Combobox.editorPreview.tsx
+++ packages/pluggableWidgets/combobox-web/src/Combobox.editorPreview.tsx
@@ -1,45 +1,50 @@
-import classNames from "classnames";
-import Downshift from "downshift";
-import { createElement, ReactElement } from "react";
+import { generateUUID } from "@mendix/widget-plugin-platform/framework/generate-uuid";
+import { ReferenceValue } from "mendix";
+import { createElement, ReactElement, ReactNode, useMemo } from "react";
 import { ComboboxPreviewProps } from "../typings/ComboboxProps";
-import { ClearButton, DownArrow } from "./assets/icons";
-import { InputPlaceholder } from "./components/Placeholder";
+import { SingleSelection } from "./components/SingleSelection/SingleSelection";
+import { AssociationSimpleCaptionsProvider } from "./helpers/Association/AssociationSimpleCaptionsProvider";
+import { BaseAssociationSelector } from "./helpers/Association/BaseAssociationSelector";
+import { SingleSelector } from "./helpers/types";
 import { getDatasourcePlaceholderText } from "./helpers/utils";
 import "./ui/Combobox.scss";
 
+class AssociationPreviewCaptionsProvider extends AssociationSimpleCaptionsProvider {
+    emptyCaption = "Combo box";
+    get(value: string | null): string {
+        return value || this.emptyCaption;
+    }
+    render(value: string | null): ReactNode {
+        return <span className="widget-combobox-caption">{this.get(value)}</span>;
+    }
+}
+class AssociationPreviewSelector extends BaseAssociationSelector<string, ReferenceValue> implements SingleSelector {
+    type = "single" as const;
+    constructor(props: ComboboxPreviewProps) {
+        super();
+        this.caption = new AssociationPreviewCaptionsProvider(new Map());
+        this.readOnly = props.readOnly;
+        this.clearable = props.clearable;
+        this.currentValue = getDatasourcePlaceholderText(props);
+    }
+}
+
 export const preview = (props: ComboboxPreviewProps): ReactElement => {
-    const backgroundColor = props.readOnly ? "#C8C8C8" : undefined;
+    const id = generateUUID().toString();
+    const commonProps = {
+        tabIndex: 1,
+        inputId: id,
+        labelId: `${id}-label`
+    };
+
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const selector: SingleSelector = useMemo(() => {
+        return new AssociationPreviewSelector(props);
+    }, [props]);
+
     return (
-        <Downshift>
-            {({ getInputProps, isOpen, getToggleButtonProps }) => (
-                <div className="widget-combobox">
-                    <div
-                        style={{ backgroundColor }}
-                        className={classNames("form-control", "widget-combobox-input-container", {
-                            "widget-combobox-input-container-active": isOpen
-                        })}
-                        {...getToggleButtonProps()}
-                    >
-                        <div className="widget-combobox-selected-items">
-                            <input
-                                style={{ backgroundColor }}
-                                className="widget-combobox-input"
-                                {...getInputProps()}
-                                placeholder=" "
-                            />
-                            <InputPlaceholder isEmpty>{getDatasourcePlaceholderText(props)}</InputPlaceholder>
-                        </div>
-                        {props.clearable && (
-                            <button className="widget-combobox-clear-button">
-                                <ClearButton />
-                            </button>
-                        )}
-                        <div className="widget-combobox-down-arrow">
-                            <DownArrow />
-                        </div>
-                    </div>
-                </div>
-            )}
-        </Downshift>
+        <div className="widget-combobox widget-combobox-editor-preview">
+            <SingleSelection selector={selector} {...commonProps} />
+        </div>
     );
 };
