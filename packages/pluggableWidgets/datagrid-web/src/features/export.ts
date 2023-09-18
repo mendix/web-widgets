a8e77d574edfb599a292e848a3117ccb48a53609
Samuel Reichert <samuelreichertt@gmail.com>
2023-09-18 17:37:38 +0200

    fix: update export API to export all data no matter the offset

--- packages/pluggableWidgets/datagrid-web/src/features/export.ts
+++ packages/pluggableWidgets/datagrid-web/src/features/export.ts
@@ -48,6 +48,7 @@ type UseDG2ExportApi = {
 };
 
 type UseExportAPIReturn = {
+    exporting: boolean;
     items: ObjectItem[];
 };
 
@@ -62,24 +63,25 @@ export const useDG2ExportApi = ({
     pageSize,
     setOffset
 }: UseDG2ExportApi): UseExportAPIReturn => {
-    const [startProcess, setStartProcess] = useState(false);
+    const [exporting, setExporting] = useState(false);
+    const [finished, setFinished] = useState(false);
     const [sentColumns, setSentColumns] = useState(false);
     const [callback, setCallback] = useState<CallbackFunction>();
-    const [memoizedItems, setItems] = useState<ObjectItem[]>([]);
+    const [memoizedItems, setMemoizedItems] = useState<ObjectItem[]>([]);
+    const [resetOffset, setResetOffset] = useState(false);
+    const [initialOffset, setInitialOffset] = useState<number | undefined>();
 
     const create = (): DataExportStream => {
-        if (startProcess) {
+        if (exporting) {
             throw new Error("There is an export already in progress");
         }
 
         const dataExportStream: DataExportStream = {
-            process: (cb: CallbackFunction) => {
-                setCallback(() => cb);
-            },
-            start: () => setStartProcess(true),
+            process: (cb: CallbackFunction) => setCallback(() => cb),
+            start: () => setExporting(true),
             abort: () => {
-                if (startProcess) {
-                    setStartProcess(false);
+                if (exporting) {
+                    setFinished(true);
                 }
             }
         };
@@ -100,13 +102,7 @@ export const useDG2ExportApi = ({
     }, []);
 
     useEffect(() => {
-        if (memoizedItems.length === 0 && items) {
-            setItems(items);
-        }
-    }, [memoizedItems.length, items]);
-
-    useEffect(() => {
-        if (startProcess) {
+        if (exporting) {
             const runColumnsCallback = async (): Promise<void> => {
                 if (!callback) {
                     return;
@@ -115,34 +111,75 @@ export const useDG2ExportApi = ({
                 await callback(datagridColumns);
                 setSentColumns(true);
             };
+
             const runDataCallback = async (): Promise<void> => {
                 if (!callback || !items) {
                     return;
                 }
 
-                if (items && hasMoreItems) {
+                if (hasMoreItems) {
                     await callback(exportData(items, columns));
                     setOffset(offset + pageSize);
-                }
-
-                if (items && hasMoreItems === false) {
+                } else {
                     await callback(exportData(items, columns));
                     await callback({ type: "end" });
-                    setOffset(0);
-                    setStartProcess(false);
+                    setFinished(true);
                 }
             };
 
-            if (sentColumns === false) {
-                runColumnsCallback();
+            if (initialOffset === undefined) {
+                setInitialOffset(offset);
+            }
+
+            if (memoizedItems.length === 0 && offset === initialOffset) {
+                setMemoizedItems(items!);
+            }
+
+            if (resetOffset === false) {
+                if (offset > 0) {
+                    setOffset(0);
+                } else {
+                    setResetOffset(true);
+                }
             } else {
-                runDataCallback();
+                if (sentColumns === false) {
+                    runColumnsCallback();
+                } else {
+                    runDataCallback();
+                }
             }
         }
-    }, [callback, columns, hasMoreItems, items, sentColumns, startProcess]);
+    }, [
+        callback,
+        columns,
+        exporting,
+        hasMoreItems,
+        initialOffset,
+        items,
+        memoizedItems.length,
+        offset,
+        resetOffset,
+        sentColumns
+    ]);
 
-    const exporting = items !== memoizedItems ? true : startProcess;
-    return { items: exporting ? memoizedItems : items ?? [] };
+    useEffect(() => {
+        if (finished) {
+            console.info({ offset, initialOffset });
+            if (offset === initialOffset) {
+                setExporting(false);
+                setResetOffset(false);
+                setInitialOffset(undefined);
+                setFinished(false);
+            } else {
+                setOffset(initialOffset || 0);
+            }
+        }
+    }, [offset, finished]);
+
+    return {
+        exporting: exporting,
+        items: exporting ? memoizedItems : items ?? []
+    };
 };
 
 function exportColumns(columns: ColumnsType[]): Message {
