b8e9e6b9ea6e05c964a7a39f4c4d658f38b4cac7
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2023-10-10 14:17:32 +0200

    add widget footer

--- packages/pluggableWidgets/gallery-web/src/components/WidgetFooter.tsx
+++ packages/pluggableWidgets/gallery-web/src/components/WidgetFooter.tsx
@@ -1,5 +1,16 @@
-import { createElement, ReactElement } from "react";
+import classNames from "classnames";
+import { createElement, ReactNode, ReactElement } from "react";
 
-export function WidgetFooter(): ReactElement {
-    return <div>1</div>;
+type P = Omit<JSX.IntrinsicElements["div"], "ref">;
+
+interface WidgetFooterProps extends P {
+    children?: ReactNode;
+}
+
+export function WidgetFooter({ children, className, ...rest }: WidgetFooterProps): ReactElement {
+    return (
+        <div className={classNames("widget-gallery-footer", className)} {...rest}>
+            {children}
+        </div>
+    );
 }
