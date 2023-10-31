faa08fe3156192f4c817eab37619348ebec549eb
Andries Smit <mr.andries.smit@gmail.com>
2023-10-31 16:17:36 +0100

    feat(datagrid-web): paging button when necessary

--- packages/pluggableWidgets/datagrid-web/src/Datagrid.editorConfig.ts
+++ packages/pluggableWidgets/datagrid-web/src/Datagrid.editorConfig.ts
@@ -74,10 +74,8 @@ export function getProperties(
         }
     });
     if (values.pagination !== "buttons") {
+        hidePropertyIn(defaultProperties, values, "showPagingButtons");
         hidePropertyIn(defaultProperties, values, "pagingPosition");
-    }
-    if (values.pagination === "none") {
-        hidePropertyIn(defaultProperties, values, "pageSize");
     }
     if (values.showEmptyPlaceholder === "none") {
         hidePropertyIn(defaultProperties, values, "emptyPlaceholder");
