4855f2533a3ac0f689a9b1fadc06f398f6fb6c46
Rahman <rahmanunver@gmail.com>
2023-11-14 13:00:20 +0100

    chore(combobox-web): remove log

--- packages/pluggableWidgets/combobox-web/src/hooks/useMenuPlacement.ts
+++ packages/pluggableWidgets/combobox-web/src/hooks/useMenuPlacement.ts
@@ -10,7 +10,6 @@ export function useMenuPlacement(menuRef: HTMLDivElement | null, isOpen: boolean
     const comboboxMenuRect = menuRef?.getBoundingClientRect();
     if (menuRef && observer && comboboxMenuRect && comboboxMenuRect.height !== 0) {
         const spaceBetween = window.innerHeight - observer.y;
-        console.log(comboboxMenuRect);
 
         if (spaceBetween < comboboxMenuRect.height) {
             return { bottom: observer.top - comboboxMenuRect.height - 4, left: observer.left }; // 4 is for bottom margin
