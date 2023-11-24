4abe80487da9ed1a529db79e541b6e8269b5bd7b
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2023-11-24 13:40:28 +0100

    refactor: rename type

--- packages/shared/widget-plugin-filtering/src/main.ts
+++ packages/shared/widget-plugin-filtering/src/main.ts
@@ -2,4 +2,4 @@ export * from "./provider.js";
 export { error, value } from "./result-meta.js";
 export type { Result, Success, Error } from "./result-meta.js";
 export { readInitFilterProps, unwrapAndExpression } from "./read-init-props.js";
-export type { InitialFilterProps, BinaryExpression } from "./read-init-props.js";
+export type { InitialFilterValue, BinaryExpression } from "./read-init-props.js";
