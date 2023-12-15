8ab9ea59a1726f0f9573b631194898bb68b21846
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2023-12-15 18:57:38 +0100

    refactor: return null while loading

--- packages/pluggableWidgets/datagrid-web/src/Datagrid.tsx
+++ packages/pluggableWidgets/datagrid-web/src/Datagrid.tsx
@@ -4,13 +4,13 @@ import Container from "./components/Datagrid";
 import { useModelApi } from "./features/model/main";
 import { useUnit } from "effector-react";
 
-export default function Datagrid(props: DatagridContainerProps): React.ReactElement {
+export default function Datagrid(props: DatagridContainerProps): React.ReactElement | null {
     const { status, model, actions } = useModelApi(props);
 
     const loading = useUnit(status) !== "ready";
 
     if (loading) {
-        return <div>Loading...</div>;
+        return null;
     }
 
     return <Container {...props} model={model} actions={actions} />;
