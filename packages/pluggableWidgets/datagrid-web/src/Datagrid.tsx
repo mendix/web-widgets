d60eb8147eb003cb9966670d4c3d4c09d5083031
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2023-12-16 15:52:22 +0100

    refactor: import grid styles

--- packages/pluggableWidgets/datagrid-web/src/Datagrid.tsx
+++ packages/pluggableWidgets/datagrid-web/src/Datagrid.tsx
@@ -3,6 +3,7 @@ import { DatagridContainerProps } from "../typings/DatagridProps";
 import Container from "./components/Datagrid";
 import { useModelApi } from "./features/model/main";
 import { useUnit } from "effector-react";
+import "./ui/Datagrid.scss";
 
 export default function Datagrid(props: DatagridContainerProps): React.ReactElement | null {
     const { status, model, actions } = useModelApi(props);
