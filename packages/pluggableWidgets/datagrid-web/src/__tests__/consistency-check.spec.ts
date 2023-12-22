a88360dfe6125e2b0cd20d922e828ae7838f4455
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2023-12-22 11:37:41 +0100

    test(datagrid-web): add tests for consistency check

---
+++ packages/pluggableWidgets/datagrid-web/src/__tests__/consistency-check.spec.ts
@@ -0,0 +1,80 @@
+import { DatagridPreviewProps } from "../../typings/DatagridProps";
+import { check } from "../Datagrid.editorConfig";
+
+describe("consistency check", () => {
+    describe("returns error when row select method and action trigger overlap", () => {
+        test("selection: single by row click", () => {
+            const props = {
+                itemSelection: "Single",
+                itemSelectionMethod: "rowClick",
+                onClick: {},
+                onClickTrigger: "single",
+                columns: []
+            };
+
+            expect(check(props as unknown as DatagridPreviewProps)).toMatchSnapshot();
+        });
+
+        test("selection: single by checkbox", () => {
+            const props = {
+                itemSelection: "Single",
+                itemSelectionMethod: "checkbox",
+                onClick: {},
+                onClickTrigger: "single",
+                columns: []
+            };
+
+            expect(check(props as unknown as DatagridPreviewProps)).toMatchSnapshot();
+        });
+
+        test("selection: multi by row click", () => {
+            const props = {
+                itemSelection: "Multi",
+                itemSelectionMethod: "rowClick",
+                onClick: {},
+                onClickTrigger: "single",
+                columns: []
+            };
+
+            expect(check(props as unknown as DatagridPreviewProps)).toMatchSnapshot();
+        });
+    });
+
+    describe("returns empty array when row selection method and action trigger not overlap", () => {
+        test("selection: none", () => {
+            const props = {
+                itemSelection: "None",
+                itemSelectionMethod: "checkbox",
+                onClick: {},
+                onClickTrigger: "single",
+                columns: []
+            };
+
+            expect(check(props as unknown as DatagridPreviewProps)).toEqual([]);
+        });
+
+        test("selection: single by checkbox", () => {
+            const props = {
+                itemSelection: "Single",
+                itemSelectionMethod: "checkbox",
+                onClick: {},
+                onClickTrigger: "single",
+                columns: []
+            };
+
+            expect(check(props as unknown as DatagridPreviewProps)).toEqual([]);
+        });
+
+        test("selection: single by checkbox, trigger: double", () => {
+            const props = {
+                itemSelection: "Single",
+                itemSelectionMethod: "checkbox",
+                onClick: {},
+                onClickTrigger: "double",
+                columns: []
+            };
+
+            expect(check(props as unknown as DatagridPreviewProps)).toEqual([]);
+        });
+    });
+});
