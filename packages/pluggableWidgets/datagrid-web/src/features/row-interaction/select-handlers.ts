414a5127ed77df6ca472f19b58612993c5bde0d9
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2024-01-03 15:45:43 +0100

    refactor(datagrid-web): finish on click

--- packages/pluggableWidgets/datagrid-web/src/features/row-interaction/select-handlers.ts
+++ packages/pluggableWidgets/datagrid-web/src/features/row-interaction/select-handlers.ts
@@ -1,8 +1,8 @@
-import { EventCaseEntry } from "@mendix/widget-plugin-grid/event-switch/base";
+import { ElementEntries, EventCaseEntry } from "@mendix/widget-plugin-grid/event-switch/base";
 import { SelectFx } from "@mendix/widget-plugin-grid/selection";
 import { CellContext } from "./base";
 
-const onClick = (selectFx: SelectFx): EventCaseEntry<"div", CellContext, "onClick"> => ({
+const onClick = (selectFx: SelectFx): EventCaseEntry<CellContext, HTMLDivElement, "onClick"> => ({
     eventName: "onClick",
     filter: ctx => {
         return ctx.selectionMethod === "rowClick" && (ctx.clickTrigger === "none" || ctx.clickTrigger === "double");
@@ -12,6 +12,6 @@ const onClick = (selectFx: SelectFx): EventCaseEntry<"div", CellContext, "onClic
     }
 });
 
-export function createSelectHandlers(selectFx: SelectFx): Array<EventCaseEntry<"div", CellContext>> {
+export function createSelectHandlers(selectFx: SelectFx): Array<ElementEntries<CellContext, HTMLDivElement>> {
     return [onClick(selectFx)];
 }
