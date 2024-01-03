05169c843224be8822eeb2f1473f78db03edca37
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2024-01-03 16:50:03 +0100

    test(datagrid-web): change dblclick cases

--- packages/pluggableWidgets/datagrid-web/src/features/row-interaction/__tests__/cell-pointer.spec.tsx
+++ packages/pluggableWidgets/datagrid-web/src/features/row-interaction/__tests__/cell-pointer.spec.tsx
@@ -121,20 +121,20 @@ describe("grid cell", () => {
 
     describe("on dblclick event", () => {
         const cases = [
-            { ct: "single", sm: "rowClick", method: "none" },
-            { ct: "double", sm: "rowClick", method: "onExecuteAction" },
-            { ct: "none", sm: "rowClick", method: "none" },
-            { ct: "single", sm: "checkbox", method: "none" },
-            { ct: "double", sm: "checkbox", method: "onExecuteAction" },
-            { ct: "none", sm: "checkbox", method: "none" },
-            { ct: "single", sm: "none", method: "none" },
-            { ct: "double", sm: "none", method: "onExecuteAction" },
-            { ct: "none", sm: "none", method: "none" }
+            { ct: "single", sm: "rowClick", n: 0 },
+            { ct: "double", sm: "rowClick", n: 1 },
+            { ct: "none", sm: "rowClick", n: 0 },
+            { ct: "single", sm: "checkbox", n: 2 },
+            { ct: "double", sm: "checkbox", n: 1 },
+            { ct: "none", sm: "checkbox", n: 0 },
+            { ct: "single", sm: "none", n: 2 },
+            { ct: "double", sm: "none", n: 1 },
+            { ct: "none", sm: "none", n: 0 }
         ];
 
         test.each(cases)(
-            "calls $method when selection method is $sm and click trigger is $ct",
-            async ({ ct, sm, method }) => {
+            "calls onExecuteAction $n times when selection method is $sm and click trigger is $ct",
+            async ({ ct, sm, n }) => {
                 const onExecuteAction = jest.fn();
 
                 const [item] = objectItems(1);
@@ -153,10 +153,8 @@ describe("grid cell", () => {
                 const { user, getByRole } = setup(<div role="gridcell" {...props} />);
                 await user.dblClick(getByRole("gridcell"));
 
-                if (method === "none") {
-                    expect(onExecuteAction).toHaveBeenCalledTimes(0);
-                } else {
-                    expect(onExecuteAction).toHaveBeenCalledTimes(1);
+                expect(onExecuteAction).toHaveBeenCalledTimes(n);
+                if (n > 0) {
                     expect(onExecuteAction).toHaveBeenLastCalledWith(item);
                 }
             }
