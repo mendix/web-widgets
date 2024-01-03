c2a57ad28879549d1a5d3d48dd7f0edfb1b00823
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2024-01-03 16:50:03 +0100

    test(datagrid-web): change dblclick cases

--- packages/shared/widget-plugin-grid/src/event-switch/use-event-switch.ts
+++ packages/shared/widget-plugin-grid/src/event-switch/use-event-switch.ts
@@ -1,5 +1,5 @@
 import { useMemo, useRef, useEffect } from "react";
-import { DOMElement, ElementEntries } from "./base";
+import { DOMElement, ElementEntries, EventCaseEntry, InferEvent } from "./base";
 import { groupEntries } from "./utils";
 
 export function useEventSwitch<Context, Element>(
@@ -25,13 +25,23 @@ function eventSwitch<Context, Element>(
     const grouped = groupEntries(cases);
     return {
         onClick(event) {
-            grouped.onClick?.forEach(entry => {
-                const ctx = contextFn();
-                const canRun = entry.filter ? entry.filter(ctx, event) : true;
-                if (canRun) {
-                    entry.handler(ctx, event);
-                }
-            });
+            grouped.onClick?.forEach(entry => exec(contextFn, event, entry));
+        },
+        onDoubleClick(event) {
+            grouped.onDoubleClick?.forEach(entry => exec(contextFn, event, entry));
         }
     };
+}
+
+function exec<
+    Context,
+    Element,
+    EventName extends keyof DOMElement<Element>,
+    E extends InferEvent<DOMElement<Element>[EventName]>
+>(contextFn: () => Context, event: E, entry: EventCaseEntry<Context, Element, EventName>): void {
+    const ctx = contextFn();
+    const canRun = entry.filter ? entry.filter(ctx, event) : true;
+    if (canRun) {
+        entry.handler(ctx, event);
+    }
 }
