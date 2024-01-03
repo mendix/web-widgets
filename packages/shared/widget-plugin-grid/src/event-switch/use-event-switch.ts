68d86b70185490d027ebe0f6cba2949ec0019b40
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2024-01-03 15:45:43 +0100

    refactor(datagrid-web): finish on click

--- packages/shared/widget-plugin-grid/src/event-switch/use-event-switch.ts
+++ packages/shared/widget-plugin-grid/src/event-switch/use-event-switch.ts
@@ -1,8 +1,37 @@
-import { EventCaseEntry } from "./base";
+import { useMemo, useRef, useEffect } from "react";
+import { DOMElement, ElementEntries } from "./base";
+import { groupEntries } from "./utils";
 
-export function useEventSwitch<Element extends keyof JSX.IntrinsicElements, Context>(
-    _contextFn: () => Context,
-    _cases: Array<EventCaseEntry<Element, Context>>
-): React.DOMAttributes<HTMLDivElement> {
-    return {};
+export function useEventSwitch<Context, Element>(
+    contextFn: () => Context,
+    cases: () => Array<ElementEntries<Context, Element>>
+): DOMElement<Element> {
+    const contextRef = useRef(contextFn);
+
+    useEffect(() => {
+        contextRef.current = contextFn;
+    });
+
+    return useMemo(() => {
+        return eventSwitch(() => contextRef.current(), cases());
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+}
+
+function eventSwitch<Context, Element>(
+    contextFn: () => Context,
+    cases: Array<ElementEntries<Context, Element>>
+): DOMElement<Element> {
+    const grouped = groupEntries(cases);
+    return {
+        onClick(event) {
+            grouped.onClick?.forEach(entry => {
+                const ctx = contextFn();
+                const canRun = entry.filter ? entry.filter(ctx, event) : true;
+                if (canRun) {
+                    entry.handler(ctx, event);
+                }
+            });
+        }
+    };
 }
