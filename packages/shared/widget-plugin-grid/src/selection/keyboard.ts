caa4d8716af63710c982acac1d91878633b1fb27
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2024-01-04 14:09:59 +0100

    test(datagrid-web): add test for select all

--- packages/shared/widget-plugin-grid/src/selection/keyboard.ts
+++ packages/shared/widget-plugin-grid/src/selection/keyboard.ts
@@ -1,4 +1,6 @@
+import { throttle } from "@mendix/widget-plugin-platform/utils/throttle";
 import { EventCaseEntry } from "../event-switch/base";
+import { SelectionType } from "./types";
 
 export function isSelectAllTrigger<T>(event: React.KeyboardEvent<T>): boolean {
     return event.code === "KeyA" && (event.metaKey || event.ctrlKey);
@@ -8,11 +10,54 @@ export function isSelectOneTrigger<T>(event: React.KeyboardEvent<T>): boolean {
     return event.code === "Space" && event.shiftKey;
 }
 
-export const preventScrollOnSpace = (): EventCaseEntry<unknown, Element, "onKeyUp"> => ({
+export function isOwnSpaceKey<T>(event: React.KeyboardEvent<T>): boolean {
+    return event.code === "Space" && event.target === event.currentTarget;
+}
+
+export const onOwnSpaceKeyUp = (
+    handler: (event: React.KeyboardEvent<Element>) => void
+): EventCaseEntry<unknown, Element, "onKeyUp"> => ({
     eventName: "onKeyUp",
-    filter: (_, event) => event.code === "Space" && event.target === event.currentTarget,
-    handler: (_, event) => {
-        event.stopPropagation();
-        event.preventDefault();
-    }
+    filter: (_, event) => isOwnSpaceKey(event),
+    handler: (_, event) => handler(event)
 });
+
+const selectAllStopKey = new Set(["KeyA", "MetaLeft", "MetaRight", "ControlLeft", "ControlRight"]);
+
+interface SelectAllContext {
+    selectionType: SelectionType;
+}
+
+export const onSelectAllHotKey = (
+    onStart: () => void,
+    onEnd?: () => void
+): [EventCaseEntry<SelectAllContext, Element, "onKeyDown">, EventCaseEntry<SelectAllContext, Element, "onKeyUp">] => {
+    let pressed = false;
+
+    const [onPressStart, stopThrottle] = throttle((): void => {
+        pressed = true;
+        onStart();
+    }, 500);
+
+    const onPressEnd = (): void => {
+        pressed = false;
+        stopThrottle();
+        onEnd?.();
+    };
+
+    const onKeyDown: EventCaseEntry<SelectAllContext, Element, "onKeyDown"> = {
+        eventName: "onKeyDown",
+        filter: (ctx, event) => {
+            return ctx.selectionType === "Multi" && isSelectAllTrigger(event);
+        },
+        handler: onPressStart
+    };
+
+    const onKeyUp: EventCaseEntry<SelectAllContext, Element, "onKeyUp"> = {
+        eventName: "onKeyUp",
+        filter: (ctx, event) => ctx.selectionType === "Multi" && selectAllStopKey.has(event.code) && pressed,
+        handler: onPressEnd
+    };
+
+    return [onKeyDown, onKeyUp];
+};
--- packages/shared/widget-plugin-grid/src/selection/utils.ts
+++ packages/shared/widget-plugin-grid/src/selection/utils.ts
@@ -24,3 +24,23 @@ export function getMultiSelectStatus(helper: SelectionHelper | undefined): Multi
 export function removeAllRanges(): void {
     window.getSelection()?.removeAllRanges();
 }
+
+export function blockUserSelect(): void {
+    document.body.style.userSelect = "none";
+}
+
+type UnblockUserSelectFn = {
+    (): void;
+    timeoutId?: number;
+};
+
+export const unblockUserSelect: UnblockUserSelectFn = () => {
+    if (unblockUserSelect.timeoutId) {
+        clearTimeout(unblockUserSelect.timeoutId);
+    }
+    unblockUserSelect.timeoutId = window.setTimeout(() => {
+        removeAllRanges();
+        document.body.style.userSelect = "";
+        unblockUserSelect.timeoutId = undefined;
+    }, 250);
+};
