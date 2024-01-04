3ab30a96d6e873e7ba112d282935c79230542557
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2024-01-04 11:27:31 +0100

    test(datagrid-web): add handler for shift+space

--- packages/shared/widget-plugin-grid/src/selection.ts
+++ packages/shared/widget-plugin-grid/src/selection.ts
@@ -1,5 +1,6 @@
 export * from "./selection/types.js";
 export * from "./selection/helpers.js";
+export * from "./selection/keyboard.js";
 export {
     getGlobalSelectionContext,
     useCreateSelectionContextValue,
