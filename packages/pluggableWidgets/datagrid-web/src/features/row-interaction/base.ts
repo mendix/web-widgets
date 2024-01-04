956af3479519c82e104015c5ae7a2aaa0b52c116
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2024-01-04 14:09:59 +0100

    test(datagrid-web): add test for select all

--- packages/pluggableWidgets/datagrid-web/src/features/row-interaction/base.ts
+++ packages/pluggableWidgets/datagrid-web/src/features/row-interaction/base.ts
@@ -1,3 +1,4 @@
+import { SelectionType } from "@mendix/widget-plugin-grid/selection";
 import { ObjectItem } from "mendix";
 
 export type SelectionMethod = "rowClick" | "checkbox" | "none";
@@ -7,5 +8,6 @@ export type ClickTrigger = "single" | "double" | "none";
 export interface CellContext {
     item: ObjectItem;
     selectionMethod: SelectionMethod;
+    selectionType: SelectionType;
     clickTrigger: ClickTrigger;
 }
