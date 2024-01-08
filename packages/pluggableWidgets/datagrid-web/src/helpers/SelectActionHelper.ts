9e48b5461d73ada4ae69a246732f031706ed06f1
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2024-01-08 15:39:10 +0100

    refactor: split code with helpers

---
+++ packages/pluggableWidgets/datagrid-web/src/helpers/SelectActionHelper.ts
@@ -0,0 +1,34 @@
+import { SelectActionHandler, SelectionHelper, WidgetSelectionProperty } from "@mendix/widget-plugin-grid/selection";
+import { ItemSelectionMethodEnum } from "../../typings/DatagridProps";
+export type SelectionMethod = "rowClick" | "checkbox" | "none";
+
+export class SelectActionHelper extends SelectActionHandler {
+    pageSize: number;
+    private _selectionMethod: ItemSelectionMethodEnum;
+    private _showSelectAllToggle: boolean;
+
+    constructor(
+        selection: WidgetSelectionProperty,
+        selectionHelper: SelectionHelper,
+        _selectionMethod: ItemSelectionMethodEnum,
+        _showSelectAllToggle: boolean,
+        pageSize: number
+    ) {
+        super(selection, selectionHelper);
+        this._selectionMethod = _selectionMethod;
+        this._showSelectAllToggle = _showSelectAllToggle;
+        this.pageSize = pageSize;
+    }
+
+    get selectionMethod(): SelectionMethod {
+        return this.selectionType === "None" ? "none" : this._selectionMethod;
+    }
+
+    get showCheckboxColumn(): boolean {
+        return this.selectionMethod === "checkbox";
+    }
+
+    get showSelectAllToggle(): boolean {
+        return this._showSelectAllToggle;
+    }
+}
