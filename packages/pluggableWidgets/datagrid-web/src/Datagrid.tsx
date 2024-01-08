e90ffa61d1b3d2d3642a8037d99d0f9348a6c7d7
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2024-01-08 23:21:45 +0100

    refactor(datagrid-web): add adjacent selection back

--- packages/pluggableWidgets/datagrid-web/src/Datagrid.tsx
+++ packages/pluggableWidgets/datagrid-web/src/Datagrid.tsx
@@ -135,12 +135,7 @@ export default function Datagrid(props: DatagridContainerProps): ReactElement {
         props.datasource.setSortOrder(undefined);
     }
 
-    const selectionHelper = useSelectionHelper(
-        props.itemSelection,
-        props.datasource,
-        props.onSelectionChange,
-        props.pageSize
-    );
+    const selectionHelper = useSelectionHelper(props.itemSelection, props.datasource, props.onSelectionChange);
 
     const selectActionHelper = useSelectActionHelper(
         {
