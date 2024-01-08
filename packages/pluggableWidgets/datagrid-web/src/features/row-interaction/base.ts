668b48051a8edbf5c2c29942b0ae630b530a4340
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2024-01-08 15:39:10 +0100

    refactor: split code with helpers

--- packages/pluggableWidgets/datagrid-web/src/features/row-interaction/base.ts
+++ packages/pluggableWidgets/datagrid-web/src/features/row-interaction/base.ts
@@ -1,9 +1,7 @@
 import { SelectionType } from "@mendix/widget-plugin-grid/selection";
 import { ObjectItem } from "mendix";
-
-export type SelectionMethod = "rowClick" | "checkbox" | "none";
-
-export type ClickTrigger = "single" | "double" | "none";
+import { ClickTrigger } from "../../helpers/ClickActionHelper";
+import { SelectionMethod } from "../../helpers/SelectActionHelper";
 
 export interface CellContext {
     item: ObjectItem;
