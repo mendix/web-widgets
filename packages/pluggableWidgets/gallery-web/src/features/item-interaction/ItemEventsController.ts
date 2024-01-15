be427ae1742f5a49ff2d101e9225abe04e0d51aa
Samuel Reichert <samuelreichertt@gmail.com>
2024-01-15 16:36:06 +0100

    feat(gallery-web): create focus target handler

--- packages/pluggableWidgets/gallery-web/src/features/item-interaction/ItemEventsController.ts
+++ packages/pluggableWidgets/gallery-web/src/features/item-interaction/ItemEventsController.ts
@@ -1,18 +1,25 @@
 import { ObjectItem } from "mendix";
 import { ElementProps } from "@mendix/widget-plugin-grid/event-switch/base";
 import { eventSwitch } from "@mendix/widget-plugin-grid/event-switch/event-switch";
+import { FocusTargetFx } from "@mendix/widget-plugin-grid/keyboard-navigation/base";
 import { SelectAllFx, SelectFx } from "@mendix/widget-plugin-grid/selection";
 import { EventEntryContext } from "./base";
+import { createFocusTargetHandlers } from "./focus-target-handlers";
 import { createItemHandlers } from "./item-handlers";
 
 export class ItemEventsController implements ItemEventsController {
     constructor(
         private contextFactory: (item: ObjectItem) => EventEntryContext,
         private selectFx: SelectFx,
-        private selectAllFx: SelectAllFx
+        private selectAllFx: SelectAllFx,
+        private focusTargetFx: FocusTargetFx
     ) {}
 
     getProps = (item: ObjectItem): ElementProps<HTMLDivElement> => {
-        return eventSwitch(() => this.contextFactory(item), [...createItemHandlers(this.selectFx, this.selectAllFx)]);
+        const entries = [
+            ...createItemHandlers(this.selectFx, this.selectAllFx),
+            ...createFocusTargetHandlers(this.focusTargetFx)
+        ];
+        return eventSwitch(() => this.contextFactory(item), entries);
     };
 }
