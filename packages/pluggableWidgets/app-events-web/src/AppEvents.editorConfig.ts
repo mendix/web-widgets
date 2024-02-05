c57165597e4c17e92108f2cb1dc55dea15488f52
gjulivan <grandjulivan@gmail.com>
2024-02-05 16:59:18 +0100

    chore(app-events): initial commit

---
+++ packages/pluggableWidgets/app-events-web/src/AppEvents.editorConfig.ts
@@ -0,0 +1,42 @@
+import { Properties } from "@mendix/pluggable-widgets-tools";
+import { AppEventsPreviewProps } from "../typings/AppEventsProps";
+
+export function getProperties(
+    _values: AppEventsPreviewProps,
+    defaultProperties: Properties /*, target: Platform*/
+): Properties {
+    // Do the values manipulation here to control the visibility of properties in Studio and Studio Pro conditionally.
+    /* Example
+    if (values.myProperty === "custom") {
+        delete defaultProperties.properties.myOtherProperty;
+    }
+    */
+    return defaultProperties;
+}
+
+// export function check(_values: NewWidgetTestPreviewProps): Problem[] {
+//     const errors: Problem[] = [];
+//     // Add errors to the above array to throw errors in Studio and Studio Pro.
+//     /* Example
+//     if (values.myProperty !== "custom") {
+//         errors.push({
+//             property: `myProperty`,
+//             message: `The value of 'myProperty' is different of 'custom'.`,
+//             url: "https://github.com/myrepo/mywidget"
+//         });
+//     }
+//     */
+//     return errors;
+// }
+
+// export function getPreview(values: NewWidgetTestPreviewProps, isDarkMode: boolean, version: number[]): PreviewProps {
+//     // Customize your pluggable widget appearance for Studio Pro.
+//     return {
+//         type: "Container",
+//         children: []
+//     }
+// }
+
+// export function getCustomCaption(values: NewWidgetTestPreviewProps, platform: Platform): string {
+//     return "NewWidgetTest";
+// }
