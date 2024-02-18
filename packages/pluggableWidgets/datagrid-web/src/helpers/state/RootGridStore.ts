079ec75de77df7d8e4a42b5ae2c71b3c0c7c0664
Roman Vyakhirev <roman.vyakhirev@mendix.com>
2024-02-18 22:33:33 +0100

    chore: set initial params of the datasource

--- packages/pluggableWidgets/datagrid-web/src/helpers/state/RootGridStore.ts
+++ packages/pluggableWidgets/datagrid-web/src/helpers/state/RootGridStore.ts
@@ -11,9 +11,19 @@ export class RootGridStore {
     settingsStore: GridSettingsStore;
 
     constructor(props: DatagridContainerProps) {
+        this.setInitParams(props);
         this.columnsStore = new ColumnsStore(props);
         this.headerFiltersStore = new HeaderFiltersStore(props);
         this.settingsStore = new GridSettingsStore(props, this.columnsStore);
+    }
+
+    private setInitParams(props: DatagridContainerProps): void {
+        if (props.pagination === "buttons") {
+            props.datasource.requestTotalCount(true);
+        }
+
+        // Set initial limit
+        props.datasource.setLimit(props.pageSize);
     }
 
     updateProps(props: DatagridContainerProps): void {
