1fadfb0755ceba732af1e67ca84818f7ac89aa98
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2024-03-26 12:40:47 +0100

    feat(datagrid-web): add improved dbl click handling

---
+++ packages/pluggableWidgets/datagrid-web/src/features/row-interaction/ClickEventSwitch.ts
@@ -0,0 +1,35 @@
+import { EventCaseEntry } from "@mendix/widget-plugin-grid/event-switch/base";
+import { groupEntries } from "@mendix/widget-plugin-grid/event-switch/utils";
+
+export type ClickEntry<Context, Element> =
+    | EventCaseEntry<Context, Element, "onClick">
+    | EventCaseEntry<Context, Element, "onDoubleClick">;
+
+export class ClickEventSwitch<Context, Element> {
+    constructor(private entries: Array<ClickEntry<Context, Element>>) {}
+
+    getClickEntry(): EventCaseEntry<Context, Element, "onClick"> {
+        const { onClick = [], onDoubleClick = [] } = groupEntries(this.entries);
+        const awaitTime = 320; // ms, approx 1/3 of a second
+        let startTime = 0;
+        return {
+            eventName: "onClick",
+            handler: (ctx, event) => {
+                if (Date.now() - startTime > awaitTime) {
+                    onClick.forEach(entry => this.runEntry(entry, ctx, event));
+                    startTime = Date.now();
+                } else {
+                    onDoubleClick.forEach(entry => this.runEntry(entry, ctx, event));
+                    startTime = 0;
+                }
+            }
+        };
+    }
+
+    private runEntry(entry: ClickEntry<Context, Element>, ctx: Context, event: React.MouseEvent<Element>): void {
+        const canRun = entry.filter?.(ctx, event) ?? true;
+        if (canRun) {
+            entry.handler(ctx, event);
+        }
+    }
+}
