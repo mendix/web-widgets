a3e08d51d7df43d987c50e88bfa2898470b4bcbf
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2024-04-26 13:37:23 +0200

    fix(line-chart-web): update @mendix/shared-charts

--- packages/pluggableWidgets/line-chart-web/src/LineChart.editorPreview.tsx
+++ packages/pluggableWidgets/line-chart-web/src/LineChart.editorPreview.tsx
@@ -1,25 +1,15 @@
+import { ChartPreview } from "@mendix/shared-charts/preview";
 import { createElement, ReactNode } from "react";
-import LineChartImage from "./assets/LineChart.light.svg";
-import LineChartLegend from "./assets/LineChart-legend.light.svg";
 import { LineChartPreviewProps } from "../typings/LineChartProps";
+import LineChartLegend from "./assets/LineChart-legend.light.svg";
+import LineChartImage from "./assets/LineChart.light.svg";
 
 export function preview(props: LineChartPreviewProps): ReactNode {
-    const { className } = props;
     return (
-        <div
-            className={className}
-            style={{
-                display: "flex",
-                width: props.showLegend ? "385px" : "300px",
-                height: "232px"
-            }}
-        >
-            <img
-                src={LineChartImage}
-                alt="line-chart-image"
-                style={{ objectFit: "contain", width: "300px", height: "100%" }}
-            />
-            {props.showLegend ? <img src={LineChartLegend} alt="line-chart-legend" style={{ width: "85px" }} /> : null}
-        </div>
+        <ChartPreview
+            {...props}
+            image={<ChartPreview.PlotImage src={LineChartImage} alt="Bubble chart" />}
+            legend={<ChartPreview.PlotLegend src={LineChartLegend} alt="Legend" />}
+        />
     );
 }
