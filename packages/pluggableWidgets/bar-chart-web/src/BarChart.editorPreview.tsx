be46f015e1d83dffa14e9614de24f2c195570d72
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2024-04-26 13:17:19 +0200

    fix(bar-chart-web): update @mendix/shared-charts

--- packages/pluggableWidgets/bar-chart-web/src/BarChart.editorPreview.tsx
+++ packages/pluggableWidgets/bar-chart-web/src/BarChart.editorPreview.tsx
@@ -1,3 +1,4 @@
+import { ChartPreview } from "@mendix/shared-charts/preview";
 import { createElement, ReactNode } from "react";
 import BarChartGrouped from "./assets/BarChart-grouped.light.svg";
 import BarChartStacked from "./assets/BarChart-stacked.light.svg";
@@ -5,23 +6,12 @@ import BarChartLegend from "./assets/BarChart-legend.light.svg";
 import { BarChartPreviewProps } from "../typings/BarChartProps";
 
 export function preview(props: BarChartPreviewProps): ReactNode {
-    const { className, barmode } = props;
-    const BarChartImage = barmode === "group" ? BarChartGrouped : BarChartStacked;
+    const BarChartImage = props.barmode === "group" ? BarChartGrouped : BarChartStacked;
     return (
-        <div
-            className={className}
-            style={{
-                display: "flex",
-                width: props.showLegend ? "385px" : "300px",
-                height: "232px"
-            }}
-        >
-            <img
-                src={BarChartImage}
-                alt="bar-chart-image"
-                style={{ objectFit: "contain", width: "300px", height: "100%" }}
-            />
-            {props.showLegend ? <img src={BarChartLegend} alt="bar-chart-legend" style={{ width: "85px" }} /> : null}
-        </div>
+        <ChartPreview
+            {...props}
+            image={<ChartPreview.PlotImage src={BarChartImage} alt="Bubble chart" />}
+            legend={<ChartPreview.PlotLegend src={BarChartLegend} alt="Legend" />}
+        />
     );
 }
