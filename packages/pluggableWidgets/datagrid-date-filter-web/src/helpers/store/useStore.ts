8723f9ecbdeb7d34941555d8d02b039b56e8f5ad
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2024-05-21 13:02:39 +0200

    refactor(datagrid-date-filter-web): redesign useStore hook

--- packages/pluggableWidgets/datagrid-date-filter-web/src/helpers/store/useStore.ts
+++ packages/pluggableWidgets/datagrid-date-filter-web/src/helpers/store/useStore.ts
@@ -1,12 +1,19 @@
-import { useReducer, useEffect } from "react";
+import { useReducer, useEffect, useMemo } from "react";
 
-export function useStore<T extends EventTarget>(store: T): T {
-    const [, forceUpdate] = useReducer(n => n + 1, 0);
+interface AbstractStore<S> extends EventTarget {
+    state: S;
+}
+
+export function useStore<S, V>(store: AbstractStore<S>, map: (state: S) => V): V;
+export function useStore<S>(store: AbstractStore<S>): AbstractStore<S>;
+export function useStore<S, V>(store: AbstractStore<S>, map?: (state: S) => V): AbstractStore<S> | V {
+    const [n, forceUpdate] = useReducer(n => n + 1, 0);
 
     useEffect(() => {
         store.addEventListener("change", forceUpdate);
         return () => store.removeEventListener("change", forceUpdate);
     }, [store]);
 
-    return store;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    return useMemo(() => (map ? map(store.state) : store), [store, n]);
 }
