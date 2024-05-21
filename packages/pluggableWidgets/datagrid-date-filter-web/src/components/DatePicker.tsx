6cd9916714c7d48668ab949484c797b832efbedb
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2024-05-21 13:08:16 +0200

    refactor(datagrid-date-filter-web): rewrite DatePicker component

--- packages/pluggableWidgets/datagrid-date-filter-web/src/components/DatePicker.tsx
+++ packages/pluggableWidgets/datagrid-date-filter-web/src/components/DatePicker.tsx
@@ -1,148 +1,96 @@
-import {
-    createElement,
-    Dispatch,
-    forwardRef,
-    Fragment,
-    MutableRefObject,
-    ReactElement,
-    SetStateAction,
-    useCallback,
-    useMemo,
-    useRef,
-    useState
-} from "react";
-import DatePickerComponent from "react-datepicker";
+import { createElement, Fragment, ReactElement, useState } from "react";
+import ReactDatePicker, { ReactDatePickerProps } from "react-datepicker";
 import classNames from "classnames";
-import { isDate, isValid } from "date-fns";
-import replaceAllInserter from "string.prototype.replaceall";
-import { doubleMonthOrDayWhenSingle, dayOfWeekWhenUpperCase } from "../utils/date-utils";
+import { pickerDateFormat, setupLocales } from "../utils/date-utils";
 import CalendarIcon from "./CalendarIcon";
-
-export type RangeDateValue = [Date | undefined, Date | undefined];
+import { DatePickerController } from "../helpers/DatePickerController";
+import { FilterStore } from "../helpers/store/FilterStore";
+import { PopupStore } from "../helpers/store/PopupStore";
+import { usePickerState } from "../helpers/usePickerState";
 
 interface DatePickerProps {
     adjustable: boolean;
-    dateFormat?: string;
-    disabledInput?: boolean;
-    enableRange?: boolean;
-    locale?: string;
-    id?: string;
+    datePickerController: DatePickerController;
+    filterStore: FilterStore;
+    parentId?: string;
     placeholder?: string;
-    setRangeValues?: Dispatch<SetStateAction<RangeDateValue>>;
-    rangeValues?: RangeDateValue;
+    popupStore: PopupStore;
     screenReaderCalendarCaption?: string;
     screenReaderInputCaption?: string;
-    setValue: Dispatch<SetStateAction<Date | undefined>>;
-    value?: Date;
-    calendarStartDay?: number;
 }
 
-replaceAllInserter.shim();
+export function DatePicker(props: DatePickerProps): ReactElement {
+    const staticProps = useSetup();
+    const { datePickerController: controller } = props;
+    const state = usePickerState(props.filterStore, props.popupStore);
 
-export const DatePicker = forwardRef(
-    (props: DatePickerProps, ref: MutableRefObject<DatePickerComponent> | null): ReactElement => {
-        const [open, setOpen] = useState(false);
-        const buttonRef = useRef<HTMLButtonElement>(null);
-        const dateFilterContainerRef = useRef<HTMLDivElement>(null);
-        const id = useMemo(() => `datepicker_` + Math.random(), []);
+    return (
+        <Fragment>
+            <div ref={controller.popoverContainerRef} id={staticProps.portalId} className="date-filter-container" />
+            <span className="sr-only" id={`${props.parentId}-label`}>
+                {props.screenReaderInputCaption}
+            </span>
+            <ReactDatePicker
+                allowSameDay={false}
+                autoFocus={false}
+                className={classNames("form-control", { "filter-input": props.adjustable })}
+                disabled={state.useRangeMode}
+                disabledKeyboardNavigation={false}
+                dropdownMode="select"
+                enableTabLoop
+                isClearable={state.useRangeMode}
+                onCalendarOpen={controller.handleCalendarOpen}
+                onChange={controller.handlePickerChange}
+                onClickOutside={controller.handlePickerOutsideClick}
+                onInputClick={controller.handlePickerInputClick}
+                placeholderText={props.placeholder}
+                preventOpenOnFocus
+                readOnly={state.useRangeMode}
+                ref={controller.pickerRef}
+                selectsRange={state.useRangeMode}
+                shouldCloseOnSelect={false}
+                showMonthDropdown
+                showPopperArrow={false}
+                showYearDropdown
+                strictParsing
+                useWeekdaysShort={false}
+                startDate={state.startDate}
+                endDate={state.endDate}
+                selected={state.selected}
+                open={state.open}
+            />
+            <button
+                aria-controls={staticProps.portalId}
+                aria-expanded={state.open}
+                aria-haspopup
+                aria-label={props.screenReaderCalendarCaption ?? "Show calendar"}
+                ref={controller.buttonRef}
+                className="btn btn-default btn-calendar"
+                onClick={controller.handleButtonClick}
+                onKeyDown={controller.handleButtonKeyDown}
+            >
+                <CalendarIcon />
+            </button>
+        </Fragment>
+    );
+}
 
-        const buttonClick = useCallback(() => {
-            setOpen(open => !open);
-            setTimeout(() => {
-                (dateFilterContainerRef.current?.querySelector(".react-datepicker > button") as HTMLElement)?.focus();
-            }, 10);
-        }, []);
+type StaticProps = Omit<ReactDatePickerProps, "onChange">;
 
-        let dateFormats;
-        if (props.dateFormat) {
-            // Replace with full patterns d -> dd, M -> MM
-            let fixedFormatString = doubleMonthOrDayWhenSingle(props.dateFormat);
-            // Replace Date format E to follow unicode standard (e...eeee)
-            fixedFormatString = dayOfWeekWhenUpperCase(fixedFormatString);
-            dateFormats =
-                props.dateFormat !== fixedFormatString ? [fixedFormatString, props.dateFormat] : [props.dateFormat];
-        }
+function useSetup(): StaticProps {
+    const [props] = useState<StaticProps>(() => {
+        const { locale } = window.mx.session.getConfig();
+        return {
+            calendarStartDay: locale.firstDayOfWeek,
+            dateFormat: pickerDateFormat(locale),
+            locale: setupLocales(locale),
+            popperPlacement: "bottom-end",
+            popperProps: {
+                strategy: "fixed"
+            },
+            portalId: `datepicker_` + Math.random()
+        };
+    });
 
-        return (
-            <Fragment>
-                <div ref={dateFilterContainerRef} id={id} className="date-filter-container" />
-                <span className="sr-only" id={`${props.id}-label`}>
-                    {props.screenReaderInputCaption}
-                </span>
-                <DatePickerComponent
-                    allowSameDay={false}
-                    ariaLabelledBy={`${props.id}-label`}
-                    autoFocus={false}
-                    calendarStartDay={props.calendarStartDay}
-                    className={classNames("form-control", { "filter-input": props.adjustable })}
-                    dateFormat={dateFormats}
-                    disabled={props.disabledInput ?? false}
-                    disabledKeyboardNavigation={false}
-                    dropdownMode="select"
-                    enableTabLoop
-                    endDate={props.enableRange ? props.rangeValues?.[1] : undefined}
-                    isClearable={props.enableRange}
-                    locale={props.locale}
-                    onChange={date => {
-                        if (Array.isArray(date)) {
-                            const [startDate, endDate] = date;
-                            props.setRangeValues?.([startDate ?? undefined, endDate ?? undefined]);
-                            props.setValue(undefined);
-                        } else {
-                            if (isDate(date) && isValid(date)) {
-                                props.setValue(date as Date);
-                            } else {
-                                props.setValue(undefined);
-                            }
-                            props.setRangeValues?.([undefined, undefined]);
-                        }
-                    }}
-                    onClickOutside={event => {
-                        if (!buttonRef.current || buttonRef.current.contains(event.target as Node)) {
-                            return;
-                        }
-                        setOpen(false);
-                    }}
-                    open={open}
-                    onInputClick={() => setOpen(true)}
-                    placeholderText={props.placeholder}
-                    popperPlacement="bottom-end"
-                    popperProps={{
-                        strategy: "fixed"
-                    }}
-                    portalId={id}
-                    preventOpenOnFocus
-                    readOnly={props.enableRange}
-                    ref={ref}
-                    startDate={props.enableRange ? props.rangeValues?.[0] : undefined}
-                    selected={props.enableRange ? undefined : props.value}
-                    selectsRange={props.enableRange}
-                    shouldCloseOnSelect={false}
-                    showMonthDropdown
-                    showPopperArrow={false}
-                    showYearDropdown
-                    strictParsing
-                    useWeekdaysShort={false}
-                />
-                <button
-                    aria-controls={id}
-                    aria-expanded={open}
-                    aria-haspopup
-                    aria-label={props.screenReaderCalendarCaption ?? "Show calendar"}
-                    ref={buttonRef}
-                    className="btn btn-default btn-calendar"
-                    onClick={buttonClick}
-                    onKeyDown={e => {
-                        if (e.key === "Enter" || e.key === " ") {
-                            e.preventDefault();
-                            e.stopPropagation();
-                            buttonClick();
-                        }
-                    }}
-                >
-                    <CalendarIcon />
-                </button>
-            </Fragment>
-        );
-    }
-);
+    return props;
+}
