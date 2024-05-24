a0a9ee510b5677a6337e482beef04a43a8242b43
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2024-05-24 13:46:43 +0200

    refactor(datagrid-date-filter-web): make it easer to reset value in range mode

--- packages/pluggableWidgets/datagrid-date-filter-web/src/helpers/store/CalendarStore.ts
+++ packages/pluggableWidgets/datagrid-date-filter-web/src/helpers/store/CalendarStore.ts
@@ -1,15 +1,16 @@
-/**
- * DON'T USE THIS STORE TO CONTROL THE CALENDAR STATE.
- * Do not use this store to control calendar state in react-datepicker.
- * This store exists only to synchronize the aria-expanded attribute on the button.
- * react-datepicker doesn't support controlled mode. So, if you think
- * that you are clever and can take control of the calendar - good luck and have fun.
- */
 export class CalendarStore extends EventTarget {
     state = {
         expanded: false
     };
 
+    /**
+     * DON'T USE THIS STORE TO CONTROL THE CALENDAR STATE.
+     * @remark
+     * Do not use this store to control calendar state in react-datepicker.
+     * This store exists only to synchronize the aria-expanded attribute on the button.
+     * react-datepicker doesn't support controlled mode. So, if you think
+     * that you are clever and can take control of the calendar - good luck and have fun.
+     */
     UNSAFE_setExpanded(expanded: boolean): void {
         if (this.state.expanded === expanded) {
             return;
