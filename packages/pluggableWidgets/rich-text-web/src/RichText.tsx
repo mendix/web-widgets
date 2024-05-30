437da0ef5e95fec52dfd6ba21b4ab3de8ffc56ab
Grand Julivan <grandjulivan@gmail.com>
2024-05-30 10:39:47 +0200

    fix: dojo client runtime iframe load issue and optimize bundle

--- packages/pluggableWidgets/rich-text-web/src/RichText.tsx
+++ packages/pluggableWidgets/rich-text-web/src/RichText.tsx
@@ -1,16 +1,13 @@
 import { ValidationAlert } from "@mendix/widget-plugin-component-kit/Alert";
 import { getDimensions } from "@mendix/widget-plugin-platform/utils/get-dimensions";
 import classNames from "classnames";
-import { createElement, Fragment } from "react";
+import { createElement, Fragment, useState, useEffect } from "react";
 import { RichTextContainerProps } from "../typings/RichTextProps";
 import BundledEditor from "./components/Editor";
 import "./ui/RichText.scss";
 import { constructWrapperStyle } from "./utils/helpers";
 import { createMenubar } from "./utils/menubar";
 import { createPreset } from "./utils/presets";
-// Content styles, including inline UI like fake cursors
-import "tinymce/skins/content/default/content";
-import "tinymce/skins/ui/oxide/content";
 
 export default function RichText(props: RichTextContainerProps): JSX.Element {
     const {
@@ -38,6 +35,31 @@ export default function RichText(props: RichTextContainerProps): JSX.Element {
     const presets = createPreset(preset, props);
     const menubar = createMenubar(menubarMode, props);
     const wrapperStyle = constructWrapperStyle(props, { width, height });
+    const [isIncubator, setIsIncubator] = useState(true);
+
+    useEffect(() => {
+        // this is a fix for dojo runtime rendering
+        // in dojo runtime, DOM is rendered inside <div class="mx-incubator mx-offscreen"> at the inital stage
+        // and moved to content once it fully loads, which cause rich text editor looses reference to it's iframe
+        // this fix waits for it to be fully out of incubator div, then only fully renders rich text afterwards.
+        const observer = new MutationObserver(_mutationList => {
+            if (!document.querySelector(`.mx-incubator.mx-offscreen`)?.childElementCount) {
+                setIsIncubator(false);
+            }
+        });
+        const observedIncubator = document.querySelector(`.mx-incubator.mx-offscreen`);
+        if (observedIncubator && observedIncubator.childElementCount) {
+            observer.observe(observedIncubator, {
+                childList: true
+            });
+        } else {
+            setIsIncubator(false);
+        }
+
+        return () => {
+            observer.disconnect();
+        };
+    }, []);
     return (
         <Fragment>
             <div
@@ -54,15 +76,19 @@ export default function RichText(props: RichTextContainerProps): JSX.Element {
                 style={wrapperStyle}
                 {...wrapperAttributes}
             >
-                <BundledEditor
-                    {...props}
-                    menubar={menubar}
-                    toolbar={presets.toolbar}
-                    editorHeight={height}
-                    editorWidth={width}
-                    key={`${String(stringAttribute.readOnly)}_${id}_${props.content_css?.value}`}
-                    resize={enableStatusBar ? resize : "false"}
-                />
+                {stringAttribute.status === "loading" || stringAttribute.status !== "available" || isIncubator ? (
+                    <div className="mx-progress"></div>
+                ) : (
+                    <BundledEditor
+                        {...props}
+                        menubar={menubar}
+                        toolbar={presets.toolbar}
+                        editorHeight={height}
+                        editorWidth={width}
+                        key={`${String(stringAttribute.readOnly)}_${id}_${props.content_css?.value}`}
+                        resize={enableStatusBar ? resize : "false"}
+                    />
+                )}
             </div>
             <ValidationAlert>{stringAttribute.validation}</ValidationAlert>
         </Fragment>
