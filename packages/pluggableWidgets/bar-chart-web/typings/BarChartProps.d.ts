cc2cb8d0a2a8d12b5c15bd94ed27bf2948a107fd
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2024-06-04 16:32:48 +0200

    feat(bar-chart-web): add structure preview

--- packages/pluggableWidgets/bar-chart-web/typings/BarChartProps.d.ts
+++ packages/pluggableWidgets/bar-chart-web/typings/BarChartProps.d.ts
@@ -68,6 +68,7 @@ export interface BarChartContainerProps {
     tabIndex?: number;
     series: SeriesType[];
     enableAdvancedOptions: boolean;
+    showPlaygroundSlot: boolean;
     playground?: ReactNode;
     xAxisLabel?: DynamicValue<string>;
     yAxisLabel?: DynamicValue<string>;
@@ -94,6 +95,7 @@ export interface BarChartPreviewProps {
     readOnly: boolean;
     series: SeriesPreviewType[];
     enableAdvancedOptions: boolean;
+    showPlaygroundSlot: boolean;
     playground: { widgetCount: number; renderer: ComponentType<{ children: ReactNode; caption?: string }> };
     xAxisLabel: string;
     yAxisLabel: string;
