9a6c086b60b4e9d7bfdeda37d119737035456e68
Samuel Reichert <samuelreichertt@gmail.com>
2024-06-10 09:45:07 +0200

    chore(combobox-web): refactor selectors to create a Base Selector

--- packages/pluggableWidgets/combobox-web/src/helpers/Database/DatabaseSingleSelectionSelector.ts
+++ packages/pluggableWidgets/combobox-web/src/helpers/Database/DatabaseSingleSelectionSelector.ts
@@ -1,58 +1,18 @@
-import { executeAction } from "@mendix/widget-plugin-platform/framework/execute-action";
-import { ActionValue, EditableValue, ObjectItem, SelectionSingleValue, ValueStatus } from "mendix";
-import { ComboboxContainerProps, OptionsSourceAssociationCustomContentTypeEnum } from "../../../typings/ComboboxProps";
-import { SingleSelector, Status } from "../types";
-import { DEFAULT_LIMIT_SIZE } from "../utils";
-import { DatabaseCaptionsProvider } from "./DatabaseCaptionsProvider";
-import { DatabaseOptionsProvider } from "./DatabaseOptionsProvider";
+import { SelectionSingleValue } from "mendix";
+import { ComboboxContainerProps } from "../../../typings/ComboboxProps";
+import { BaseDatabaseSingleSelector } from "./BaseDatabaseSingleSelector";
 import { extractDatabaseProps } from "./utils";
 
-export class DatabaseSingleSelectionSelector<T extends string | Big, R extends EditableValue<T>>
-    implements SingleSelector
-{
-    type = "single" as const;
-    status: Status = "unavailable";
-    options: DatabaseOptionsProvider;
-    clearable = false;
-    currentId: string | null = null;
-    lastSetValue: T | null | undefined = null;
-    caption: DatabaseCaptionsProvider;
-    readOnly = false;
-    lazyLoading = false;
-    customContentType: OptionsSourceAssociationCustomContentTypeEnum = "no";
-    validation?: string = undefined;
+export class DatabaseSingleSelectionSelector<T extends string | Big> extends BaseDatabaseSingleSelector<T> {
     selection?: SelectionSingleValue;
-    protected _attr: R | undefined;
-    private onChangeEvent?: ActionValue;
-    private _objectsMap: Map<string, ObjectItem> = new Map();
-    private limit: number = DEFAULT_LIMIT_SIZE;
-
-    constructor() {
-        this.caption = new DatabaseCaptionsProvider(this._objectsMap);
-        this.options = new DatabaseOptionsProvider(this.caption, this._objectsMap);
-    }
 
     updateProps(props: ComboboxContainerProps): void {
-        const [
-            _attr,
-            ds,
-            captionProvider,
-            emptyOption,
-            clearable,
-            filterType,
-            onChangeEvent,
-            customContent,
-            customContentType,
-            valueAttribute,
-            _emptyValue,
-            lazyLoading
-        ] = extractDatabaseProps(props);
+        super.updateProps(props);
 
-        const newLimit = this.newLimit(ds.limit, false, ds.status, lazyLoading);
-        if (newLimit !== ds.limit) {
-            ds.setLimit(newLimit);
-        }
+        const { captionProvider, customContent, customContentType, ds, emptyOption, lazyLoading, valueAttribute } =
+            extractDatabaseProps(props);
 
+        this.lazyLoader.setLimit(this.lazyLoader.getLimit(ds.limit, false, ds.status, lazyLoading));
         this.caption.updateProps({
             emptyOptionText: emptyOption,
             formattingAttributeOrExpression: captionProvider,
@@ -61,10 +21,6 @@ export class DatabaseSingleSelectionSelector<T extends string | Big, R extends E
             attribute: valueAttribute
         });
 
-        this.options._updateProps({
-            ds,
-            filterType
-        });
         if (!ds || ds.status === "unavailable" || !emptyOption || emptyOption.status === "unavailable") {
             this.status = "unavailable";
             this.currentId = null;
@@ -72,38 +28,12 @@ export class DatabaseSingleSelectionSelector<T extends string | Big, R extends E
             return;
         }
 
-        this.clearable = clearable;
-        this.status = ds.status;
-        // this.readOnly = ;
-        this.onChangeEvent = onChangeEvent;
-        this.customContentType = customContentType;
-        // this.validation = attr.validation;
-        this.lazyLoading = lazyLoading;
         this.selection = props.optionsSourceDatabaseItemSelection as SelectionSingleValue;
+        this.status = ds.status;
     }
 
     setValue(objectId: string | null): void {
         this.selection?.setSelection(this.options._optionToValue(objectId));
-
-        this.currentId = objectId;
-        executeAction(this.onChangeEvent);
-    }
-
-    private newLimit(limit: number, readOnly: boolean, status: ValueStatus, lazyLoading: boolean): number | undefined {
-        if (status !== "available" || readOnly === true) {
-            return 0;
-        }
-
-        if (lazyLoading) {
-            if (limit < this.limit) {
-                return this.limit;
-            }
-            if (limit > this.limit) {
-                this.limit = limit;
-            }
-            return limit;
-        }
-
-        return undefined;
+        super.setValue(objectId);
     }
 }
