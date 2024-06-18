b2f74fd1b6f5b1c1bed9783f017b2e278db0ce2b
Samuel Reichert <samuelreichertt@gmail.com>
2024-06-18 10:44:04 +0200

    feat(combobox-web): remove isLoading logic from useLazyLoading

--- packages/pluggableWidgets/combobox-web/src/hooks/useLazyLoading.ts
+++ packages/pluggableWidgets/combobox-web/src/hooks/useLazyLoading.ts
@@ -1,26 +1,20 @@
 import { useCallback, useEffect, useState } from "react";
 import { InfiniteBodyProps, useInfiniteControl } from "@mendix/widget-plugin-grid/components/InfiniteBody";
-import { Status } from "../helpers/types";
 
 type UseLazyLoadingProps = Pick<InfiniteBodyProps, "hasMoreItems" | "isInfinite"> & {
     isOpen: boolean;
     loadMore?: () => void;
-    searchTerm: string;
-    status: Status;
 };
 
 type UseLazyLoadingReturn = {
-    isLoading: boolean;
     onScroll: (e: any) => void;
 };
 
 export function useLazyLoading(props: UseLazyLoadingProps): UseLazyLoadingReturn {
-    const { hasMoreItems, isInfinite, isOpen, loadMore, searchTerm, status } = props;
-    const [isLoading, setIsLoading] = useState(false);
+    const { hasMoreItems, isInfinite, isOpen, loadMore } = props;
     const [firstLoad, setFirstLoad] = useState(false);
     const setPageCallback = useCallback(() => {
         if (loadMore) {
-            setIsLoading(true);
             loadMore();
         }
     }, [loadMore]);
@@ -34,15 +28,5 @@ export function useLazyLoading(props: UseLazyLoadingProps): UseLazyLoadingReturn
         }
     }, [firstLoad, isInfinite, isOpen, setPageCallback]);
 
-    useEffect(() => {
-        setIsLoading(true);
-    }, [searchTerm]);
-
-    useEffect(() => {
-        if (status !== "loading") {
-            setIsLoading(false);
-        }
-    }, [status]);
-
-    return { isLoading, onScroll: trackScrolling };
+    return { onScroll: trackScrolling };
 }
