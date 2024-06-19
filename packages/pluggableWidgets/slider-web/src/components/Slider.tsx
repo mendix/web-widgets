ed772e0cf0a5838eaca5f18418a888e1b2a4e976
Rahman Unver <rahmanunver@gmail.com>
2024-06-19 15:19:01 +0200

    fix(slider-web): use refObject to use as container for the tooltip

--- packages/pluggableWidgets/slider-web/src/components/Slider.tsx
+++ packages/pluggableWidgets/slider-web/src/components/Slider.tsx
@@ -1,23 +1,29 @@
-import { createElement, CSSProperties } from "react";
+import { createElement, CSSProperties, forwardRef, RefObject } from "react";
 import RcSlider, { SliderProps as RcSliderProps } from "rc-slider";
 import classNames from "classnames";
 
 export interface SliderProps extends RcSliderProps {
     classNameSlider?: string;
     rootStyle?: CSSProperties;
+    sliderRef?: RefObject<HTMLDivElement>;
 }
 
-export const Slider = ({ className, classNameSlider, rootStyle, ...rcSliderProps }: SliderProps): JSX.Element => (
-    <div
-        style={rootStyle}
-        className={classNames(
-            "widget-slider",
-            {
-                "widget-slider-vertical": rcSliderProps.vertical
-            },
-            className
-        )}
-    >
-        <RcSlider className={classNameSlider} {...rcSliderProps} />
-    </div>
+export const Slider = forwardRef(
+    ({ className, classNameSlider, rootStyle, sliderRef, ...rcSliderProps }: SliderProps): JSX.Element => {
+        return (
+            <div
+                ref={sliderRef}
+                style={rootStyle}
+                className={classNames(
+                    "widget-slider",
+                    {
+                        "widget-slider-vertical": rcSliderProps.vertical
+                    },
+                    className
+                )}
+            >
+                <RcSlider className={classNameSlider} {...rcSliderProps} />
+            </div>
+        );
+    }
 );
