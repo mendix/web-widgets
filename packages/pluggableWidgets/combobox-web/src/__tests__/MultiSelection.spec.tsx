9cd51efe1763956f9029aa647c5ab9f1ab626cff
Samuel Reichert <samuelreichertt@gmail.com>
2024-06-21 11:46:36 +0200

    test(combobox-web): fix failing unit tests

--- packages/pluggableWidgets/combobox-web/src/__tests__/MultiSelection.spec.tsx
+++ packages/pluggableWidgets/combobox-web/src/__tests__/MultiSelection.spec.tsx
@@ -40,6 +40,7 @@ describe("Combo box (Association)", () => {
             optionsSourceAssociationCaptionExpression: listExp(() => "$currentObject/CountryName"),
             optionsSourceAssociationCustomContentType: "no",
             optionsSourceAssociationCustomContent: undefined,
+            optionsSourceDatabaseUsageType: "attribute",
             emptyOptionText: dynamicValue("Select an option 111"),
             ariaRequired: true,
             clearable: true,
