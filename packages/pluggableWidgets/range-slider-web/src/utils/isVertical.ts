96fe526e988808a02196eb980b63d5e83c7ae0c8
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2024-07-04 15:26:02 +0200

    fix(range-slider-web): add loading state

--- packages/pluggableWidgets/range-slider-web/src/utils/isVertical.ts
+++
@@ -1,5 +0,0 @@
-import { RangeSliderContainerProps } from "../../typings/RangeSliderProps";
-
-export function isVertical(params: Pick<RangeSliderContainerProps, "orientation">): boolean {
-    return params.orientation === "vertical";
-}
