321eaee5b3ad94ff3ab2ed46fb584267cf050bf5
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2024-07-12 16:05:06 +0200

    feat(widget-plugin-filtering): add basic sync hook

--- packages/pluggableWidgets/datagrid-text-filter-web/src/DatagridTextFilter.tsx
+++ packages/pluggableWidgets/datagrid-text-filter-web/src/DatagridTextFilter.tsx
@@ -1,12 +1,14 @@
 import * as mobx from "mobx";
 import { observer } from "mobx-react-lite";
 import { getGlobalFilterContextObject, isStringFilter } from "@mendix/widget-plugin-filtering";
-import { createElement, ReactElement, useRef, useContext, Fragment } from "react";
+import { createElement, ReactElement, useRef, useContext } from "react";
 import { DatagridTextFilterContainerProps } from "../typings/DatagridTextFilterProps";
 import { FilterComponent } from "./components/FilterComponent";
 
 import { generateUUID } from "@mendix/widget-plugin-platform/framework/generate-uuid";
+import { withPreloader } from "@mendix/widget-plugin-platform/hoc/withPreloader";
 import { String_InputFilterInterface } from "@mendix/widget-plugin-filtering/dist/stores/typings/InputFilterInterface";
+import { useBasicSync } from "@mendix/widget-plugin-filtering/controls";
 
 (document as any)._mobx = mobx;
 
@@ -17,14 +19,13 @@ type FilterAPI = {
 
 const filterContext = getGlobalFilterContextObject();
 
-const Widget = withFilterAPI(withPreloader(observer(ReactiveDatagridTextFilter)));
-
-export default function DatagridTextFilter(props: DatagridTextFilterContainerProps): ReactElement {
-    return <Widget {...props} />;
-}
+const Widget = withFilterAPI(withPreloader(observer(ReactiveDatagridTextFilter), isLoadingDefaultValues));
 
 function ReactiveDatagridTextFilter(props: DatagridTextFilterContainerProps & FilterAPI): ReactElement {
     const id = (useRef<string>().current ??= `TextFilter${generateUUID()}`);
+
+    useBasicSync(props, props.filterStore);
+
     return (
         <FilterComponent
             adjustable={props.adjustable}
@@ -58,16 +59,11 @@ function withFilterAPI<T>(Component: (props: T & FilterAPI) => ReactElement): (p
     };
 }
 
-function withPreloader<P extends DatagridTextFilterContainerProps>(
-    Component: (props: P) => React.ReactElement
-): (props: P) => React.ReactElement {
-    return function Preloader(props) {
-        const isLoaded = (useRef(false).current ||= !isLoadingDefaultValues(props));
-        return isLoaded ? <Component {...props} /> : <Fragment />;
-    };
-}
-
 function isLoadingDefaultValues(props: DatagridTextFilterContainerProps): boolean {
     const statusList = [props.defaultValue?.status];
     return statusList.some(status => status === "loading");
+}
+
+export default function DatagridTextFilter(props: DatagridTextFilterContainerProps): ReactElement {
+    return <Widget {...props} />;
 }
