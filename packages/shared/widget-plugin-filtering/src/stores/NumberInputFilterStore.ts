e7fee43b67ae4ae6261484e06fc50bb5656165b3
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2024-07-16 14:45:20 +0200

    refactor(widget-plugin-filtering): split input filters to separate files

---
+++ packages/shared/widget-plugin-filtering/src/stores/NumberInputFilterStore.ts
@@ -0,0 +1,40 @@
+import { Big } from "big.js";
+import { ListAttributeValue } from "mendix";
+import { action, makeObservable } from "mobx";
+import { NumberArgument } from "./Argument";
+import { BaseInputFilterStore } from "./BaseInputFilterStore";
+import { FilterFunctionBinary, FilterFunctionGeneric, FilterFunctionNonValue } from "./typings/FilterFunctions";
+import { Number_InputFilterInterface } from "./typings/InputFilterInterface";
+
+export class NumberInputFilterStore
+    extends BaseInputFilterStore<
+        NumberArgument,
+        FilterFunctionGeneric | FilterFunctionNonValue | FilterFunctionBinary,
+        Big
+    >
+    implements Number_InputFilterInterface
+{
+    readonly storeType = "input";
+    readonly type = "number";
+
+    constructor(attributes: Array<ListAttributeValue<Big>>) {
+        const { formatter } = attributes[0];
+        super(new NumberArgument(formatter), new NumberArgument(formatter), "equal", attributes);
+        makeObservable(this, {
+            updateProps: action
+        });
+        // todo restore operation and value from config
+    }
+
+    updateProps(attributes: ListAttributeValue[]): void {
+        this._attributes = attributes;
+        const formatter = attributes.at(0)?.formatter;
+        // Just pleasing TypeScript.
+        if (formatter?.type !== "number") {
+            console.error("InputFilterStore: encounter invalid attribute type while updating props.");
+            return;
+        }
+        this.arg1.updateProps(formatter);
+        this.arg2.updateProps(formatter);
+    }
+}
---
+++ packages/shared/widget-plugin-filtering/src/stores/StringInputFilterStore.ts
@@ -0,0 +1,44 @@
+import { ListAttributeValue } from "mendix";
+import { action, makeObservable } from "mobx";
+import { StringArgument } from "./Argument";
+import { BaseInputFilterStore } from "./BaseInputFilterStore";
+import { String_InputFilterInterface } from "./main";
+import {
+    FilterFunctionBinary,
+    FilterFunctionGeneric,
+    FilterFunctionNonValue,
+    FilterFunctionString
+} from "./typings/FilterFunctions";
+
+export class StringInputFilterStore
+    extends BaseInputFilterStore<
+        StringArgument,
+        FilterFunctionString | FilterFunctionGeneric | FilterFunctionNonValue | FilterFunctionBinary,
+        string
+    >
+    implements String_InputFilterInterface
+{
+    readonly storeType = "input";
+    readonly type = "string";
+
+    constructor(attributes: Array<ListAttributeValue<string>>) {
+        const { formatter } = attributes[0];
+        super(new StringArgument(formatter), new StringArgument(formatter), "equal", attributes);
+        makeObservable(this, {
+            updateProps: action
+        });
+        // todo restore operation and value from config
+    }
+
+    updateProps(attributes: ListAttributeValue[]): void {
+        this._attributes = attributes;
+        const formatter = attributes.at(0)?.formatter;
+        // Just pleasing TypeScript.
+        if (!formatter || formatter.type === "number" || formatter.type === "datetime") {
+            console.error("InputFilterStore: encounter invalid attribute type while updating props.");
+            return;
+        }
+        this.arg1.updateProps(formatter as ListAttributeValue<string>["formatter"]);
+        this.arg2.updateProps(formatter as ListAttributeValue<string>["formatter"]);
+    }
+}
