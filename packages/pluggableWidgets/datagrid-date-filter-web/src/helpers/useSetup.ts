464047ca98945c07bfe1ede3bc19fcced94a745a
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2024-07-16 14:20:44 +0200

    refactor(datagrid-date-filter-web): set defaults on mount

--- packages/pluggableWidgets/datagrid-date-filter-web/src/helpers/useSetup.ts
+++ packages/pluggableWidgets/datagrid-date-filter-web/src/helpers/useSetup.ts
@@ -1,13 +1,18 @@
-import { useState, useMemo } from "react";
+import { useState, useMemo, useEffect } from "react";
 import { Date_InputFilterInterface } from "@mendix/widget-plugin-filtering";
 import { CalendarStore } from "./CalendarStore";
 import { DatePickerController } from "../helpers/DatePickerController";
 import { generateUUID } from "@mendix/widget-plugin-platform/framework/generate-uuid";
 import { Locale } from "date-fns";
 import { getLocale, pickerDateFormat, setupLocales } from "../utils/date-utils";
+import { FilterTypeEnum } from "./base-types";
 
 interface SetupProps {
     filterStore: Date_InputFilterInterface;
+    defaultFilter: FilterTypeEnum;
+    defaultValue?: Date;
+    defaultStartValue?: Date;
+    defaultEndValue?: Date;
 }
 
 type SetupResult = {
@@ -22,6 +27,16 @@ type SetupResult = {
 export function useSetup(props: SetupProps): SetupResult {
     const [calendarStore] = useState(() => new CalendarStore());
     const [datePickerController] = useState(() => new DatePickerController(props.filterStore, calendarStore));
+
+    // Set default state for the filter.
+    useEffect(() => {
+        if (props.defaultFilter === "between") {
+            props.filterStore.UNSAFE_setDefaults(["between", props.defaultStartValue, props.defaultEndValue]);
+        } else {
+            props.filterStore.UNSAFE_setDefaults([props.defaultFilter, props.defaultValue]);
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
     return useMemo(() => {
         const locale = getLocale();
--- packages/pluggableWidgets/datagrid-date-filter-web/src/utils/widget-utils.ts
+++ packages/pluggableWidgets/datagrid-date-filter-web/src/utils/widget-utils.ts
@@ -1,49 +1,6 @@
-import { parseStyle } from "@mendix/widget-plugin-platform/preview/parse-style";
-import { DynamicValue } from "mendix";
-import {
-    DatagridDateFilterContainerProps,
-    DatagridDateFilterPreviewProps
-} from "../../typings/DatagridDateFilterProps";
+import { DatagridDateFilterContainerProps } from "../../typings/DatagridDateFilterProps";
 
 export function isLoadingDefaultValues(props: DatagridDateFilterContainerProps): boolean {
     const statusList = [props.defaultValue?.status, props.defaultStartDate?.status, props.defaultEndDate?.status];
     return statusList.some(status => status === "loading");
-}
-
-type UnknownProps = DatagridDateFilterContainerProps | DatagridDateFilterPreviewProps;
-
-export function isPreviewProps(props: UnknownProps): props is DatagridDateFilterPreviewProps {
-    let isPreview = typeof props.style === "string";
-    isPreview ||= typeof props.defaultValue === "string";
-    isPreview ||= typeof props.valueAttribute === "string";
-    return isPreview;
-}
-
-export function normalizeProps(props: UnknownProps): DatagridDateFilterContainerProps {
-    const wrap = (v: string): DynamicValue<string> => ({ value: v, status: "available" } as DynamicValue<string>);
-
-    if (isPreviewProps(props)) {
-        return {
-            name: "DateFilter",
-            class: props.class,
-            style: parseStyle(props.style),
-            tabIndex: undefined,
-            advanced: props.advanced,
-            defaultValue: undefined,
-            defaultStartDate: undefined,
-            defaultEndDate: undefined,
-            defaultFilter: props.defaultFilter,
-            placeholder: wrap(props.placeholder),
-            adjustable: props.adjustable,
-            valueAttribute: undefined,
-            startDateAttribute: undefined,
-            endDateAttribute: undefined,
-            onChange: undefined,
-            screenReaderButtonCaption: wrap(props.screenReaderButtonCaption),
-            screenReaderCalendarCaption: wrap(props.screenReaderCalendarCaption),
-            screenReaderInputCaption: wrap(props.screenReaderInputCaption)
-        };
-    }
-
-    return props;
 }
