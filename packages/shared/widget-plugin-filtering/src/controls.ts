e99e95cff09477ca45a85183370843963bd8445e
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2024-07-16 14:35:49 +0200

    refactor(datagrid-date-filter-web): add value backup hook

--- packages/shared/widget-plugin-filtering/src/controls.ts
+++ packages/shared/widget-plugin-filtering/src/controls.ts
@@ -1,5 +1,4 @@
-export { InputWithFilters } from "./controls/input/InputWithFilters";
 export * from "./controls/input/typings";
+export { InputWithFilters } from "./controls/input/InputWithFilters";
 export { useInputProps } from "./controls/input/useInputProps";
-export { useBasicSync } from "./controls/input/useBasicSync";
 export { InputStore } from "./controls/input/InputStore";
--- packages/shared/widget-plugin-filtering/src/main.ts
+++ packages/shared/widget-plugin-filtering/src/main.ts
@@ -2,6 +2,8 @@ export * from "./provider.js";
 export { error, value } from "./result-meta.js";
 export { readInitFilterValues, unwrapAndExpression } from "./read-init-props.js";
 export { useDefaultValue } from "./useDefaultValue";
+export { useBasicSync } from "./helpers/useBasicSync.js";
+export { useDateSync } from "./helpers/useDateSync.js";
 export * from "./stores/main.js";
 export type { Result, Success, Error } from "./result-meta.js";
 export type { InitialFilterValue, BinaryExpression } from "./read-init-props.js";
