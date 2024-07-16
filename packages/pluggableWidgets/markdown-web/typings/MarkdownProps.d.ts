6d0cdda492aa958dc6a94d54f0031ae92d9013ac
Rahman Unver <rahmanunver@gmail.com>
2024-07-16 18:32:02 +0200

    feat(markdown-web): revert spellcheck and events

--- packages/pluggableWidgets/markdown-web/typings/MarkdownProps.d.ts
+++ packages/pluggableWidgets/markdown-web/typings/MarkdownProps.d.ts
@@ -3,30 +3,18 @@
  * WARNING: All changes made to this file will be overwritten
  * @author Mendix Widgets Framework Team
  */
-import { ActionValue, EditableValue } from "mendix";
-
-export type ReadOnlyStyleEnum = "bordered" | "text";
+import { EditableValue } from "mendix";
 
 export interface MarkdownContainerProps {
     name: string;
     tabIndex?: number;
     id: string;
     stringAttribute: EditableValue<string>;
-    spellcheck: boolean;
     showFooter: boolean;
-    readOnlyStyle: ReadOnlyStyleEnum;
-    onChangeEvent?: ActionValue;
-    onEnterEvent?: ActionValue;
-    onLeaveEvent?: ActionValue;
 }
 
 export interface MarkdownPreviewProps {
     readOnly: boolean;
     stringAttribute: string;
-    spellcheck: boolean;
     showFooter: boolean;
-    readOnlyStyle: ReadOnlyStyleEnum;
-    onChangeEvent: {} | null;
-    onEnterEvent: {} | null;
-    onLeaveEvent: {} | null;
 }
