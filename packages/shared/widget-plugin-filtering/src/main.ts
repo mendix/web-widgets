ccd2132d7632af2ec4db720550a966d31c7fbaa3
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2024-07-18 12:08:45 +0200

    feat(datagrid-dropdown-filter-web): start apiv2 migration

--- packages/shared/widget-plugin-filtering/src/main.ts
+++ packages/shared/widget-plugin-filtering/src/main.ts
@@ -9,6 +9,7 @@ export type { Result, Success, Error } from "./result-meta.js";
 export type { InitialFilterValue, BinaryExpression } from "./read-init-props.js";
 export type { Option, OptionListFilterInterface } from "./typings/OptionListFilterInterface.js";
 export type { InputFilterInterface } from "./typings/InputFilterInterface.js";
+export { StaticFilterController } from "./controllers/StaticFilterController.js";
 
 import * as mobx from "mobx";
 (document as any).mobx ??= mobx;
