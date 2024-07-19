0beb3ad0d48c065bbde5ffe308e0c76fabd4e559
Rahman Unver <rahmanunver@gmail.com>
2024-07-19 15:22:50 +0200

    feat(markdown-web): styling for preview

--- packages/pluggableWidgets/markdown-web/src/Markdown.editorPreview.tsx
+++ packages/pluggableWidgets/markdown-web/src/Markdown.editorPreview.tsx
@@ -6,9 +6,7 @@ export const preview = (props: MarkdownPreviewProps): ReactElement => {
     const { stringAttribute } = props;
     return (
         <div className="widget-markdown preview">
-            <div className="widget-markdown-content">
-                {stringAttribute ? `[${stringAttribute}]` : "[No attribute selected]"}
-            </div>
+            {stringAttribute ? `[${stringAttribute}]` : "[No attribute selected]"}
         </div>
     );
 };
