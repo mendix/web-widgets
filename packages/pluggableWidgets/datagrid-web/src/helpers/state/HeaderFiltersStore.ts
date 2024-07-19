4b96f7b7a72e3e5f8d40bc135d87f94e3c510ec4
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2024-07-19 11:16:42 +0200

    feat: migrate legacy filters to mobx

--- packages/pluggableWidgets/datagrid-web/src/helpers/state/HeaderFiltersStore.ts
+++ packages/pluggableWidgets/datagrid-web/src/helpers/state/HeaderFiltersStore.ts
@@ -1,70 +1,27 @@
-import {
-    FilterContextValue,
-    FilterState,
-    FilterType,
-    readInitFilterValues
-} from "@mendix/widget-plugin-filtering/main";
-import { action, computed, makeObservable, observable } from "mobx";
+import { FilterAPIv2 } from "@mendix/widget-plugin-filtering/provider-next";
+import { LegacyPv } from "@mendix/widget-plugin-filtering/LegacyPv";
+import { computed, makeObservable } from "mobx";
 import { FilterCondition } from "mendix/filters";
-import { DatagridContainerProps, FilterListType } from "../../../typings/DatagridProps";
+import { DatagridContainerProps } from "../../../typings/DatagridProps";
 
 export class HeaderFiltersStore {
-    isDirty = false;
+    private provider: LegacyPv;
+    context: FilterAPIv2;
 
-    private initialFilters: FilterCondition | undefined;
-    private filterList: FilterListType[];
-    private filters: Record<FilterType, FilterState | undefined> = {
-        [FilterType.STRING]: undefined,
-        [FilterType.NUMBER]: undefined,
-        [FilterType.DATE]: undefined,
-        [FilterType.ENUMERATION]: undefined,
-        [FilterType.ASSOCIATION]: undefined
-    };
-
-    constructor(props: Pick<DatagridContainerProps, "datasource" | "filterList">) {
-        this.initialFilters = props.datasource.filter;
-        this.filterList = props.filterList;
-
-        makeObservable<typeof this, "filters">(this, {
-            filters: observable,
-            isDirty: observable,
-
-            filterConditions: computed.struct,
-
-            setFilterState: action,
-            setDirty: action
+    constructor(props: Pick<DatagridContainerProps, "filterList">) {
+        this.provider = new LegacyPv(props.filterList.map(f => f.filter));
+        this.context = {
+            version: 2,
+            parentChannelName: "",
+            provider: this.provider
+        };
+        makeObservable(this, {
+            filterConditions: computed
         });
     }
 
-    setFilterState(type: FilterType, filter: FilterState | undefined): void {
-        this.filters[type] = filter;
-    }
-
     get filterConditions(): FilterCondition[] {
-        return Object.keys(this.filters)
-            .map((key: FilterType) => this.filters[key]?.getFilterCondition?.())
-            .filter((filter): filter is FilterCondition => filter !== undefined);
-    }
-
-    getFilterContextProps(): Pick<FilterContextValue, "multipleAttributes" | "multipleInitialFilters"> {
-        const multipleAttributes = this.filterList.reduce(
-            (filters, { filter }) => ({ ...filters, [filter.id]: filter }),
-            {}
-        );
-        const multipleInitialFilters = this.filterList.reduce(
-            (filters, { filter }) => ({
-                ...filters,
-                [filter.id]: readInitFilterValues(filter, this.initialFilters)
-            }),
-            {}
-        );
-        return {
-            multipleAttributes,
-            multipleInitialFilters
-        };
-    }
-
-    setDirty(): void {
-        this.isDirty = true;
+        const cond = this.provider.filterCondition;
+        return cond ? [cond] : [];
     }
 }
