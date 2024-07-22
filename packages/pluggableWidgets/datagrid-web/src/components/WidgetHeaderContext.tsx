96a07334f983d075cc8c795af8d4d9fa563b9648
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2024-07-22 10:25:47 +0200

    feat: migrate header to api v2

--- packages/pluggableWidgets/datagrid-web/src/components/WidgetHeaderContext.tsx
+++ packages/pluggableWidgets/datagrid-web/src/components/WidgetHeaderContext.tsx
@@ -1,40 +1,39 @@
-import { createElement, ReactElement, memo, ReactNode } from "react";
-
-import { getGlobalSelectionContext } from "@mendix/widget-plugin-grid/selection";
-import { getGlobalFilterContextObject } from "@mendix/widget-plugin-filtering/provider";
+import { createElement, memo, ReactElement, ReactNode } from "react";
+import { getGlobalFilterContextObject } from "@mendix/widget-plugin-filtering/provider-next";
+import {
+    getGlobalSelectionContext,
+    SelectionHelper,
+    useCreateSelectionContextValue
+} from "@mendix/widget-plugin-grid/selection";
 import { HeaderFiltersStore } from "../helpers/state/HeaderFiltersStore";
 
 interface WidgetHeaderContextProps {
-    selectionContextValue: { status: "all" | "some" | "none"; toggle: () => void } | undefined;
     children?: ReactNode;
-    eventsChannelName?: string;
-    headerFilterStore: HeaderFiltersStore;
+    filtersStore: HeaderFiltersStore;
+    selectionHelper?: SelectionHelper;
 }
 
-const component = memo((props: WidgetHeaderContextProps) => {
-    const SelectionContext = getGlobalSelectionContext();
-    const FilterContext = getGlobalFilterContextObject();
+const SelectionContext = getGlobalSelectionContext();
+const FilterContext = getGlobalFilterContextObject();
 
+function FilterAPIProvider(props: { filtersStore: HeaderFiltersStore; children?: ReactNode }): ReactElement {
+    return <FilterContext.Provider value={props.filtersStore.context}>{props.children}</FilterContext.Provider>;
+}
+
+function SelectionStatusProvider(props: { selectionHelper?: SelectionHelper; children?: ReactNode }): ReactElement {
+    const value = useCreateSelectionContextValue(props.selectionHelper);
+    return <SelectionContext.Provider value={value}>{props.children}</SelectionContext.Provider>;
+}
+
+function HeaderContainer(props: WidgetHeaderContextProps): ReactElement {
     return (
-        <FilterContext.Provider
-            value={{
-                version: 1,
-                eventsChannelName: props.eventsChannelName,
-                store: null,
-                filterDispatcher: prev => {
-                    if (prev.filterType) {
-                        props.headerFilterStore.setFilterState(prev.filterType, prev);
-                        props.headerFilterStore.setDirty();
-                    }
-                    return prev;
-                },
-                ...props.headerFilterStore.getFilterContextProps()
-            }}
-        >
-            <SelectionContext.Provider value={props.selectionContextValue}>{props.children}</SelectionContext.Provider>
-        </FilterContext.Provider>
+        <FilterAPIProvider filtersStore={props.filtersStore}>
+            <SelectionStatusProvider selectionHelper={props.selectionHelper}>{props.children}</SelectionStatusProvider>
+        </FilterAPIProvider>
     );
-});
+}
+
+const component = memo(HeaderContainer);
 
 component.displayName = "WidgetHeaderContext";
 
