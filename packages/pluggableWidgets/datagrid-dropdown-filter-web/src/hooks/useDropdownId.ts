188089607873e1294c6e5c6cad2ed9959851a8df
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2024-07-24 14:55:20 +0200

    feat: migrate select filter

--- packages/pluggableWidgets/datagrid-dropdown-filter-web/src/hooks/useDropdownId.ts
+++
@@ -1,7 +0,0 @@
-import { generateUUID } from "@mendix/widget-plugin-platform/framework/generate-uuid";
-import { useState } from "react";
-
-export function useDropdownId(): string {
-    const [id] = useState(`DropdownFilter${generateUUID()}`);
-    return id;
-}
--- packages/pluggableWidgets/datagrid-dropdown-filter-web/src/utils/helper.ts
+++
@@ -1,3 +0,0 @@
-export function preventReactErrorsAboutReadOnly(): void {
-    return undefined;
-}
