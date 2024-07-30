67113894784bb9f98ddf11cbf4e0f65279e6217c
Rahman Unver <rahmanunver@gmail.com>
2024-07-30 16:16:13 +0200

    feat(markdown-web): add mx-progress element

--- packages/pluggableWidgets/markdown-web/src/Markdown.tsx
+++ packages/pluggableWidgets/markdown-web/src/Markdown.tsx
@@ -1,5 +1,6 @@
+import classNames from "classnames";
 import MarkdownIt from "markdown-it";
-import { ReactNode, createElement, useRef, useEffect } from "react";
+import { ReactNode, createElement, useEffect, useRef } from "react";
 import { MarkdownContainerProps } from "../typings/MarkdownProps";
 import "./ui/Markdown.scss";
 const mdParser = new MarkdownIt("default", {
@@ -10,12 +11,15 @@ const mdParser = new MarkdownIt("default", {
 export function Markdown(props: MarkdownContainerProps): ReactNode {
     const { stringAttribute } = props;
     const previewRef = useRef<HTMLDivElement>(null);
-
     useEffect(() => {
         if (previewRef.current) {
             previewRef.current.innerHTML = mdParser.render(stringAttribute.value ?? "");
         }
     }, [stringAttribute?.value, stringAttribute?.status]);
 
-    return stringAttribute?.status === "available" && <div className="widget-markdown" ref={previewRef}></div>;
+    return stringAttribute?.status === "available" ? (
+        <div className={classNames("widget-markdown")} ref={previewRef}></div>
+    ) : (
+        <div className="mx-progress"></div>
+    );
 }
