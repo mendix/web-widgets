6159c6c563773a449d5fd6ca301ac0d3d9ff6b92
Grand Julivan <grandjulivan@gmail.com>
2024-08-06 12:45:19 +0200

    chore(rte): add dialog modal

---
+++ packages/pluggableWidgets/rich-text-web/src/utils/formats.d.ts
@@ -0,0 +1,15 @@
+export type linkConfigType = {
+    href: string;
+    title: string;
+    target: string;
+};
+
+export type videoConfigType = {
+    src: string;
+    width: number;
+    height: number;
+};
+
+export type videoEmbedConfigType = {
+    embedcode: string;
+};
