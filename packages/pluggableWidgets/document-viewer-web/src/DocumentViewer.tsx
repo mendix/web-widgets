ffc7cc6261374b32a863900cc0cb1d80374fea40
Rahman Unver <rahmanunver@gmail.com>
2024-08-12 16:04:16 +0300

    feat(document-viewer-web): test project to troubleshoot

--- packages/pluggableWidgets/document-viewer-web/src/DocumentViewer.tsx
+++ packages/pluggableWidgets/document-viewer-web/src/DocumentViewer.tsx
@@ -1,9 +1,16 @@
 import { ReactNode, createElement } from "react";
 import { DocumentViewerContainerProps } from "../typings/DocumentViewerProps";
 import "./ui/DocumentViewer.scss";
+import DocViewer, { DocViewerRenderers } from "react-doc-viewer";
 
-export function DocumentViewer(_props: DocumentViewerContainerProps): ReactNode {
-    console.log("props", _props);
-
-    return <div className="widget-document-viewer"></div>;
+export function DocumentViewer(props: DocumentViewerContainerProps): ReactNode {
+    if (props.file.status === "available" && props.file.value.uri) {
+        const docs = [{ uri: props.file.value?.uri }];
+        return <DocViewer pluginRenderers={DocViewerRenderers} documents={docs} />;
+    }
+    return (
+        <div>
+            <h1>File Unavailable</h1>
+        </div>
+    );
 }
