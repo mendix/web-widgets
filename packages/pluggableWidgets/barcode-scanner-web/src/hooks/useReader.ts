fb35815d7bc847b7acba74949bdf944ea13005b5
hedwig.doets <hedwig.doets@mendix.com>
2024-08-19 10:02:12 +0200

    fix: create utils, update video dimensions in loop

--- packages/pluggableWidgets/barcode-scanner-web/src/hooks/useReader.ts
+++ packages/pluggableWidgets/barcode-scanner-web/src/hooks/useReader.ts
@@ -1,16 +1,8 @@
 import { useEffect, useRef, RefObject } from "react";
-import {
-    BarcodeFormat,
-    BinaryBitmap,
-    BrowserMultiFormatReader,
-    DecodeHintType,
-    HTMLCanvasElementLuminanceSource,
-    HybridBinarizer,
-    NotFoundException,
-    Result
-} from "@zxing/library";
+import { BrowserMultiFormatReader, NotFoundException, Result } from "@zxing/library";
 import { useEventCallback } from "@mendix/widget-plugin-hooks/useEventCallback";
 import { BarcodeFormatsType } from "../../typings/BarcodeScannerProps";
+import { createHints, decodeCanvas, drawCropOnCanvas } from "../components/utils";
 
 const mediaStreamConstraints: MediaStreamConstraints = {
     audio: false,
@@ -41,90 +33,34 @@ export const useReader: UseReaderHook = args => {
         return ifNotFound && !stopped.current;
     };
 
-    const loop = (
+    const decodeCropFromVideo = (
+        // loop decode canvas till it finds a result
         resolve: (value: Result) => void,
         reject: (reason?: Error) => void,
-        captureCanvas: HTMLCanvasElement,
-        videoCropWidth: number,
-        videoCropHeight: number
+        captureCanvas: HTMLCanvasElement
     ): void => {
         try {
-            const canvasContext = captureCanvas.getContext("2d", { willReadFrequently: true });
-            if (canvasContext !== null && videoRef.current !== null && reader.current !== undefined) {
-                canvasContext.drawImage(
-                    videoRef.current,
-                    (videoRef.current.videoWidth - videoCropWidth) / 2,
-                    (videoRef.current.videoHeight - videoCropHeight) / 2,
-                    videoCropWidth,
-                    videoCropHeight,
-                    0,
-                    0,
-                    videoCropWidth,
-                    videoCropHeight
-                );
-                const luminanceSource = new HTMLCanvasElementLuminanceSource(captureCanvas);
-                const binaryBitmap = new BinaryBitmap(new HybridBinarizer(luminanceSource));
-                const result = reader.current.decodeBitmap(binaryBitmap);
-                resolve(result);
+            if (videoRef.current === null || reader.current === undefined || args.canvasMiddleRef.current === null) {
+                setTimeout(() => decodeCropFromVideo(resolve, reject, captureCanvas), 50);
+                return;
             }
+            const croppedOnCanvas = drawCropOnCanvas(captureCanvas, videoRef.current, args.canvasMiddleRef.current);
+            const result = decodeCanvas(reader.current, croppedOnCanvas);
+            if (result === null) {
+                throw new NotFoundException();
+            }
+            resolve(result);
         } catch (error) {
             if (checkNotFound(error)) {
-                setTimeout(() => loop(resolve, reject, captureCanvas, videoCropWidth, videoCropHeight), 50);
+                setTimeout(() => decodeCropFromVideo(resolve, reject, captureCanvas), 50);
             } else {
                 reject(error);
             }
         }
     };
 
-    const scanWithCropOnce = (
-        reader: BrowserMultiFormatReader,
-        videoRefCurrent: HTMLVideoElement,
-        canvasRef: HTMLDivElement
-    ): Promise<Result> => {
-        const aspectRatioClient = videoRefCurrent.clientWidth / videoRefCurrent.clientHeight;
-        const aspectRatioVideo = videoRefCurrent.videoWidth / videoRefCurrent.videoHeight;
-
-        let videoCropWidth = (canvasRef.clientWidth / videoRefCurrent.clientWidth) * videoRefCurrent.videoWidth;
-        let videoCropHeight = (canvasRef.clientHeight / videoRefCurrent.clientHeight) * videoRefCurrent.videoHeight;
-
-        if (aspectRatioVideo < aspectRatioClient) {
-            videoCropHeight = videoCropWidth;
-        } else {
-            videoCropWidth = videoCropHeight;
-        }
-
-        const captureCanvas = reader.createCaptureCanvas(videoRefCurrent);
-        captureCanvas.width = videoCropWidth;
-        captureCanvas.height = videoCropHeight;
-
-        return new Promise((resolve, reject) => loop(resolve, reject, captureCanvas, videoCropWidth, videoCropHeight));
-    };
-
     useEffect(() => {
-        const hints = new Map();
-        let formats: BarcodeFormat[] = [];
-        if (args.useAllFormats) {
-            formats = [
-                BarcodeFormat.UPC_A,
-                BarcodeFormat.UPC_E,
-                BarcodeFormat.EAN_8,
-                BarcodeFormat.EAN_13,
-                BarcodeFormat.CODE_39,
-                BarcodeFormat.CODE_128,
-                BarcodeFormat.ITF,
-                BarcodeFormat.RSS_14,
-                BarcodeFormat.QR_CODE,
-                BarcodeFormat.DATA_MATRIX,
-                BarcodeFormat.AZTEC,
-                BarcodeFormat.PDF_417
-            ];
-        } else {
-            if (args.barcodeFormats) {
-                formats = args.barcodeFormats.map(val => BarcodeFormat[val.barcodeFormat]);
-            }
-        }
-        hints.set(DecodeHintType.POSSIBLE_FORMATS, formats);
-        hints.set(DecodeHintType.ENABLE_CODE_39_EXTENDED_MODE, true);
+        const hints = createHints(args.useAllFormats, args.barcodeFormats);
         const newReader = new BrowserMultiFormatReader(hints, 500);
         reader.current = newReader;
         const stop = (): void => {
@@ -139,16 +75,26 @@ export const useReader: UseReaderHook = args => {
             }
             try {
                 stream = await navigator.mediaDevices.getUserMedia(mediaStreamConstraints);
+                stream.getVideoTracks().forEach(track => {
+                    console.error(track);
+                    console.error(track.getCapabilities());
+                });
+                navigator.mediaDevices.enumerateDevices().then(value => {
+                    value.forEach(info => {
+                        console.error(info.deviceId);
+                    });
+                });
+
                 let result: Result;
                 if (args.useCrop) {
                     videoRef.current.srcObject = stream;
                     videoRef.current.autofocus = true;
                     videoRef.current.playsInline = true; // Fix error in Safari
                     await videoRef.current.play();
-                    if (args.canvasMiddleRef.current === null) {
-                        return;
-                    }
-                    result = await scanWithCropOnce(newReader, videoRef.current, args.canvasMiddleRef.current);
+                    const captureCanvas = newReader.createCaptureCanvas(videoRef.current);
+                    result = await new Promise((resolve, reject) =>
+                        decodeCropFromVideo(resolve, reject, captureCanvas)
+                    );
                 } else {
                     result = await newReader.decodeOnceFromStream(stream, videoRef.current);
                 }
