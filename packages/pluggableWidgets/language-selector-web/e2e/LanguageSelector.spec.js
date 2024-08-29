83c23d7e0012dd72b19d521d9c1f7734aea8b0da
leonardomendix <leonardo.de.souza@mendix.com>
2024-08-29 15:03:53 +0200

    test: add a11y test checks

--- packages/pluggableWidgets/language-selector-web/e2e/LanguageSelector.spec.js
+++ packages/pluggableWidgets/language-selector-web/e2e/LanguageSelector.spec.js
@@ -1,4 +1,5 @@
 import { test, expect } from "@playwright/test";
+import AxeBuilder from "@axe-core/playwright";
 
 test.afterEach("Cleanup session", async ({ page }) => {
     // Because the test isolation that will open a new session for every test executed, and that exceeds Mendix's license limit of 5 sessions, so we need to force logout after each test.
@@ -27,5 +28,25 @@ test.describe("language-selector-web", () => {
         await page.click(".current-language-text", { force: true });
         await page.click("text=Chinese");
         await expect(page).toHaveScreenshot(`languageSelectorChinese.png`);
+    });
+
+    test("checks accessibility violations", async ({ page }) => {
+        await page.goto("/");
+        await page.waitForLoadState("networkidle");
+        const accessibilityScanResults = await new AxeBuilder({ page })
+            .withTags(["wcag21aa"])
+            .disableRules([
+                "aria-required-children",
+                "label",
+                "aria-roles",
+                "button-name",
+                "duplicate-id-active",
+                "duplicate-id",
+                "aria-allowed-attr"
+            ])
+            .exclude(".mx-name-navigationTree3")
+            .analyze();
+
+        expect(accessibilityScanResults.violations).toEqual([]);
     });
 });
