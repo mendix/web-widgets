3660c0757591c23f890c56cd7a634193651061e1
Grand Julivan <grandjulivan@gmail.com>
2024-09-03 10:01:39 +0200

    feat(rte): sticky toolbar

--- packages/pluggableWidgets/rich-text-web/src/utils/themes/mendix.ts
+++ packages/pluggableWidgets/rich-text-web/src/utils/themes/mendix.ts
@@ -3,7 +3,7 @@ import Quill from "quill";
 import SnowTheme from "quill/themes/snow";
 
 class MendixTheme extends SnowTheme {
-    buildPickers(selects: NodeListOf<HTMLSelectElement>, icons: Record<string, string | Record<string, string>>) {
+    buildPickers(selects: NodeListOf<HTMLSelectElement>, icons: Record<string, string | Record<string, string>>): void {
         super.buildPickers(selects, icons);
     }
 }
