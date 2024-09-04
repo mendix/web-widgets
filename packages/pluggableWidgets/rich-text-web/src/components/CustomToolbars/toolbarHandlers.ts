8d752cb9aff2024e60ac5da19cc43f75c805321f
Grand Julivan <grandjulivan@gmail.com>
2024-09-04 12:25:20 +0200

    feat(rte): support legacy quill format and code cleanup

---
+++ packages/pluggableWidgets/rich-text-web/src/components/CustomToolbars/toolbarHandlers.ts
@@ -0,0 +1,27 @@
+import Quill from "quill";
+import { MutableRefObject } from "react";
+
+/**
+ * give custom indent handler to use our custom "indent-left" and "indent-right" formats
+ */
+export function getIndentHandler(ref: MutableRefObject<Quill | null>) {
+    const customIndentHandler = (value: any): void => {
+        const range = ref.current?.getSelection();
+        // @ts-expect-error type error expected
+        const formats = ref.current?.getFormat(range);
+        if (formats) {
+            const indent = parseInt((formats.indent as string) || "0", 10);
+            if (value === "+1" || value === "-1") {
+                let modifier = value === "+1" ? 1 : -1;
+                if (formats.direction === "rtl") {
+                    modifier *= -1;
+                    ref.current?.format("indent-right", indent + modifier, Quill.sources.USER);
+                } else {
+                    ref.current?.format("indent-left", indent + modifier, Quill.sources.USER);
+                }
+            }
+        }
+    };
+
+    return customIndentHandler;
+}
