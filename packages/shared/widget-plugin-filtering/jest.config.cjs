48c875632045a7653f6dcc08a6e1a6a9b7047e2b
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2024-09-17 10:39:55 +0200

    test(widget-plugin-filtering): add date store test

--- packages/shared/widget-plugin-filtering/jest.config.cjs
+++ packages/shared/widget-plugin-filtering/jest.config.cjs
@@ -14,6 +14,7 @@ module.exports = {
             }
         ]
     },
+    moduleDirectories: ["node_modules", "src"],
     moduleNameMapper: {
         "(.+)\\.js": "$1"
     },
--- packages/shared/widget-plugin-filtering/src/__tests__/example.spec.ts
+++
@@ -1,5 +0,0 @@
-describe("widget-plugin-hooks stub", () => {
-    it("run tests", () => {
-        expect(1).toEqual(1);
-    });
-});
