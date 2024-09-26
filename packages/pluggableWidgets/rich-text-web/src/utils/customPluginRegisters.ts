59ad8834be2111f8929ff5d71ee1182b673d439a
Grand Julivan <grandjulivan@gmail.com>
2024-09-26 09:42:10 +0200

    fix: design mode and embed video and formula and fonts and keyboards

--- packages/pluggableWidgets/rich-text-web/src/utils/customPluginRegisters.ts
+++ packages/pluggableWidgets/rich-text-web/src/utils/customPluginRegisters.ts
@@ -9,13 +9,17 @@ import { Attributor } from "parchment";
 const direction = Quill.import("attributors/style/direction") as Attributor;
 const alignment = Quill.import("attributors/style/align") as Attributor;
 import { IndentLeftStyle, IndentRightStyle } from "./formats/indent";
-
+import { HeightAttribute, WidthAttribute } from "./formats/sizing";
+import Formula from "./formats/formula";
 class Empty {
     doSomething(): string {
         return "";
     }
 }
 
+/**
+ * Custom format registration for quill.
+ */
 Quill.debug("error");
 Quill.register({ "themes/snow": MendixTheme }, true);
 Quill.register(CustomListItem, true);
@@ -25,4 +29,8 @@ Quill.register(direction, true);
 Quill.register(alignment, true);
 Quill.register(IndentLeftStyle, true);
 Quill.register(IndentRightStyle, true);
+Quill.register(Formula, true);
+Quill.register(HeightAttribute, true);
+Quill.register(WidthAttribute, true);
+// add empty handler for view code, this format is handled by toolbar's custom config via ViewCodeDialog
 Quill.register({ "ui/view-code": Empty });
