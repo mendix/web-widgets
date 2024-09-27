f15b8dfaa2421cc3170e598a887a96533912df14
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2024-09-27 16:13:00 +0200

    test(widget-plugin-filtering): add test implementations

--- packages/shared/widget-plugin-filtering/src/__tests__/RefFilterStore.spec.ts
+++ packages/shared/widget-plugin-filtering/src/__tests__/RefFilterStore.spec.ts
@@ -9,21 +9,22 @@ describe("RefFilterStore", () => {
     describe("get options()", () => {
         let items: ObjectItem[];
         let mapItem: (item: ObjectItem) => string;
+        let store: RefFilterStore;
         beforeEach(() => {
             const [a, b, c] = [obj("id3n"), obj("f8x3"), obj("932c")];
             items = [a, b, c];
             mapItem = cases([a, "Alice"], [b, "Bob"], [c, "Chuck"], [undefined, "No caption"]);
+            store = new RefFilterStore(
+                {
+                    ref: listReference(),
+                    refOptions: list(items),
+                    caption: listExpression(mapItem)
+                },
+                null
+            );
         });
 
         it("returns array of options", () => {
-            const props = {
-                ref: listReference(),
-                refOptions: list(items),
-                caption: listExpression(mapItem)
-            };
-
-            const store = new RefFilterStore(props, null);
-
             expect(store.options).toMatchInlineSnapshot(`
                 [
                   {
@@ -44,61 +45,17 @@ describe("RefFilterStore", () => {
                 ]
             `);
         });
-
-        it("compute new options whenever datasource is changed", () => {
-            const [a] = items;
-            const props = {
-                ref: listReference(),
-                refOptions: list.loading(),
-                caption: listExpression(mapItem)
-            };
-            const store = new RefFilterStore(props, null);
-            const output: any[] = [];
-            autorun(() => output.push(store.options));
-
-            expect(output[0]).toEqual([]);
-            store.updateProps({ ...props, refOptions: list([a]) });
-            expect(output[1]).toEqual([
-                {
-                    caption: "Alice",
-                    selected: false,
-                    value: "obj_id3n"
-                }
-            ]);
-        });
-
-        it("compute new options when state is restored", () => {
-            const props = {
-                ref: listReference(),
-                refOptions: list(items),
-                caption: listExpression(mapItem)
-            };
-            const store = new RefFilterStore(props, null);
-            const output: Array<typeof store.options> = [];
-            autorun(() => output.push(store.options));
-
-            // Check that every option is unselected
-            expect(output[0].map(option => option.selected)).toEqual([false, false, false]);
-            // Restore state
-            store.fromJSON(["obj_id3n"]);
-            // Check that first option is selected
-            expect(output[1].map(option => option.selected)).toEqual([true, false, false]);
-        });
     });
 
     describe("get condition()", () => {});
 
     describe("toJSON()", () => {
-        let a: ObjectItem;
-        let b: ObjectItem;
-        let c: ObjectItem;
         let store: RefFilterStore;
         beforeEach(() => {
-            [a, b, c] = [obj("id3n"), obj("f8x3"), obj("932c")];
             store = new RefFilterStore(
                 {
                     ref: listReference(),
-                    refOptions: list([a, b, c]),
+                    refOptions: list(3),
                     caption: listExpression(() => "[string]")
                 },
                 null
@@ -128,8 +85,44 @@ describe("RefFilterStore", () => {
     });
 
     describe("fromJSON()", () => {
-        it.todo("restore state from JS array");
-        it.todo("overrides any previous state");
+        let store: RefFilterStore;
+        beforeEach(() => {
+            store = new RefFilterStore(
+                {
+                    ref: listReference(),
+                    refOptions: list([obj("id3n"), obj("f8x3"), obj("932c")]),
+                    caption: listExpression(() => "[string]")
+                },
+                null
+            );
+        });
+
+        it("restore state from JS array", () => {
+            const output: Array<typeof store.options> = [];
+            autorun(() => output.push(store.options));
+
+            // Check that every option is unselected
+            expect(output[0].map(option => option.selected)).toEqual([false, false, false]);
+            // Restore state
+            store.fromJSON(["obj_id3n"]);
+            // Check that first option is selected
+            expect(output[1].map(option => option.selected)).toEqual([true, false, false]);
+        });
+
+        it("overrides any previous state", () => {
+            const output: Array<typeof store.options> = [];
+
+            // Select each option
+            store.options.forEach(option => {
+                store.toggle(option.value);
+            });
+            // Start observing
+            autorun(() => output.push(store.options));
+            // Check the state
+            expect(output[0].map(option => option.selected)).toEqual([true, true, true]);
+            store.fromJSON(["obj_932c"]);
+            expect(output[1].map(option => option.selected)).toEqual([false, false, true]);
+        });
     });
 
     describe("with 'fetchOptionsLazy' flag", () => {
@@ -139,5 +132,28 @@ describe("RefFilterStore", () => {
 
     describe("when datasource changed", () => {
         it.todo("discard any unknown selected GUIDs");
+
+        it("compute new options", () => {
+            const a = obj("id3n");
+            const mapItem = cases([a, "Alice"], [undefined, "No caption"]);
+            const props = {
+                ref: listReference(),
+                refOptions: list.loading(),
+                caption: listExpression(mapItem)
+            };
+            const store = new RefFilterStore(props, null);
+            const output: any[] = [];
+            autorun(() => output.push(store.options));
+
+            expect(output[0]).toEqual([]);
+            store.updateProps({ ...props, refOptions: list([a]) });
+            expect(output[1]).toEqual([
+                {
+                    caption: "Alice",
+                    selected: false,
+                    value: "obj_id3n"
+                }
+            ]);
+        });
     });
 });
