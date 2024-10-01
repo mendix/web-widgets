af66daf0b6e2fb46d59fa9e921a2e19397fe5532
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2024-10-01 09:34:42 +0200

    feat(datagrid-web): add fetch lazy flag

--- packages/pluggableWidgets/datagrid-web/src/helpers/state/column/ColumnFilterStore.tsx
+++ packages/pluggableWidgets/datagrid-web/src/helpers/state/column/ColumnFilterStore.tsx
@@ -60,7 +60,7 @@ export class ColumnFilterStore implements IColumnFilterStore {
             ref: ensure(props.filterAssociation, errorMessage("filterAssociation")),
             refOptions: ensure(props.filterAssociationOptions, errorMessage("filterAssociationOptions")),
             caption: ensure(props.filterAssociationOptionLabel, errorMessage("filterAssociationOptionLabel")),
-            fetchOptionsLazy: false
+            fetchOptionsLazy: props.fetchOptionsLazy
         };
     }
 
