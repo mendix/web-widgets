de6d015627bc78cda6f6145d98d95e37a128c8e2
Samuel Reichert <samuelreichertt@gmail.com>
2024-10-07 17:31:46 +0200

    chore(datagrid-web): split gridHeader to a separate component

---
+++ packages/pluggableWidgets/datagrid-web/src/components/GridHeader.tsx
@@ -0,0 +1,100 @@
+import { createElement, ReactElement, ReactNode, useCallback, useState } from "react";
+import { ColumnId, GridColumn } from "../typings/GridColumn";
+import { CheckboxColumnHeader } from "./CheckboxColumnHeader";
+import { ColumnResizer } from "./ColumnResizer";
+import { ColumnSelector } from "./ColumnSelector";
+import { Header } from "./Header";
+import { HeaderSkeletonLoader } from "./loader/HeaderSkeletonLoader";
+
+type GridHeaderProps = {
+    availableColumns: GridColumn[];
+    columns: GridColumn[];
+    columnsCreateSizeSnapshot: () => void;
+    columnsDraggable: boolean;
+    columnsFilterable: boolean;
+    columnsHidable: boolean;
+    columnsResizable: boolean;
+    columnsSortable: boolean;
+    columnsSwap: (source: ColumnId, target: [ColumnId, "after" | "before"]) => void;
+    filterRenderer: (renderWrapper: (children: ReactNode) => ReactElement, columnIndex: number) => ReactElement;
+    headerWrapperRenderer: (columnIndex: number, header: ReactElement) => ReactElement;
+    id: string;
+    isLoading: boolean;
+    preview?: boolean;
+};
+
+export function GridHeader({
+    availableColumns,
+    columns,
+    columnsCreateSizeSnapshot,
+    columnsDraggable,
+    columnsFilterable,
+    columnsHidable,
+    columnsResizable,
+    columnsSortable,
+    columnsSwap,
+    filterRenderer,
+    headerWrapperRenderer,
+    id,
+    isLoading,
+    preview
+}: GridHeaderProps): ReactElement {
+    const [dragOver, setDragOver] = useState<[ColumnId, "before" | "after"] | undefined>(undefined);
+    const [isDragging, setIsDragging] = useState<[ColumnId | undefined, ColumnId, ColumnId | undefined] | undefined>();
+
+    const renderFilterWrapper = useCallback(
+        (children: ReactNode) => (
+            <div className="filter" style={{ pointerEvents: isDragging ? "none" : undefined }}>
+                {children}
+            </div>
+        ),
+        [isDragging]
+    );
+
+    if (isLoading) {
+        return <HeaderSkeletonLoader size={columns.length} />;
+    }
+
+    return (
+        <div key="headers_row" className="tr" role="row">
+            <CheckboxColumnHeader key="headers_column_select_all" />
+            {columns.map((column, index) =>
+                headerWrapperRenderer(
+                    index,
+                    <Header
+                        key={`${column.columnId}`}
+                        className={`align-column-${column.alignment}`}
+                        gridId={id}
+                        column={column}
+                        draggable={columnsDraggable}
+                        dropTarget={dragOver}
+                        filterable={columnsFilterable}
+                        filterWidget={filterRenderer(renderFilterWrapper, column.columnIndex)}
+                        hidable={columnsHidable}
+                        isDragging={isDragging}
+                        preview={preview}
+                        resizable={columnsResizable && columns.at(-1) !== column}
+                        resizer={
+                            <ColumnResizer
+                                onResizeStart={columnsCreateSizeSnapshot}
+                                setColumnWidth={(width: number) => column.setSize(width)}
+                            />
+                        }
+                        swapColumns={columnsSwap}
+                        setDropTarget={setDragOver}
+                        setIsDragging={setIsDragging}
+                        sortable={columnsSortable}
+                    />
+                )
+            )}
+            {columnsHidable && (
+                <ColumnSelector
+                    key="headers_column_selector"
+                    columns={availableColumns}
+                    id={id}
+                    visibleLength={columns.length}
+                />
+            )}
+        </div>
+    );
+}
