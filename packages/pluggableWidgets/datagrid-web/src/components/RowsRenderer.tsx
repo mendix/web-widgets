d7fde2adfda4654eba4e6b177a7173e8df3f25f3
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2024-10-10 10:31:43 +0200

    chore: remove loggers

--- packages/pluggableWidgets/datagrid-web/src/components/RowsRenderer.tsx
+++ packages/pluggableWidgets/datagrid-web/src/components/RowsRenderer.tsx
@@ -1,5 +1,5 @@
 import { ObjectItem } from "mendix";
-import { createElement, useId } from "react";
+import { createElement } from "react";
 import { CellComponent, EventsController } from "../typings/CellComponent";
 import { GridColumn } from "../typings/GridColumn";
 import { KeyNavProvider } from "@mendix/widget-plugin-grid/keyboard-navigation/context";
@@ -22,33 +22,27 @@ interface RowsRendererProps {
 }
 
 export function RowsRenderer(props: RowsRendererProps): React.ReactElement {
-    const id = useId();
-    console.time(`RowsRenderer_${id}`);
-    try {
-        return (
-            <KeyNavProvider focusController={props.focusController}>
-                {props.rows.map((item, rowIndex) => {
-                    return (
-                        <Row
-                            totalRows={props.rows.length}
-                            clickable={props.interactive}
-                            selectActionHelper={props.selectActionHelper}
-                            preview={props.preview}
-                            eventsController={props.eventsController}
-                            CellComponent={props.Cell}
-                            className={props.rowClass?.(item)}
-                            columns={props.columns}
-                            index={rowIndex}
-                            item={item}
-                            key={`row_${item.id}`}
-                            showSelectorCell={props.columnsHidable}
-                            selectableWrapper={props.selectableWrapper}
-                        />
-                    );
-                })}
-            </KeyNavProvider>
-        );
-    } finally {
-        console.timeEnd(`RowsRenderer_${id}`);
-    }
+    return (
+        <KeyNavProvider focusController={props.focusController}>
+            {props.rows.map((item, rowIndex) => {
+                return (
+                    <Row
+                        totalRows={props.rows.length}
+                        clickable={props.interactive}
+                        selectActionHelper={props.selectActionHelper}
+                        preview={props.preview}
+                        eventsController={props.eventsController}
+                        CellComponent={props.Cell}
+                        className={props.rowClass?.(item)}
+                        columns={props.columns}
+                        index={rowIndex}
+                        item={item}
+                        key={`row_${item.id}`}
+                        showSelectorCell={props.columnsHidable}
+                        selectableWrapper={props.selectableWrapper}
+                    />
+                );
+            })}
+        </KeyNavProvider>
+    );
 }
