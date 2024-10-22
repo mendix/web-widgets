b940186252fadee8bf7be8571c73656da925c55b
Rahman Unver <rahmanunver@gmail.com>
2024-10-22 17:19:49 +0200

    fix(popup-menu-web): move styling from widget to atlas

--- packages/pluggableWidgets/popup-menu-web/src/PopupMenu.tsx
+++ packages/pluggableWidgets/popup-menu-web/src/PopupMenu.tsx
@@ -2,7 +2,6 @@ import { ReactElement, createElement } from "react";
 
 import { PopupMenuContainerProps } from "../typings/PopupMenuProps";
 import { PopupMenu as PopupMenuComponent } from "./components/PopupMenu";
-import "./ui/PopupMenu.scss";
 
 export default function PopupMenu(props: PopupMenuContainerProps): ReactElement {
     return <PopupMenuComponent {...props} />;
