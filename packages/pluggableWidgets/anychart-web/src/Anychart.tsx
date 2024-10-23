5bef74c1bdc8c2169ff525ddc4d6c92aa6040f1d
Samuel Reichert <samuelreichertt@gmail.com>
2024-10-23 13:26:47 +0200

    feat(anychart-web): create pluggable anychart widget

---
+++ packages/pluggableWidgets/anychart-web/src/Anychart.tsx
@@ -0,0 +1,9 @@
+import { ReactElement, createElement, useCallback } from "react";
+
+export function AnychartWeb(props: AnychartWebContainerProps): ReactElement {
+    return (
+        <div>
+            <p>test</p>
+        </div>
+    );
+}
