0098d157892077d565dd28364d58c47e81a61fb2
Grand Julivan <grandjulivan@gmail.com>
2024-10-24 09:19:14 +0200

    fix(rte): update based on test

--- packages/pluggableWidgets/rich-text-web/src/utils/MxQuill.ts
+++ packages/pluggableWidgets/rich-text-web/src/utils/MxQuill.ts
@@ -1,4 +1,38 @@
 /**
+ * https://github.com/slab/quill/
+Copyright (c) 2017-2024, Slab
+Copyright (c) 2014, Jason Chen
+Copyright (c) 2013, salesforce.com
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+1. Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+   contributors may be used to endorse or promote products derived from
+   this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ * 
  * this file overrides Quill instance.
  * allowing us to override certain function that is not easy to extend.
  */
@@ -74,8 +108,10 @@ function convertListHTML(items: any[], lastIndent: number, types: string[]): str
     if (items.length === 0) {
         const [endTag] = getListType(types.pop());
         if (lastIndent <= 0) {
+            // modified by web-content: adding new line \n
             return `</li></${endTag}>\n`;
         }
+        // modified by web-content: adding new line \n
         return `</li></${endTag}>\n${convertListHTML([], lastIndent - 1, types)}`;
     }
     const [{ child, offset, length, indent, type }, ...rest] = items;
@@ -85,7 +121,7 @@ function convertListHTML(items: any[], lastIndent: number, types: string[]): str
     if (indent > lastIndent) {
         types.push(type);
         if (indent === lastIndent + 1) {
-            // modified by web-content: adding list-style-type to allow retaining list style when converted to html
+            // modified by web-content: adding list-style-type to allow retaining list style when converted to html and new line \n
             return `<${tag} style="list-style-type: ${expectedType}">\n<li${attribute}>${convertHTML(
                 child,
                 offset,
@@ -96,9 +132,11 @@ function convertListHTML(items: any[], lastIndent: number, types: string[]): str
     }
     const previousType = types[types.length - 1];
     if (indent === lastIndent && type === previousType) {
+        // modified by web-content: adding new line \n
         return `</li>\n<li${attribute}>${convertHTML(child, offset, length)}${convertListHTML(rest, indent, types)}`;
     }
     const [endTag] = getListType(types.pop());
+    // modified by web-content: adding new line \n
     return `</li></${endTag}>\n${convertListHTML(items, lastIndent - 1, types)}`;
 }
 
