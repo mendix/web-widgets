2c85ceb2a3c3f993efc9d64b00768d05f9229a50
Samuel Reichert <samuelreichertt@gmail.com>
2024-10-29 11:19:02 +0100

    feat(any-chart-web): show chart properties as text

--- packages/pluggableWidgets/any-chart-web/src/AnyChart.tsx
+++ packages/pluggableWidgets/any-chart-web/src/AnyChart.tsx
@@ -5,7 +5,34 @@ export function AnychartWeb(props: AnyChartContainerProps): ReactElement {
     console.info("AnyChart", props);
     return (
         <div>
-            <p>test</p>
+            <p>Data</p>
+            <p>Data static: {props.dataStatic}</p>
+            <p>Data attribute: {props.dataAttribute?.value}</p>
+            <p>Sample: {props.sampleData}</p>
+            <p>Dev mode: {props.devMode}</p>
+            <br />
+
+            <p>Layout options</p>
+            <p>Layout static: {props.layoutStatic}</p>
+            <p>Layout attribute: {props.layoutAttribute?.value}</p>
+            <p>Sample: {props.sampleLayout}</p>
+            <br />
+
+            <p>Configuration</p>
+            <p>Configuration: {props.configurationOptions}</p>
+            <br />
+
+            <p>Sizing</p>
+            <p>Width unit: {props.widthUnit}</p>
+            <p>Width: {props.width}</p>
+            <p>Height unit: {props.heightUnit}</p>
+            <p>Height: {props.height}</p>
+            <br />
+
+            <p>Common</p>
+            <p>Name: {props.name}</p>
+            <p>Tab index: {props.tabIndex}</p>
+            <br />
         </div>
     );
 }
