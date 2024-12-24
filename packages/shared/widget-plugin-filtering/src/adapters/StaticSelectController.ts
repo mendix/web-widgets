f3d4d16546a9a739722f2438bf090979d879cee7
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2024-12-24 16:16:24 +0100

    chore: rename to select

--- packages/shared/widget-plugin-filtering/src/adapters/StaticSelectController.ts
+++ packages/shared/widget-plugin-filtering/src/adapters/StaticSelectController.ts
@@ -2,15 +2,30 @@ import { UseSelectProps } from "downshift";
 import { StaticSelectFilterStore } from "../stores/StaticSelectFilterStore";
 import { OptionWithState } from "../typings/BaseSelectStore";
 
-export class AttrSelectAdapter {
+const none = "__none__" as const;
+
+export class StaticSelectController {
     private filterStore: StaticSelectFilterStore;
 
-    constructor(filterStore: StaticSelectFilterStore) {
+    readonly emptyOption = {
+        value: none,
+        caption: "None",
+        selected: false
+    };
+
+    readonly useEmptyOption: boolean = false;
+
+    constructor({ filterStore }: { filterStore: StaticSelectFilterStore; useEmptyOption?: boolean }) {
         this.filterStore = filterStore;
+        this.useEmptyOption = true;
     }
 
     get options(): OptionWithState[] {
-        return this.filterStore.options;
+        if (!this.useEmptyOption) {
+            return this.filterStore.options;
+        }
+
+        return [this.emptyOption, ...this.filterStore.options];
     }
 
     triggerValue(): string {
@@ -23,7 +38,7 @@ export class AttrSelectAdapter {
         return selected.map(option => option.caption).join(", ");
     }
 
-    getSelectProps(): UseSelectProps<OptionWithState> {
+    getSelectProps = (): UseSelectProps<OptionWithState> => {
         return {
             items: this.options,
             itemToKey: item => item?.value,
@@ -36,9 +51,10 @@ export class AttrSelectAdapter {
                 if (multiselect) {
                     this.filterStore.toggle(selectedItem.value);
                 } else {
-                    this.filterStore.setSelected([selectedItem.value]);
+                    const selected = selectedItem.value === none ? [] : [selectedItem.value];
+                    this.filterStore.setSelected(selected);
                 }
             }
         };
-    }
+    };
 }
