c80925f0edd5d821fafa853c94385dc0ec31fc4c
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2025-01-02 16:21:40 +0100

    feat: add styling for new select

--- packages/shared/widget-plugin-filtering/src/next/primitives.tsx
+++ packages/shared/widget-plugin-filtering/src/next/primitives.tsx
@@ -2,7 +2,7 @@
 import { createElement, Fragment, forwardRef } from "react";
 import cn from "classnames";
 
-const rootName = "dropdown-filter";
+const rootName = "widget-dropdown-filter";
 
 export const Root = forwardRef<HTMLDivElement, JSX.IntrinsicElements["div"]>(function Root(props, ref) {
     return <div {...props} className={cn("form-control", rootName, props.className)} ref={ref} />;
@@ -12,7 +12,7 @@ export const ValueButton = forwardRef<HTMLButtonElement, JSX.IntrinsicElements["
     props,
     ref
 ) {
-    return <button {...props} className={`${rootName}-button`} ref={ref} />;
+    return <button {...props} className={`${rootName}-value-button`} ref={ref} />;
 });
 
 export const Input = forwardRef<HTMLInputElement, JSX.IntrinsicElements["input"]>(function Input(props, ref) {
@@ -34,8 +34,20 @@ export const Toggle = forwardRef<HTMLButtonElement, JSX.IntrinsicElements["butto
 
 export const Menu = forwardRef<HTMLUListElement, JSX.IntrinsicElements["ul"]>(function Menu(props, ref) {
     return (
-        <ul {...props} ref={ref}>
+        <ul {...props} className={cn(`${rootName}-menu`, props.className)} ref={ref}>
             {props.children}
         </ul>
     );
+});
+
+export const MenuItem = forwardRef<HTMLLIElement, JSX.IntrinsicElements["li"]>(function MenuItem(props, ref) {
+    return <li {...props} className={`${rootName}-menu-item`} ref={ref} />;
+});
+
+export const Popover = forwardRef<HTMLDivElement, JSX.IntrinsicElements["div"]>(function Popover(props, ref) {
+    return <div {...props} className={cn(`${rootName}-popover`, props.className)} ref={ref} />;
+});
+
+export const MenuSlot = forwardRef<HTMLDivElement, JSX.IntrinsicElements["div"]>(function MenuSlot(props, ref) {
+    return <div {...props} className={`${rootName}-menu-slot`} ref={ref} />;
 });
