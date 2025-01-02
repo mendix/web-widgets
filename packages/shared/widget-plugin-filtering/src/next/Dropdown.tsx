69257aa005831cc1bee04f563291120e59647b60
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2025-01-02 16:21:40 +0100

    feat: add styling for new select

--- packages/shared/widget-plugin-filtering/src/next/Dropdown.tsx
+++ packages/shared/widget-plugin-filtering/src/next/Dropdown.tsx
@@ -1,8 +1,9 @@
 import { observer } from "mobx-react-lite";
-import { createElement, Fragment } from "react";
+import React, { createElement, Fragment, useId, useMemo } from "react";
 import { useSelect, UseSelectProps } from "downshift";
-import { ValueButton, Menu, Root, Toggle } from "./primitives";
+import { ValueButton, Menu, Root, Toggle, MenuItem, Popover, MenuSlot } from "./primitives";
 import { OptionWithState } from "../typings/OptionListFilterInterface";
+import { autoUpdate, useFloating, size } from "@floating-ui/react-dom";
 
 interface DropdownProps {
     triggerValue: string;
@@ -12,20 +13,61 @@ interface DropdownProps {
 
 // eslint-disable-next-line prefer-arrow-callback
 export const Select = observer(function Select(props: DropdownProps): React.ReactElement {
-    const { getToggleButtonProps, getMenuProps, getItemProps } = useSelect(props.getHookProps());
+    const valueId = useId();
+    const { getToggleButtonProps, getMenuProps, getItemProps, isOpen, highlightedIndex } = useSelect(
+        props.getHookProps()
+    );
+
+    const { refs, floatingStyles } = useFloatingMenu(isOpen);
+
     return (
         <Fragment>
-            <Root className="v-classic">
-                <ValueButton {...getToggleButtonProps()}>{props.triggerValue}</ValueButton>
+            <Root className="variant-select" ref={refs.setReference}>
+                <ValueButton {...getToggleButtonProps({ id: valueId, "aria-label": props.triggerValue })}>
+                    {props.triggerValue}
+                </ValueButton>
                 <Toggle {...getToggleButtonProps()} />
+                <Popover hidden={!isOpen} ref={refs.setFloating} style={floatingStyles}>
+                    <MenuSlot>
+                        <Menu {...getMenuProps()}>
+                            {isOpen &&
+                                props.items.map((item, index) => (
+                                    <MenuItem
+                                        data-selected={item.selected || undefined}
+                                        data-highlighted={highlightedIndex === index || undefined}
+                                        key={index}
+                                        {...getItemProps({ item, index })}
+                                    >
+                                        {item.caption}
+                                    </MenuItem>
+                                ))}
+                        </Menu>
+                    </MenuSlot>
+                </Popover>
             </Root>
-            <Menu {...getMenuProps()}>
-                {props.items.map((item, index) => (
-                    <li key={index} {...getItemProps({ item, index })}>
-                        {item.caption}
-                    </li>
-                ))}
-            </Menu>
         </Fragment>
     );
 });
+
+function useFloatingMenu(open: boolean): ReturnType<typeof useFloating> {
+    const middleware = useMemo(
+        () => [
+            size({
+                apply({ rects, elements }) {
+                    Object.assign(elements.floating.style, {
+                        width: `${rects.reference.width}px`
+                    });
+                }
+            })
+        ],
+        []
+    );
+
+    return useFloating({
+        open,
+        placement: "bottom-start",
+        strategy: "fixed",
+        middleware,
+        whileElementsMounted: autoUpdate
+    });
+}
