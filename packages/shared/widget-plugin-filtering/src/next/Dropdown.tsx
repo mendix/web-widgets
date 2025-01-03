16b8c0ad2677f373490b00dee55b2ba521daa568
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2025-01-03 13:35:31 +0100

    feat: select case done

--- packages/shared/widget-plugin-filtering/src/next/Dropdown.tsx
+++ packages/shared/widget-plugin-filtering/src/next/Dropdown.tsx
@@ -1,7 +1,8 @@
 import { observer } from "mobx-react-lite";
+import cn from "classnames";
 import React, { createElement, Fragment, useId, useMemo } from "react";
 import { useSelect, UseSelectProps } from "downshift";
-import { ValueButton, Menu, Root, Toggle, MenuItem, Popover, MenuSlot } from "./primitives";
+import { ValueButton, Menu, Root, Toggle, MenuItem, Popover, MenuSlot, classes, Arrow } from "./primitives";
 import { OptionWithState } from "../typings/OptionListFilterInterface";
 import { autoUpdate, useFloating, size } from "@floating-ui/react-dom";
 
@@ -11,6 +12,7 @@ interface DropdownProps {
     getHookProps: () => UseSelectProps<OptionWithState>;
 }
 
+const cls = classes();
 // eslint-disable-next-line prefer-arrow-callback
 export const Select = observer(function Select(props: DropdownProps): React.ReactElement {
     const valueId = useId();
@@ -22,20 +24,29 @@ export const Select = observer(function Select(props: DropdownProps): React.Reac
 
     return (
         <Fragment>
-            <Root className="variant-select" ref={refs.setReference}>
-                <ValueButton {...getToggleButtonProps({ id: valueId, "aria-label": props.triggerValue })}>
+            <Root className={cn(cls.root, "form-control", "variant-select")} ref={refs.setReference}>
+                <ValueButton
+                    className={cls.valueButton}
+                    {...getToggleButtonProps({
+                        id: valueId,
+                        "aria-label": props.triggerValue
+                    })}
+                >
                     {props.triggerValue}
                 </ValueButton>
-                <Toggle {...getToggleButtonProps()} />
-                <Popover hidden={!isOpen} ref={refs.setFloating} style={floatingStyles}>
-                    <MenuSlot>
-                        <Menu {...getMenuProps()}>
+                <Toggle data-expanded={isOpen} className={cls.toggle} {...getToggleButtonProps()}>
+                    <Arrow className={cls.stateIcon} />
+                </Toggle>
+                <Popover className={cls.popover} hidden={!isOpen} ref={refs.setFloating} style={floatingStyles}>
+                    <MenuSlot className={cls.menuSlot}>
+                        <Menu {...getMenuProps({ className: cls.menu })}>
                             {isOpen &&
                                 props.items.map((item, index) => (
                                     <MenuItem
                                         data-selected={item.selected || undefined}
                                         data-highlighted={highlightedIndex === index || undefined}
                                         key={index}
+                                        className={cls.menuItem}
                                         {...getItemProps({ item, index })}
                                     >
                                         {item.caption}
--- packages/shared/widget-plugin-filtering/src/next/primitives.tsx
+++ packages/shared/widget-plugin-filtering/src/next/primitives.tsx
@@ -1,53 +1,76 @@
 /* eslint-disable prefer-arrow-callback */
-import { createElement, Fragment, forwardRef } from "react";
-import cn from "classnames";
+import { createElement, forwardRef } from "react";
 
-const rootName = "widget-dropdown-filter";
+type ClassKeys =
+    | "root"
+    | "valueButton"
+    | "input"
+    | "clear"
+    | "toggle"
+    | "menu"
+    | "menuSlot"
+    | "menuItem"
+    | "popover"
+    | "stateIcon";
+
+export function classes(rootName = "widget-dropdown-filter"): Record<ClassKeys, string> {
+    return {
+        root: rootName,
+        valueButton: `${rootName}-value-button`,
+        input: `${rootName}-input`,
+        clear: `${rootName}-clear`,
+        toggle: `${rootName}-toggle`,
+        menu: `${rootName}-menu`,
+        menuSlot: `${rootName}-menu-slot`,
+        menuItem: `${rootName}-menu-item`,
+        popover: `${rootName}-popover`,
+        stateIcon: `${rootName}-state-icon`
+    };
+}
 
 export const Root = forwardRef<HTMLDivElement, JSX.IntrinsicElements["div"]>(function Root(props, ref) {
-    return <div {...props} className={cn("form-control", rootName, props.className)} ref={ref} />;
+    return <div {...props} ref={ref} />;
 });
 
 export const ValueButton = forwardRef<HTMLButtonElement, JSX.IntrinsicElements["button"]>(function ValueButton(
     props,
     ref
 ) {
-    return <button {...props} className={`${rootName}-value-button`} ref={ref} />;
+    return <button {...props} ref={ref} />;
 });
 
 export const Input = forwardRef<HTMLInputElement, JSX.IntrinsicElements["input"]>(function Input(props, ref) {
-    return <input {...props} className={cn(`${rootName}-input`, props.className)} ref={ref} />;
+    return <input {...props} ref={ref} />;
 });
 
 export const Clear = forwardRef<HTMLButtonElement, JSX.IntrinsicElements["button"]>(function Clear(props, ref) {
-    return <button {...props} className={`${rootName}-clear`} ref={ref} />;
+    return <button {...props} ref={ref} />;
 });
 
 export const Toggle = forwardRef<HTMLButtonElement, JSX.IntrinsicElements["button"]>(function Toggle(props, ref) {
-    const isOpen = true;
-    return (
-        <button {...props} className={`${rootName}-toggle`} tabIndex={-1} ref={ref}>
-            {isOpen ? <Fragment>&#8593;</Fragment> : <Fragment>&#8595;</Fragment>}
-        </button>
-    );
+    return <button {...props} tabIndex={-1} ref={ref} />;
 });
 
 export const Menu = forwardRef<HTMLUListElement, JSX.IntrinsicElements["ul"]>(function Menu(props, ref) {
-    return (
-        <ul {...props} className={cn(`${rootName}-menu`, props.className)} ref={ref}>
-            {props.children}
-        </ul>
-    );
+    return <ul {...props} ref={ref} />;
 });
 
 export const MenuItem = forwardRef<HTMLLIElement, JSX.IntrinsicElements["li"]>(function MenuItem(props, ref) {
-    return <li {...props} className={`${rootName}-menu-item`} ref={ref} />;
+    return <li {...props} ref={ref} />;
 });
 
 export const Popover = forwardRef<HTMLDivElement, JSX.IntrinsicElements["div"]>(function Popover(props, ref) {
-    return <div {...props} className={cn(`${rootName}-popover`, props.className)} ref={ref} />;
+    return <div {...props} ref={ref} />;
 });
 
 export const MenuSlot = forwardRef<HTMLDivElement, JSX.IntrinsicElements["div"]>(function MenuSlot(props, ref) {
-    return <div {...props} className={`${rootName}-menu-slot`} ref={ref} />;
+    return <div {...props} ref={ref} />;
 });
+
+export function Arrow(props: JSX.IntrinsicElements["svg"]): React.ReactElement {
+    return (
+        <svg width="16" height="16" viewBox="0 0 32 32" {...props}>
+            <path d="M16 23.41L4.29004 11.71L5.71004 10.29L16 20.59L26.29 10.29L27.71 11.71L16 23.41Z" />
+        </svg>
+    );
+}
