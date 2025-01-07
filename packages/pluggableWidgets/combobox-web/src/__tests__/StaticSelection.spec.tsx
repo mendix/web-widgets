47519d1ddf0f3c8e44a81cf0dee71a21288ced52
Rahman Unver <rahmanunver@gmail.com>
2025-01-07 15:08:13 +0100

    test(combobox-web): ix tests

--- packages/pluggableWidgets/combobox-web/src/__tests__/StaticSelection.spec.tsx
+++ packages/pluggableWidgets/combobox-web/src/__tests__/StaticSelection.spec.tsx
@@ -71,7 +71,9 @@ describe("Combo box (Static values)", () => {
                     staticDataSourceCustomContent: undefined,
                     staticDataSourceCaption: dynamic("caption2")
                 }
-            ]
+            ],
+            databaseSelectedItemsSorting: "value",
+            selectedItemsSorting: "value"
         };
         if (defaultProps.optionsSourceAssociationCaptionType === "expression") {
             defaultProps.optionsSourceAssociationCaptionExpression!.get = i => dynamic(`${i.id}`);
