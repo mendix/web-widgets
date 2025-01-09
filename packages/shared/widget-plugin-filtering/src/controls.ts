b30263b8d8d7e9320f81af94ff4189d2e063a0f3
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2025-01-09 14:50:35 +0100

    chore: finish combobox

--- packages/shared/widget-plugin-filtering/src/controls.ts
+++ packages/shared/widget-plugin-filtering/src/controls.ts
@@ -1,5 +1,4 @@
 export { FilterSelector } from "./controls/filter-selector/FilterSelector";
 export { InputWithFilters, InputWithFiltersComponent } from "./controls/input/InputWithFilters";
 export * from "./controls/input/typings";
-export { SelectPanel } from "./controls/select-panel/SelectPanel";
 export { Select } from "./controls/select/SelectComponent";
