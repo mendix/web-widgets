c8b1f2294d2a5189485bb778699e7d47c3c34c74
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2025-01-10 15:36:05 +0100

    chore: add js action helper

--- packages/pluggableWidgets/datagrid-dropdown-filter-web/src/components/StaticFilterContainer.tsx
+++ packages/pluggableWidgets/datagrid-dropdown-filter-web/src/components/StaticFilterContainer.tsx
@@ -1,18 +1,17 @@
-import { StaticFilterController } from "@mendix/widget-plugin-filtering/controllers/StaticFilterController";
-import { useOnResetValueEvent, useOnSetValueEvent } from "@mendix/widget-plugin-external-events/hooks";
 import { StaticSelectController } from "@mendix/widget-plugin-filtering/controllers/StaticSelectController";
 import { StaticComboboxController } from "@mendix/widget-plugin-filtering/controllers/StaticComboboxController";
 import { Select } from "@mendix/widget-plugin-filtering/controls/select/Select";
 import { Combobox } from "@mendix/widget-plugin-filtering/controls/combobox/Combobox";
 import { ActionValue, EditableValue } from "mendix";
 import { observer } from "mobx-react-lite";
-import { createElement, CSSProperties, useEffect, useState } from "react";
+import { createElement, CSSProperties, useState } from "react";
 import { FilterOptionsType } from "../../typings/DatagridDropdownFilterProps";
 import { withCustomOptionsGuard } from "../hocs/withCustomOptionsGuard";
 import { StaticSelectFilterStore } from "@mendix/widget-plugin-filtering/stores/StaticSelectFilterStore";
 import { StaticMultiboxController } from "@mendix/widget-plugin-filtering/controllers/StaticMultiboxController";
 import { Multibox } from "@mendix/widget-plugin-filtering/controls/multibox/Multibox";
 import { useSetup } from "@mendix/widget-plugin-filtering/helpers/useSetup";
+import { usePickerJSActions } from "@mendix/widget-plugin-filtering/helpers/usePickerJSActions";
 
 export interface StaticFilterContainerProps {
     parentChannelName: string | undefined;
@@ -31,53 +30,58 @@ export interface StaticFilterContainerProps {
 }
 
 function Container(props: StaticFilterContainerProps): React.ReactElement {
-    const [controller] = useState(() => new StaticFilterController(props));
-    const [ctrl2] = useState(() => new StaticSelectController({ filterStore: props.filterStore }));
-    const ctrl3 = useSetup(() => new StaticComboboxController({ filterStore: props.filterStore }));
-    const [ctrl4] = useState(() => new StaticMultiboxController({ filterStore: props.filterStore }));
-
-    useEffect(() => controller.setup(), [controller]);
-    useEffect(() => controller.updateProps(props));
-
-    useOnResetValueEvent({
-        widgetName: props.name,
-        parentChannelName: props.parentChannelName,
-        listener: controller.handleResetValue
-    });
-
-    useOnSetValueEvent({
-        widgetName: props.name,
-        listener: controller.handleSetValue
-    });
-
     const USE_SELECT = false;
     const USE_COMBOBOX = true;
 
     if (USE_SELECT) {
-        return (
-            <Select
-                value={ctrl2.value}
-                useSelectProps={ctrl2.useSelectProps}
-                options={ctrl2.options}
-                onClear={ctrl2.handleClear}
-                clearable
-            />
-        );
+        return <SelectWidget {...props} />;
     }
 
     if (USE_COMBOBOX) {
-        return (
-            <Combobox
-                options={ctrl3.options}
-                inputPlaceholder={ctrl3.inputPlaceholder}
-                useComboboxProps={ctrl3.useComboboxProps}
-                onClear={ctrl3.handleClear}
-                onFocus={ctrl3.handleFocus}
-                onBlur={ctrl3.handleBlur}
-                empty={ctrl3.isEmpty}
-            />
-        );
+        return <ComboboxWidget {...props} />;
     }
+
+    return <MultiboxWidget {...props} />;
+}
+
+function SelectWidget(props: StaticFilterContainerProps): React.ReactElement {
+    const [ctrl2] = useState(() => new StaticSelectController({ filterStore: props.filterStore }));
+
+    usePickerJSActions(ctrl2, props);
+
+    return (
+        <Select
+            value={ctrl2.value}
+            useSelectProps={ctrl2.useSelectProps}
+            options={ctrl2.options}
+            onClear={ctrl2.handleClear}
+            clearable
+        />
+    );
+}
+
+function ComboboxWidget(props: StaticFilterContainerProps): React.ReactElement {
+    const ctrl3 = useSetup(() => new StaticComboboxController({ filterStore: props.filterStore }));
+
+    // usePickerJSActions(ctrl3, props);
+
+    return (
+        <Combobox
+            options={ctrl3.options}
+            inputPlaceholder={ctrl3.inputPlaceholder}
+            useComboboxProps={ctrl3.useComboboxProps}
+            onClear={ctrl3.handleClear}
+            onFocus={ctrl3.handleFocus}
+            onBlur={ctrl3.handleBlur}
+            empty={ctrl3.isEmpty}
+        />
+    );
+}
+
+function MultiboxWidget(props: StaticFilterContainerProps): React.ReactElement {
+    const [ctrl4] = useState(() => new StaticMultiboxController({ filterStore: props.filterStore }));
+
+    // usePickerJSActions(ctrl4, props);
 
     return (
         <Multibox
---
+++ packages/shared/widget-plugin-filtering/src/controllers/PickerJSActionsHelper.ts
@@ -0,0 +1,41 @@
+import { IJSActionsControlled } from "../stores/IJSActionsControlled";
+
+interface FilterStore {
+    reset: () => void;
+    clear: () => void;
+    setSelected: (value: Iterable<string>) => void;
+}
+
+type Parse = (value: string) => Iterable<string>;
+
+export class PickerJSActionsHelper implements IJSActionsControlled {
+    private filterStore: FilterStore;
+    private parse: Parse;
+    private multiselect: boolean;
+
+    constructor({ filterStore, parse, multiselect }: { filterStore: FilterStore; parse: Parse; multiselect: boolean }) {
+        this.filterStore = filterStore;
+        this.parse = parse;
+        this.multiselect = multiselect;
+    }
+
+    handleResetValue = (useDefaultValue: boolean): void => {
+        if (useDefaultValue) {
+            this.filterStore.reset();
+            return;
+        }
+        this.filterStore.clear();
+    };
+
+    handleSetValue = (useDefaultValue: boolean, params: { stringValue: string }): void => {
+        if (useDefaultValue) {
+            this.filterStore.reset();
+            return;
+        }
+        let value = Array.from(this.parse(params.stringValue));
+        if (!this.multiselect) {
+            value = value.slice(0, 1);
+        }
+        this.filterStore.setSelected(value);
+    };
+}
