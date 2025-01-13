a07f0ea7e1a1a1faba7726b59b54c97f67141c6c
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2025-01-13 12:38:07 +0100

    chore: move files

--- packages/shared/widget-plugin-filtering/src/typings/PickerFilterStore.ts
+++ packages/shared/widget-plugin-filtering/src/typings/PickerFilterStore.ts
@@ -1,4 +1,4 @@
-import { RefFilterStore } from "../stores/RefFilterStore";
+import { RefFilterStore } from "../stores/picker/RefFilterStore";
 import { StaticSelectFilterStore } from "../stores/picker/StaticSelectFilterStore";
 
 export type PickerFilterStore = RefFilterStore | StaticSelectFilterStore;
