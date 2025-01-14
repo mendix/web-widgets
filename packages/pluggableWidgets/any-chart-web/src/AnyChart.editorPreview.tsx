1a01ab829cf65aa346e80c59346ea63ae5cb4a45
Rahman Unver <rahmanunver@gmail.com>
2025-01-14 15:51:24 +0100

    feat(any-chart-web): styling, cleanup

--- packages/pluggableWidgets/any-chart-web/src/AnyChart.editorPreview.tsx
+++ packages/pluggableWidgets/any-chart-web/src/AnyChart.editorPreview.tsx
@@ -2,43 +2,43 @@ import { ReactElement, createElement } from "react";
 import { AnyChartPreviewProps } from "../typings/AnyChartProps";
 import AnyChart from "./AnyChart";
 
+const defaultSampleData = `[{
+    "type": "scatter",
+    "x": [1, 2, 3, 4, 5, 6, 7, 8],
+    "y": [5, 35, 15, 45, 25, 65, 30, 55],
+    "mode": "lines+markers",
+    "marker": { 
+        "color": "#264AE5",
+        "size": 8
+    },
+    "line": {
+        "width": 2
+    }
+}]`;
+
+const defaultSampleLayout = `{
+    "showlegend": false,
+    "margin": { "t": 40, "r": 10, "l": 40, "b": 40 },
+    "xaxis": {
+        "showgrid": true,
+        "zeroline": true,
+        "showline": true,
+        "title": "X Axis"
+    },
+    "yaxis": {
+        "showgrid": true,
+        "zeroline": true,
+        "showline": true,
+        "title": "Y Axis"
+    }
+}`;
+
+const defaultConfig = `{
+    "displayModeBar": false,
+    "staticPlot": true
+}`;
+
 export function preview(props: AnyChartPreviewProps): ReactElement {
-    const defaultSampleData = `[{
-        "type": "scatter",
-        "x": [1, 2, 3, 4, 5, 6, 7, 8],
-        "y": [5, 35, 15, 45, 25, 65, 30, 55],
-        "mode": "lines+markers",
-        "marker": { 
-            "color": "#264AE5",
-            "size": 8
-        },
-        "line": {
-            "width": 2
-        }
-    }]`;
-
-    const defaultSampleLayout = `{
-        "showlegend": false,
-        "margin": { "t": 40, "r": 10, "l": 40, "b": 40 },
-        "xaxis": {
-            "showgrid": true,
-            "zeroline": true,
-            "showline": true,
-            "title": "X Axis"
-        },
-        "yaxis": {
-            "showgrid": true,
-            "zeroline": true,
-            "showline": true,
-            "title": "Y Axis"
-        }
-    }`;
-
-    const defaultConfig = `{
-        "displayModeBar": false,
-        "staticPlot": true
-    }`;
-
     const containerProps = {
         name: "preview-chart",
         class: props.class,
