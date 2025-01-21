b560a94d789aade3f9d827b8d05215f973487a1f
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2025-01-21 11:17:05 +0100

    chore: add ref select

--- packages/pluggableWidgets/datagrid-dropdown-filter-web/src/components/StaticFilterContainer.tsx
+++ packages/pluggableWidgets/datagrid-dropdown-filter-web/src/components/StaticFilterContainer.tsx
@@ -25,7 +25,6 @@ export interface StaticFilterContainerProps {
     onChange?: ActionValue;
     parentChannelName: string | undefined;
     styles?: CSSProperties;
-    tabIndex?: number;
     valueAttribute?: EditableValue<string>;
 }
 
