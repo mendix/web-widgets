ee71d56b23564e41b57bf052904b6e6c2f524040
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2025-01-22 16:08:58 +0100

    chore: change static to mixins

--- packages/shared/widget-plugin-filtering/src/controllers/picker/StaticSelectController.ts
+++ packages/shared/widget-plugin-filtering/src/controllers/picker/StaticSelectController.ts
@@ -1,73 +1,9 @@
-import { UseSelectProps, useSelect } from "downshift";
-import { OptionWithState } from "../../typings/OptionWithState";
 import { StaticBaseController, StaticBaseControllerProps } from "./StaticBaseController";
+import { SelectControllerMixin } from "./mixins/SelectControllerMixin";
 
-const none = "[[__none__]]" as const;
-
-export class StaticSelectController extends StaticBaseController {
-    readonly emptyOption = {
-        value: none,
-        caption: "None",
-        selected: false
-    };
-
+export class StaticSelectController extends SelectControllerMixin(StaticBaseController) {
     constructor(props: StaticBaseControllerProps) {
         super(props);
         this.emptyOption.caption = props.emptyCaption || "None";
     }
-
-    get options(): OptionWithState[] {
-        return [this.emptyOption, ...this.filterStore.options];
-    }
-
-    get value(): string {
-        const selected = this.filterStore.selectedOptions;
-
-        if (selected.length < 1) {
-            return "Select";
-        }
-
-        return selected.map(option => option.caption).join(", ");
-    }
-
-    handleClear = (): void => {
-        this.filterStore.clear();
-    };
-
-    useSelectProps = (): UseSelectProps<OptionWithState> => {
-        const props: UseSelectProps<OptionWithState> = {
-            items: this.options,
-            itemToKey: item => item?.value,
-            itemToString: item => item?.caption ?? "",
-            onSelectedItemChange: ({ selectedItem }) => {
-                if (!selectedItem) {
-                    return;
-                }
-                if (selectedItem.value === none) {
-                    this.filterStore.clear();
-                } else if (this.multiselect) {
-                    this.filterStore.toggle(selectedItem.value);
-                } else {
-                    this.filterStore.setSelected([selectedItem.value]);
-                }
-            }
-        };
-
-        if (this.multiselect) {
-            props.stateReducer = (state, { changes, type }) => {
-                switch (type) {
-                    case useSelect.stateChangeTypes.ToggleButtonKeyDownEnter:
-                    case useSelect.stateChangeTypes.ItemClick:
-                        return {
-                            ...changes,
-                            isOpen: true,
-                            highlightedIndex: state.highlightedIndex
-                        };
-                    default:
-                        return changes;
-                }
-            };
-        }
-        return props;
-    };
 }
