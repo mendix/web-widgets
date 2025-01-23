6f01f32235ffce20ffcead25cd61afcbd75904a2
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2025-01-23 11:18:43 +0100

    feat: add column settings validation

--- packages/pluggableWidgets/datagrid-dropdown-filter-web/src/hocs/withSelectFilterAPI.tsx
+++ packages/pluggableWidgets/datagrid-dropdown-filter-web/src/hocs/withSelectFilterAPI.tsx
@@ -4,11 +4,11 @@ import { createElement } from "react";
 
 export { Select_FilterAPIv2 };
 
-export function withSelectFilterAPI<P extends object>(
+export function withSelectFilterAPI<P extends { filterable: boolean }>(
     Component: (props: P & Select_FilterAPIv2) => React.ReactElement
 ): (props: P) => React.ReactElement {
     return function FilterAPIProvider(props: P): React.ReactElement {
-        const api = useSelectFilterAPI("");
+        const api = useSelectFilterAPI(props);
         if (api.hasError) {
             return <Alert bootstrapStyle="danger">{api.error.message}</Alert>;
         }
