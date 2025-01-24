ff1260ca65509332500c9f33fd26183a5ac3f4e5
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2025-01-24 09:52:59 +0100

    chore: remove dead code

--- packages/shared/widget-plugin-filtering/src/controls.ts
+++ packages/shared/widget-plugin-filtering/src/controls.ts
@@ -1,4 +1,3 @@
 export { FilterSelector } from "./controls/filter-selector/FilterSelector";
 export { InputWithFilters, InputWithFiltersComponent } from "./controls/input/InputWithFilters";
 export * from "./controls/input/typings";
-export { Select } from "./controls/select/SelectComponent";
