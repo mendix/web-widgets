03fe0e289621db894737570d6f451b885a1da88c
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2025-02-01 18:54:31 +0100

    feat: add mobx-kit plugin

---
+++ packages/shared/widget-plugin-mobx-kit/src/main.ts
@@ -0,0 +1,4 @@
+export { autoEffect } from "./autoEffect";
+export { disposeBatch } from "./disposeBatch";
+export type { ReactiveController } from "./ReactiveController";
+export { ReactiveControllerHost } from "./ReactiveControllerHost";
