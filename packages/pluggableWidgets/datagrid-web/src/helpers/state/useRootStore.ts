ea9e4cb386467a446b655a0a8d7e06c0c38804a5
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2025-02-03 09:55:55 +0100

    chore: root changes

--- packages/pluggableWidgets/datagrid-web/src/helpers/state/useRootStore.ts
+++ packages/pluggableWidgets/datagrid-web/src/helpers/state/useRootStore.ts
@@ -1,23 +1,21 @@
-import { DatagridContainerProps } from "../../../typings/DatagridProps";
-import { useEffect, useRef, useState } from "react";
-import { RootGridStore } from "./RootGridStore";
 import { autorun, IReactionDisposer } from "mobx";
+import { useEffect, useRef, useState } from "react";
+import { DatagridContainerProps } from "../../../typings/DatagridProps";
+import { RootGridStore } from "./RootGridStore";
 
 export function useRootStore(props: DatagridContainerProps): RootGridStore {
+    const pref = useRef(props);
     const [rootStore] = useState(() => {
         return new RootGridStore(props);
     });
 
-    useEffect(() => {
-        const cleanup = rootStore.setup();
-        return () => {
-            rootStore.dispose();
-            cleanup?.();
-        };
-    }, [rootStore]);
+    useEffect(() => rootStore.setup(), [rootStore]);
 
     useEffect(() => {
+        compareObjects(pref.current, props);
+        console.log("same ref?", pref.current === props);
         rootStore.updateProps(props);
+        pref.current = props;
     });
 
     const datasourceRef = useRef(props.datasource);
@@ -46,4 +44,34 @@ export function useRootStore(props: DatagridContainerProps): RootGridStore {
     }, [rootStore]);
 
     return rootStore;
+}
+export function compareObjects(obj1: Record<string, any>, obj2: Record<string, any>): void {
+    const differences: Record<string, { obj1: any; obj2: any }> = {};
+
+    function compare(obj1: Record<string, any>, obj2: Record<string, any>, path: string = ""): void {
+        const allKeys = new Set([...Object.keys(obj1), ...Object.keys(obj2)]);
+
+        allKeys.forEach(key => {
+            const fullPath = path ? `${path}.${key}` : key;
+            if (
+                typeof obj1[key] === "object" &&
+                typeof obj2[key] === "object" &&
+                obj1[key] !== null &&
+                obj2[key] !== null
+            ) {
+                compare(obj1[key], obj2[key], fullPath);
+            } else if (obj1[key] !== obj2[key]) {
+                differences[fullPath] = { obj1: obj1[key], obj2: obj2[key] };
+            }
+        });
+    }
+
+    compare(obj1, obj2);
+
+    if (Object.keys(differences).length > 0) {
+        console.log("Differences found:");
+        console.table(differences);
+    } else {
+        console.log("No differences found.");
+    }
 }
