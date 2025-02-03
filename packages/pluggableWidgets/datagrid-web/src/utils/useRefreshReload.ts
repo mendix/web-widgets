7edec1aa0210957fc8efd9662fe5692f7bd40fc3
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2025-02-03 09:55:55 +0100

    chore: root changes

--- packages/pluggableWidgets/datagrid-web/src/utils/useRefreshReload.ts
+++ packages/pluggableWidgets/datagrid-web/src/utils/useRefreshReload.ts
@@ -1,34 +1,34 @@
-import { ListValue, ValueStatus } from "mendix";
-import { useEffect, useState } from "react";
+import { ListValue } from "mendix";
+import { useState } from "react";
 
 type UseRefreshReloadProps = {
     datasource: ListValue;
     refreshInterval: number;
 };
 
-export function useRefreshReload({ datasource, refreshInterval }: UseRefreshReloadProps): { isRefreshing: boolean } {
-    const [isRefreshing, setRefreshing] = useState(false);
+export function useRefreshReload(_: UseRefreshReloadProps): { isRefreshing: boolean } {
+    const [isRefreshing] = useState(false);
 
-    useEffect(() => {
-        let timer: ReturnType<typeof setTimeout>;
-        if (refreshInterval > 0) {
-            timer = setTimeout(() => {
-                datasource.reload();
-                setRefreshing(true);
-            }, refreshInterval * 1000);
-        }
-        return () => {
-            if (timer) {
-                clearTimeout(timer);
-            }
-        };
-    }, [datasource, refreshInterval]);
+    // useEffect(() => {
+    //     let timer: ReturnType<typeof setTimeout>;
+    //     if (refreshInterval > 0) {
+    //         timer = setTimeout(() => {
+    //             datasource.reload();
+    //             setRefreshing(true);
+    //         }, refreshInterval * 1000);
+    //     }
+    //     return () => {
+    //         if (timer) {
+    //             clearTimeout(timer);
+    //         }
+    //     };
+    // }, [datasource, refreshInterval]);
 
-    useEffect(() => {
-        if (datasource.status === ValueStatus.Available) {
-            setRefreshing(false);
-        }
-    }, [datasource]);
+    // useEffect(() => {
+    //     if (datasource.status === ValueStatus.Available) {
+    //         setRefreshing(false);
+    //     }
+    // }, [datasource]);
 
     return { isRefreshing };
 }
