842aea838c69aa10a37fb362463c383f3ff7e09d
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2025-02-03 15:30:19 +0100

    chore: change interfaces

--- packages/shared/widget-plugin-mobx-kit/src/react/useSetup.ts
+++ packages/shared/widget-plugin-mobx-kit/src/react/useSetup.ts
@@ -1,11 +1,11 @@
 import { useEffect } from "react";
 import { useConst } from "./useConst";
 
-export interface Setupable {
-    setup(): void | (() => void);
-}
-
-export function useSetup<T extends Setupable>(fn: () => T): T {
+export function useSetup<
+    T extends {
+        setup(): void | (() => void);
+    }
+>(fn: () => T): T {
     const obj = useConst(fn);
     useEffect(() => obj.setup(), [obj]);
     return obj;
