6aa6926445ae79d8945f7ddaf0b8f5184e7c1be0
gjulivan <grandjulivan@gmail.com>
2025-02-04 15:36:35 +0100

    chore(datagrid): extract common component

--- packages/shared/widget-plugin-filtering/src/controls/select/Select.tsx
+++ packages/shared/widget-plugin-filtering/src/controls/select/Select.tsx
@@ -3,8 +3,10 @@ import { useSelect, UseSelectProps } from "downshift";
 import { observer } from "mobx-react-lite";
 import React, { createElement, useRef } from "react";
 import { OptionWithState } from "../../typings/OptionWithState";
-import { Arrow, classes, Cross, ClearButton } from "../picker-primitives";
+import { ClearButton } from "../base/ClearButton";
+import { OptionsWrapper } from "../base/OptionsWrapper";
 import { useFloatingMenu } from "../hooks/useFloatingMenu";
+import { Arrow, classes } from "../picker-primitives";
 
 interface SelectProps {
     value: string;
@@ -50,54 +52,29 @@ export const Select = observer(function Select(props: SelectProps): React.ReactE
             >
                 {props.value}
             </button>
-            {showClear && (
-                <ClearButton
-                    className={cls.clear}
-                    onClick={() => {
-                        props.onClear();
-                        toggleRef.current?.focus();
-                    }}
-                >
-                    <Cross className={cls.clearIcon} />
-                </ClearButton>
-            )}
+            <ClearButton
+                cls={cls}
+                onClick={() => {
+                    props.onClear();
+                    toggleRef.current?.focus();
+                }}
+                showSeparator={false}
+                visible={showClear}
+            />
             <Arrow className={cls.stateIcon} />
-            <div className={cls.popover} hidden={!isOpen} ref={refs.setFloating} style={floatingStyles}>
-                <div className={cls.menuSlot}>
-                    <ul {...getMenuProps({ className: cls.menu })} onScroll={props.onMenuScroll}>
-                        {isOpen &&
-                            props.options.map((item, index) => (
-                                <li
-                                    data-selected={item.selected || undefined}
-                                    data-highlighted={highlightedIndex === index || undefined}
-                                    key={index}
-                                    className={cls.menuItem}
-                                    {...getItemProps({ item, index })}
-                                >
-                                    {showCheckboxes && (
-                                        <span className={cls.checkboxSlot}>
-                                            {index > 0 ? (
-                                                <input
-                                                    role="presentation"
-                                                    type="checkbox"
-                                                    checked={item.selected}
-                                                    value={item.caption}
-                                                    onChange={noop}
-                                                    tabIndex={-1}
-                                                />
-                                            ) : (
-                                                <div style={{ width: 16, height: 16 }} />
-                                            )}
-                                        </span>
-                                    )}
-                                    {item.caption}
-                                </li>
-                            ))}
-                    </ul>
-                </div>
-            </div>
+            <OptionsWrapper
+                cls={cls}
+                ref={refs.setFloating}
+                style={floatingStyles}
+                onMenuScroll={props.onMenuScroll}
+                isOpen={isOpen}
+                options={props.options}
+                highlightedIndex={highlightedIndex}
+                showCheckboxes={showCheckboxes}
+                haveEmptyFirstOption
+                getMenuProps={getMenuProps}
+                getItemProps={getItemProps}
+            />
         </div>
     );
 });
-
-const noop = (): void => {};
