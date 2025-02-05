8afda7df8b248361a9de184133f9a88181fe4f2c
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2025-02-05 11:42:33 +0100

    chore: convert hook to controller

--- packages/pluggableWidgets/custom-chart-web/src/CustomChart.tsx
+++ packages/pluggableWidgets/custom-chart-web/src/CustomChart.tsx
@@ -1,16 +1,19 @@
+import { useSetup } from "@mendix/widget-plugin-mobx-kit/react/useSetup";
 import { ReactElement, createElement } from "react";
 import { CustomChartContainerProps } from "../typings/CustomChartProps";
 import { useCustomChart } from "./hooks/useCustomChart";
 import { useActionEvents } from "./hooks/useActionEvents";
 import "./ui/CustomChart.scss";
+import { Host } from "./controllers/Host";
 
 export default function CustomChart(props: CustomChartContainerProps): ReactElement {
+    const host = useSetup(() => new Host());
     const { chartRef, containerStyle } = useCustomChart(props);
     const { handleClick } = useActionEvents(props);
 
     return (
         <div
-            ref={chartRef}
+            ref={host.resizeCtrl.setTarget}
             className="widget-custom-chart"
             style={containerStyle}
             tabIndex={props.tabIndex}
