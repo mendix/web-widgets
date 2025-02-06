e9d8c69bbf12294b0b83cacb1efcef82822a4b50
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2025-02-06 18:54:07 +0100

    chore: finalize refresh

--- packages/pluggableWidgets/datagrid-web/src/controllers/query-controller.ts
+++ packages/pluggableWidgets/datagrid-web/src/controllers/query-controller.ts
@@ -1,10 +1,18 @@
 import { ListValue } from "mendix";
 
-type Members = "setOffset" | "setSortOrder" | "setFilter" | "requestTotalCount" | "totalCount" | "limit" | "offset";
+type Members =
+    | "setOffset"
+    | "setSortOrder"
+    | "setFilter"
+    | "setLimit"
+    | "requestTotalCount"
+    | "totalCount"
+    | "limit"
+    | "offset";
 
 export interface QueryController extends Pick<ListValue, Members> {
     refresh(): void;
-    setLimit(limit: number, options?: { loadingMore?: boolean }): void;
+    setPageSize(size: number): void;
     isLoading: boolean;
     isRefreshing: boolean;
     isLoadingMore: boolean;
