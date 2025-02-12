63e79db4fdce6a8eb0bda48912583f8f32271267
leonardomendix <leonardo.de.souza@mendix.com>
2025-02-12 16:11:55 +0100

    test: change update test project e2e

--- automation/run-e2e/lib/config.mjs
+++ automation/run-e2e/lib/config.mjs
@@ -3,13 +3,13 @@ export const testProjectDir = "tests/testProject";
 export const postUnzipProjectDirGlob = "tests/testProjects-*";
 export const mprFileGlob = "tests/testProject/*.mpr";
 export const nameForDownloadedArchive = "testProject.zip";
-export const nameForDownloadedStarterApp = "StarterAppRelease.zip";
-export const starterAppLatestReleaseUrl = "https://api.github.com/repos/mendix/StarterApp_Blank/releases/latest";
+export const nameForDownloadedAtlasCore = "AtlasCore.zip";
+export const nameForDownloadedAtlasTheme = "AtlasTheme.zip";
+export const atlasCoreReleaseUrl = "https://api.github.com/repos/mendix/atlas/releases";
 export const mxVersionMapUrl =
     "https://raw.githubusercontent.com/mendix/web-widgets/main/automation/run-e2e/mendix-versions.json";
 export const tmpDirPrefix = "run_e2e_files_";
 export const atlasDirsToRemove = [
-    "tests/testProject/theme",
     "tests/testProject/themesource/atlas_ui_resources",
     "tests/testProject/themesource/atlas_core",
     "tests/testProject/themesource/atlas_nativemobile_content",
