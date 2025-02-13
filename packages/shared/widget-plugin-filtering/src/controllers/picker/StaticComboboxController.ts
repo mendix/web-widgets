3e5a2eefb56f8afa639df509c92c9967b8be1b0c
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2025-02-13 13:26:26 +0100

    fix: add select placeholder

--- packages/shared/widget-plugin-filtering/src/controllers/picker/StaticComboboxController.ts
+++ packages/shared/widget-plugin-filtering/src/controllers/picker/StaticComboboxController.ts
@@ -4,6 +4,6 @@ import { ComboboxControllerMixin } from "./mixins/ComboboxControllerMixin";
 export class StaticComboboxController extends ComboboxControllerMixin(StaticBaseController) {
     constructor(props: StaticBaseControllerProps) {
         super({ ...props, multiselect: false });
-        this.inputPlaceholder = props.inputPlaceholder ?? "Search";
+        this.inputPlaceholder = props.placeholder ?? "Search";
     }
 }
