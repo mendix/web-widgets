5b361ed381dd178a94073221e721a212e47138d5
Samuel Reichert <samuelreichertt@gmail.com>
2025-02-20 09:41:26 +0100

    feat(custom-chart): split ChartController into two controllers

--- packages/pluggableWidgets/custom-chart-web/src/controllers/CustomChartControllerHost.ts
+++ packages/pluggableWidgets/custom-chart-web/src/controllers/CustomChartControllerHost.ts
@@ -1,15 +1,29 @@
+import { EditorStoreState } from "@mendix/shared-charts/main";
 import { BaseControllerHost } from "@mendix/widget-plugin-mobx-kit/BaseControllerHost";
+import { DerivedPropsGate } from "@mendix/widget-plugin-mobx-kit/props-gate";
 import { ResizeController } from "./ResizeController";
-import { ChartController } from "./ChartController";
-import { CustomChartContainerProps } from "../../typings/CustomChartProps";
+import { ChartPropsController } from "./ChartPropsController";
+import { ControllerProps } from "./typings";
+import { PlotlyController } from "./PlotlyController";
+
+interface CustomChartControllerHostSpec {
+    propsGate: DerivedPropsGate<ControllerProps>;
+    editorStateGate: DerivedPropsGate<EditorStoreState>;
+}
 
 export class CustomChartControllerHost extends BaseControllerHost {
     resizeCtrl: ResizeController;
-    chartCtrl: ChartController;
+    chartPropsController: ChartPropsController;
+    plotlyController: PlotlyController;
 
-    constructor(props: CustomChartContainerProps) {
+    constructor(spec: CustomChartControllerHostSpec) {
         super();
         this.resizeCtrl = new ResizeController(this);
-        this.chartCtrl = new ChartController(this, props);
+        this.chartPropsController = new ChartPropsController(this, {
+            propsGate: spec.propsGate,
+            sizeProvider: this.resizeCtrl,
+            editorStateGate: spec.editorStateGate
+        });
+        this.plotlyController = new PlotlyController({ propsProvider: this.chartPropsController });
     }
 }
