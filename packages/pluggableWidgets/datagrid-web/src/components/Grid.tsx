2c6f1310c25960b44a5a5f8b1b935c67fd59f176
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2025-02-24 15:48:23 +0100

    fix(datagrid-web): fallback to previous css layout

--- packages/pluggableWidgets/datagrid-web/src/components/Grid.tsx
+++ packages/pluggableWidgets/datagrid-web/src/components/Grid.tsx
@@ -1,15 +1,35 @@
 import classNames from "classnames";
 import { ReactElement, createElement } from "react";
+import { PaginationEnum } from "../../typings/DatagridProps";
+import { useInfiniteControl } from "@mendix/widget-plugin-grid/components/InfiniteBody";
 
 type P = Omit<JSX.IntrinsicElements["div"], "role" | "ref">;
 
-export interface GridProps extends P {}
+export interface GridProps extends P {
+    paginationType: PaginationEnum;
+    className?: string;
+    hasMoreItems: boolean;
+    setPage?: (update: (page: number) => number) => void;
+}
 
 export function Grid(props: GridProps): ReactElement {
-    const { children, className, ...rest } = props;
+    const { className, style, paginationType, hasMoreItems, setPage, children, ...rest } = props;
+    const isInfinite = paginationType === "virtualScrolling";
+    const [trackScrolling, bodySize, containerRef] = useInfiniteControl({
+        hasMoreItems,
+        isInfinite,
+        setPage
+    });
 
     return (
-        <div className={classNames("widget-datagrid-grid table", className)} role="grid" {...rest}>
+        <div
+            className={classNames("widget-datagrid-grid table", { "infinite-loading": isInfinite }, className)}
+            role="grid"
+            {...rest}
+            ref={containerRef}
+            style={isInfinite && bodySize > 0 ? { ...style, maxHeight: bodySize } : style}
+            onScroll={isInfinite ? trackScrolling : undefined}
+        >
             {children}
         </div>
     );
