3b4a00a5a531253de8d80257e9d23a9cdc9ede25
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2025-02-28 10:14:13 +0100

    feat: add filter observer

---
+++ packages/shared/widget-plugin-filtering/src/stores/generic/CustomFilterHost.ts
@@ -0,0 +1,29 @@
+import { FilterCondition } from "mendix/filters";
+import { Filter, FilterObserver } from "../../typings/FilterObserver";
+import { FiltersSettingsMap } from "../../typings/settings";
+
+export class CustomFilterHost implements FilterObserver {
+    private filters: Map<string, Filter> = new Map();
+
+    get settings(): FiltersSettingsMap<string> {
+        return new Map([...this.filters].map(([key, filter]) => [key, filter.toJSON()]));
+    }
+
+    set settings(data: FiltersSettingsMap<string>) {
+        for (const [key, value] of data) {
+            this.filters.get(key)?.fromJSON(value);
+        }
+    }
+
+    get condition(): Array<FilterCondition | undefined> {
+        return [...this.filters.values()].map(filter => filter.condition);
+    }
+
+    observe(key: string, filter: Filter): void {
+        this.filters.set(key, filter);
+    }
+
+    unobserve(key: string): void {
+        this.filters.delete(key);
+    }
+}
