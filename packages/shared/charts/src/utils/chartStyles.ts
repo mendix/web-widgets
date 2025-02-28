873efc4577e5d5849de42c21171a3a76e0270584
Rahman Unver <rahmanunver@gmail.com>
2025-02-28 16:34:07 +0100

    feat(custom-chart-web): remove containerprops from customchart

--- packages/shared/charts/src/utils/chartStyles.ts
+++ packages/shared/charts/src/utils/chartStyles.ts
@@ -1,11 +1,28 @@
 import { CSSProperties } from "react";
-import { CustomChartContainerProps } from "../../../../pluggableWidgets/custom-chart-web/typings/CustomChartProps";
+
+export type WidthUnitEnum = "pixels" | "percentage";
+export type HeightUnitEnum = "percentageOfWidth" | "pixels" | "percentageOfParent" | "percentageOfView";
+export type MinHeightUnitEnum = "none" | "pixels" | "percentageOfParent" | "percentageOfView";
+export type MaxHeightUnitEnum = "none" | "pixels" | "percentageOfParent" | "percentageOfView";
+export type OverflowYEnum = "auto" | "scroll" | "hidden";
+
+export interface ChartContainerProps {
+    widthUnit: WidthUnitEnum;
+    width: number;
+    heightUnit: HeightUnitEnum;
+    height: number;
+    minHeightUnit: MinHeightUnitEnum;
+    minHeight: number;
+    maxHeightUnit: MaxHeightUnitEnum;
+    maxHeight: number;
+    OverflowY: OverflowYEnum;
+}
 
 function getHeightScale(height: number, heightUnit: "pixels" | "percentageOfParent" | "percentageOfView"): string {
     return `${height}${heightUnit === "pixels" ? "px" : heightUnit === "percentageOfView" ? "vh" : "%"}`;
 }
 
-export function constructWrapperStyle(props: CustomChartContainerProps): CSSProperties {
+export function constructWrapperStyle<T extends ChartContainerProps>(props: T): CSSProperties {
     const { widthUnit, heightUnit, minHeightUnit, maxHeightUnit, width, height, minHeight, maxHeight, OverflowY } =
         props;
 
