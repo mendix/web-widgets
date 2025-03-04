90e9c5a8beff4247a9b789119fa02824d1d9cb86
Samuel Reichert <samuelreichertt@gmail.com>
2025-03-04 09:37:32 +0100

    fix(table-better): update how svg files are added to DOM

--- packages/pluggableWidgets/rich-text-web/src/utils/customPluginRegisters.ts
+++ packages/pluggableWidgets/rich-text-web/src/utils/customPluginRegisters.ts
@@ -37,9 +37,4 @@ Quill.register(Button, true);
 // add empty handler for view code, this format is handled by toolbar's custom config via ViewCodeDialog
 Quill.register({ "ui/view-code": Empty });
 // Quill.register(MxTableCell, true);
-Quill.register(
-    {
-        "modules/table-better": QuillTableBetter
-    },
-    true
-);
+Quill.register({ "modules/table-better": QuillTableBetter }, true);
