8ea4f80e8fb150e02cca0a5ab6fb1b093dcd195e
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2025-03-04 10:54:54 +0100

    feat: add state persistence

--- packages/shared/widget-plugin-filtering/src/stores/generic/CustomFilterHost.ts
+++ packages/shared/widget-plugin-filtering/src/stores/generic/CustomFilterHost.ts
@@ -1,18 +1,23 @@
 import { FilterCondition } from "mendix/filters";
+import { autorun, makeAutoObservable } from "mobx";
 import { Filter, FilterObserver } from "../../typings/FilterObserver";
 import { FiltersSettingsMap } from "../../typings/settings";
 
 export class CustomFilterHost implements FilterObserver {
     private filters: Map<string, Filter> = new Map();
+    private settingsBuffer: FiltersSettingsMap<string> = new Map();
+    private disposeMap: Map<string, () => void> = new Map();
+
+    constructor() {
+        makeAutoObservable(this);
+    }
 
     get settings(): FiltersSettingsMap<string> {
         return new Map([...this.filters].map(([key, filter]) => [key, filter.toJSON()]));
     }
 
     set settings(data: FiltersSettingsMap<string>) {
-        for (const [key, value] of data) {
-            this.filters.get(key)?.fromJSON(value);
-        }
+        this.settingsBuffer = data;
     }
 
     get conditions(): Array<FilterCondition | undefined> {
@@ -20,10 +25,18 @@ export class CustomFilterHost implements FilterObserver {
     }
 
     observe(key: string, filter: Filter): void {
+        const dispose = autorun(() => {
+            if (this.settingsBuffer.has(key)) {
+                filter.fromJSON(this.settingsBuffer.get(key));
+            }
+        });
+        this.disposeMap.set(key, dispose);
         this.filters.set(key, filter);
     }
 
     unobserve(key: string): void {
+        this.disposeMap.get(key)?.();
+        this.disposeMap.delete(key);
         this.filters.delete(key);
     }
 }
