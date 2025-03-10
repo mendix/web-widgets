2f7b233ff179ac30a90dff7e091a44f0930f5d7e
gjulivan <grandjulivan@gmail.com>
2025-03-10 07:47:09 +0100

    build(combobox-web): turbo.json config for running create-translation before release

--- packages/pluggableWidgets/combobox-web/rollup.config.js
+++ packages/pluggableWidgets/combobox-web/rollup.config.js
@@ -1,22 +1,20 @@
 const { join } = require("path");
-const { cp, mkdir } = require("shelljs");
+const { cp, mkdir, rm } = require("shelljs");
 
 const sourcePath = process.cwd();
 const outDir = join(sourcePath, "/dist/tmp/widgets/");
-const widgetPackageJson = require(join(sourcePath, "package.json"));
-const widgetName = widgetPackageJson.widgetName;
-const widgetPackage = widgetPackageJson.packagePath;
-const outWidgetDir = join(widgetPackage.replace(/\./g, "/"), widgetName.toLowerCase());
-const absoluteOutPackageDir = join(outDir, outWidgetDir);
 
 module.exports = args => {
     const result = args.configDefaultConfig;
 
-    const localesDir = join(absoluteOutPackageDir, "locales");
+    const localesDir = join(outDir, "locales/");
     mkdir("-p", localesDir);
 
     const translationFiles = join(sourcePath, "dist/locales/**/*");
+    // copy everything under dist/locales to dist/tmp/widgets/locales for the widget mpk
     cp("-r", translationFiles, localesDir);
+    // remove root level *.json locales files (duplicate with language specific files (e.g. en-US/*.json))
+    rm("-f", join(outDir, "locales/*.json"), localesDir);
 
     return result;
 };
