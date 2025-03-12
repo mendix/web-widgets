019f3955b38f3f4dcc4c7b05ce89ce08e4f914ad
Rahman Unver <rahmanunver@gmail.com>
2025-03-12 22:07:55 +0100

    feat(rich-text-web): clean styles, icon

--- packages/pluggableWidgets/rich-text-web/src/assets/Icons.tsx
+++ packages/pluggableWidgets/rich-text-web/src/assets/Icons.tsx
@@ -99,3 +99,21 @@ export function IconLowerAlpha(): ReactElement {
         </span>
     );
 }
+
+export const FullscreenIcon = () => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            width="18"
+            height="18"
+            fill="none"
+            stroke="#444"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+        >
+            <path d="M8 3H5a2 2 0 0 0-2 2v3m18 0V5a2 2 0 0 0-2-2h-3m0 18h3a2 2 0 0 0 2-2v-3M3 16v3a2 2 0 0 0 2 2h3" />
+        </svg>
+    );
+};
