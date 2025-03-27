8b801aefddadd084c95e23255dddd7a5cbee3b63
Samuel Reichert <samuelreichertt@gmail.com>
2025-03-27 16:37:06 +0100

    feat(rich-text): update rollup config for table-better support

--- packages/pluggableWidgets/rich-text-web/rollup.config.js
+++ packages/pluggableWidgets/rich-text-web/rollup.config.js
@@ -1,9 +1,24 @@
+import typescript from "@rollup/plugin-typescript";
 import preserveDirectives from "rollup-preserve-directives";
 
 export default args => {
     const result = args.configDefaultConfig;
     return result.map((config, _index) => {
-        config.plugins = [...config.plugins, preserveDirectives()];
+        const newPlugins = config.plugins.map(plugin => {
+            if (plugin && plugin.name === "typescript") {
+                return typescript({
+                    noEmitOnError: !args.watch,
+                    sourceMap: config.sourceMaps,
+                    inlineSources: config.sourceMaps,
+                    target: "es2022", // we transpile the result with babel anyway, see below
+                    useDefineForClassFields: false,
+                    exclude: ["**/__tests__/**/*"]
+                });
+            }
+            return plugin;
+        });
+        config.plugins = [...newPlugins, preserveDirectives()];
+
         return config;
     });
 };
