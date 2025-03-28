579f5a7e5475e061923130fe2da61ee3c8920479
gjulivan <grandjulivan@gmail.com>
2025-03-28 10:22:34 +0100

    fix: based on initial testing feedback

--- packages/pluggableWidgets/rich-text-web/src/__mocks__/quill-resize-module.ts
+++ packages/pluggableWidgets/rich-text-web/src/__mocks__/quill-resize-module.ts
@@ -1,5 +1,14 @@
 // eslint-disable-next-line @typescript-eslint/no-extraneous-class
+export class QuillResizeToolbar {
+    constructor() {
+        return this;
+    }
+}
+
 export default class QuillResize {
+    static Modules = {
+        Toolbar: QuillResizeToolbar
+    };
     constructor() {
         return this;
     }
