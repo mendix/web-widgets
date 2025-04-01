24f23b5b12239f2922f4cef960cb009b5e75f22e
gjulivan <grandjulivan@gmail.com>
2025-04-02 00:38:10 +0200

    feat(docviewer): add docx support

---
+++ packages/pluggableWidgets/document-viewer-web/components/ErrorViewer.tsx
@@ -0,0 +1,17 @@
+import { createElement, Fragment, useContext } from "react";
+import { DocumentContext } from "store";
+import { DocRendererElement } from "./documentRenderer";
+
+const ErrorViewer: DocRendererElement = () => {
+    const props = useContext(DocumentContext);
+    console.log("ErrorViewer", props);
+    return (
+        <Fragment>
+            <div className="widget-document-viewer-controls">No document selected</div>
+        </Fragment>
+    );
+};
+
+ErrorViewer.contentTypes = [];
+
+export default ErrorViewer;
---
+++ packages/pluggableWidgets/document-viewer-web/utils/useRendererSelector.ts
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+
+import { DocumentRenderers } from "../components";
+import { DocRendererElement } from "../components/documentRenderer";
+import ErrorViewer from "../components/ErrorViewer";
+import { DocumentViewerContainerProps } from "../typings/DocumentViewerProps";
+
+interface DocumentRenderer {
+    CurrentRenderer: DocRendererElement;
+}
+
+export function useRendererSelector(props: DocumentViewerContainerProps): DocumentRenderer {
+    const { file } = props;
+    const [component, setComponent] = useState<DocRendererElement>(() => ErrorViewer);
+    useEffect(() => {
+        const controller = new AbortController();
+        const { signal } = controller;
+        if (file.status === "available" && file.value.uri) {
+            fetch(file.value.uri, { method: "HEAD", signal }).then(response => {
+                const contentTypeRaw = response.headers.get("content-type");
+                const contentTypes = contentTypeRaw?.split(";") || [];
+                const contentType = contentTypes.length ? contentTypes[0] : undefined;
+
+                if (contentType) {
+                    const selectedRenderer: DocRendererElement[] = [];
+                    DocumentRenderers.forEach(renderer => {
+                        if (renderer.contentTypes.includes(contentType)) {
+                            selectedRenderer.push(renderer);
+                        }
+                    });
+                    if (selectedRenderer.length > 0) {
+                        setComponent(() => selectedRenderer[0]);
+                    }
+                }
+            });
+        }
+
+        return () => {
+            controller.abort();
+        };
+    }, [file, file?.status, file?.value?.uri]);
+    return { CurrentRenderer: component };
+}
