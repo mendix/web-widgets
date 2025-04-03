de52bfd58a001ab0b66fcd13e98df9c05fedccb1
gjulivan <grandjulivan@gmail.com>
2025-04-03 11:22:53 +0200

    fix: based on test feedback

--- packages/pluggableWidgets/rich-text-web/src/components/CustomToolbars/Fullscreen.tsx
+++ packages/pluggableWidgets/rich-text-web/src/components/CustomToolbars/Fullscreen.tsx
@@ -1,13 +1,23 @@
-import { createElement, ReactElement, useCallback } from "react";
+import { createElement, ReactElement, useCallback, useContext } from "react";
 import { ToolbarButton } from "./ToolbarWrapper";
 import { CustomToolbarProps } from "./customToolbars";
 import { ACTION_DISPATCHER } from "../../utils/helpers";
 import { SET_FULLSCREEN_ACTION } from "../../store/store";
+import { EditorContext } from "../../store/EditorProvider";
+import classNames from "classnames";
 
 export function FullscreenButton({ quill }: CustomToolbarProps): ReactElement {
     const handleClick = useCallback(() => {
         quill?.emitter.emit(ACTION_DISPATCHER, { type: SET_FULLSCREEN_ACTION });
     }, [quill]);
 
-    return <ToolbarButton className={"icons icon-Expand"} title={"fullscreen"} onClick={handleClick} />;
+    const { isFullscreen } = useContext(EditorContext);
+
+    return (
+        <ToolbarButton
+            className={classNames(`icons icon-Expand`, { "ql-active": isFullscreen })}
+            title={"Fullscreen"}
+            onClick={handleClick}
+        />
+    );
 }
