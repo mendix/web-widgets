5b9afa6c7859b1a24879461bfb300e579321d6f8
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2025-04-04 11:32:14 +0200

    feat: add date store provider

---
+++ packages/shared/widget-plugin-filtering/src/custom-filter-api/BaseStoreProvider.ts
@@ -0,0 +1,32 @@
+import { disposeBatch } from "@mendix/widget-plugin-mobx-kit/disposeBatch";
+import { ISetupable } from "@mendix/widget-plugin-mobx-kit/setupable";
+import { FilterCondition } from "mendix/filters";
+import { isAnd, isTag } from "../condition-utils";
+import { FilterAPI } from "../context";
+import { Filter } from "../typings/FilterObserver";
+
+export abstract class BaseStoreProvider<S extends Filter> implements ISetupable {
+    protected abstract _store: S;
+    protected abstract filterAPI: FilterAPI;
+    abstract readonly dataKey: string;
+
+    protected findInitFilter(conditions: Array<FilterCondition | undefined>, key: string): FilterCondition | null {
+        for (const cond of conditions) {
+            if (cond && isAnd(cond)) {
+                const [tag, initFilter] = cond.args;
+                if (isTag(tag) && tag.arg1.value === key) {
+                    return initFilter;
+                }
+            }
+        }
+        return null;
+    }
+
+    setup(): () => void {
+        const [add, disposeAll] = disposeBatch();
+        this.filterAPI.filterObserver.observe(this.dataKey, this._store);
+        add(() => this.filterAPI.filterObserver.unobserve(this.dataKey));
+        add(this._store.setup?.());
+        return disposeAll;
+    }
+}
