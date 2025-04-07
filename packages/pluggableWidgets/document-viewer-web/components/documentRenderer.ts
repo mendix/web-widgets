471a346db581ab5e10099dba329f6d5963b25099
gjulivan <grandjulivan@gmail.com>
2025-04-07 16:01:06 +0200

    chore(docviewer): cleanup mammoth js

--- packages/pluggableWidgets/document-viewer-web/components/documentRenderer.ts
+++ packages/pluggableWidgets/document-viewer-web/components/documentRenderer.ts
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import { DocumentViewerContainerProps } from "typings/DocumentViewerProps";
+import { DocumentViewerContainerProps } from "../typings/DocumentViewerProps";
 
 export interface DocRendererElement extends FC<DocumentViewerContainerProps> {
     contentTypes: string[];
