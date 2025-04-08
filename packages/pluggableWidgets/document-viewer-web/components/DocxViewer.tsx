d199f09bb3ce3b864ad110f55f4a710939721c89
gjulivan <grandjulivan@gmail.com>
2025-04-08 11:02:17 +0200

    chore(doc-viewer): reintroduce mammoth

--- packages/pluggableWidgets/document-viewer-web/components/DocxViewer.tsx
+++ packages/pluggableWidgets/document-viewer-web/components/DocxViewer.tsx
@@ -1,9 +1,54 @@
-import { createElement, Fragment } from "react";
-import { DocumentViewerContainerProps } from "../typings/DocumentViewerProps";
+import { createElement, Fragment, useCallback, useEffect, useState } from "react";
+import mammoth from "mammoth";
+import { DocumentViewerContainerProps } from "typings/DocumentViewerProps";
 import { DocRendererElement } from "./documentRenderer";
 
-const DocxViewer: DocRendererElement = (_props: DocumentViewerContainerProps) => {
-    return <Fragment>DOCX</Fragment>;
+const DocxViewer: DocRendererElement = (props: DocumentViewerContainerProps) => {
+    const { file } = props;
+    const [docxHtml, setDocxHtml] = useState<string | null>(null);
+
+    const loadContent = useCallback(async (arrayBuffer: any) => {
+        try {
+            mammoth
+                .convertToHtml(
+                    { arrayBuffer: arrayBuffer },
+                    {
+                        includeDefaultStyleMap: true
+                    }
+                )
+                .then(result => {
+                    if (result) {
+                        setDocxHtml(result.value);
+                    }
+                });
+        } catch (error) {}
+    }, []);
+
+    useEffect(() => {
+        const controller = new AbortController();
+        const { signal } = controller;
+        if (file.status === "available" && file.value.uri) {
+            fetch(file.value.uri, { method: "GET", signal })
+                .then(res => res.arrayBuffer())
+                .then(response => {
+                    loadContent(response);
+                });
+        }
+
+        return () => {
+            controller.abort();
+        };
+    }, [file, file?.status, file?.value?.uri]);
+
+    return (
+        <Fragment>
+            {docxHtml && (
+                <div className="widget-document-viewer-content" dangerouslySetInnerHTML={{ __html: docxHtml }}>
+                    {/* {docHtmlStr} */}
+                </div>
+            )}
+        </Fragment>
+    );
 };
 
 DocxViewer.contentTypes = [
--- packages/pluggableWidgets/document-viewer-web/components/ErrorViewer.tsx
+++ packages/pluggableWidgets/document-viewer-web/components/ErrorViewer.tsx
@@ -4,7 +4,6 @@ import { DocRendererElement } from "./documentRenderer";
 
 const ErrorViewer: DocRendererElement = () => {
     const props = useContext(DocumentContext);
-    console.log("ErrorViewer", props);
     return (
         <Fragment>
             <div className="widget-document-viewer-content">No document selected</div>
