708f8015c4af774f8b2daa7a902868d0a9269b76
Samuel Reichert <samuelreichertt@gmail.com>
2025-04-08 12:01:42 +0200

    test(accordion-web): update test cases to use only react-testing-library

--- packages/pluggableWidgets/accordion-web/src/components/__tests__/Icon.spec.tsx
+++ packages/pluggableWidgets/accordion-web/src/components/__tests__/Icon.spec.tsx
@@ -1,65 +1,73 @@
+import "@testing-library/jest-dom";
+import { render, RenderResult } from "@testing-library/react";
 import { createElement } from "react";
-import { mount, render } from "enzyme";
 import { Icon, IconProps } from "../Icon";
 
 describe("Icon", () => {
-    let defaultIconProps: IconProps;
+    const defaultIconProps: IconProps = {
+        data: { type: "glyph", iconClass: "icon-class" },
+        loading: false,
+        animate: true
+    };
 
-    beforeEach(() => {
-        defaultIconProps = {
-            data: { type: "glyph", iconClass: "icon-class" },
-            loading: false,
-            animate: true
-        };
+    function renderIcon(props: Partial<IconProps> = {}): RenderResult {
+        return render(<Icon {...defaultIconProps} {...props} />);
+    }
+
+    describe("Rendering", () => {
+        it("renders glyph icons", () => {
+            const icon = renderIcon();
+
+            expect(icon.container).toMatchSnapshot();
+        });
+
+        it("renders image icons", () => {
+            const icon = renderIcon({ data: { type: "image", iconUrl: "icon.url" } });
+
+            expect(icon.container).toMatchSnapshot();
+        });
+
+        it("renders a default icon", () => {
+            const icon = renderIcon({ data: undefined });
+
+            expect(icon.container).toMatchSnapshot();
+        });
+
+        it("doesn't render a default icon while loading", () => {
+            const icon = renderIcon({ data: undefined, loading: true });
+
+            expect(icon.container).toMatchSnapshot();
+        });
+
+        it("doesn't render an icon with an unknown icon data type", () => {
+            const icon = renderIcon({ data: { type: "unknown" } as any });
+
+            expect(icon.container).toMatchSnapshot();
+        });
     });
 
-    it("renders glyph icons", () => {
-        const iconWrapper = render(<Icon {...defaultIconProps} />);
+    describe("Animation Behaviour", () => {
+        it("doesn't apply an animate class to a glyph icon when animate is false", () => {
+            const icon = renderIcon({ animate: false });
+            const spanElement = icon.container.querySelector("span");
 
-        expect(iconWrapper).toMatchSnapshot();
-    });
+            expect(spanElement).not.toHaveClass("widget-accordion-group-header-icon-animate");
+        });
 
-    it("renders image icons", () => {
-        const iconWrapper = render(<Icon {...defaultIconProps} data={{ type: "image", iconUrl: "icon.url" }} />);
+        it("doesn't apply an animate class to an image icon when animate is false", () => {
+            const icon = renderIcon({
+                data: { type: "image", iconUrl: "icon.url" },
+                animate: false
+            });
+            const imgElement = icon.container.querySelector("img");
 
-        expect(iconWrapper).toMatchSnapshot();
-    });
+            expect(imgElement).not.toHaveClass("widget-accordion-group-header-icon-animate");
+        });
 
-    it("renders a default icon", () => {
-        const iconWrapper = render(<Icon {...defaultIconProps} data={undefined} />);
-
-        expect(iconWrapper).toMatchSnapshot();
-    });
-
-    it("doesn't render a default icon while loading", () => {
-        const iconWrapper = render(<Icon {...defaultIconProps} data={undefined} loading />);
-
-        expect(iconWrapper).toMatchSnapshot();
-    });
-
-    it("doesn't render an icon with an unknown icon data type", () => {
-        const iconWrapper = render(<Icon {...defaultIconProps} data={{ type: "unknown" } as any} />);
-
-        expect(iconWrapper).toMatchSnapshot();
-    });
-
-    it("doesn't apply an animate class to a glyph icon when animate is false", () => {
-        const iconWrapper = mount(<Icon {...defaultIconProps} animate={false} />);
-
-        expect(iconWrapper.find("span").prop("className")).not.toContain("widget-accordion-group-header-icon-animate");
-    });
-
-    it("doesn't apply an animate class to an image icon when animate is false", () => {
-        const iconWrapper = mount(
-            <Icon {...defaultIconProps} data={{ type: "image", iconUrl: "icon.url" }} animate={false} />
-        );
-
-        expect(iconWrapper.find("img").prop("className")).not.toContain("widget-accordion-group-header-icon-animate");
-    });
-
-    it("doesn't apply an animate class to a default icon when animate is false", () => {
-        const iconWrapper = mount(<Icon {...defaultIconProps} data={undefined} animate={false} />);
-
-        expect(iconWrapper.find("svg").prop("className")).not.toContain("widget-accordion-group-header-icon-animate");
+        it("doesn't apply an animate class to a default icon when animate is false", () => {
+            const icon = renderIcon({ data: undefined, animate: false });
+            const svgElement = icon.container.querySelector("svg");
+            expect(svgElement).not.toHaveClass("widget-accordion-group-header-icon-animate");
+        });
     });
 });
