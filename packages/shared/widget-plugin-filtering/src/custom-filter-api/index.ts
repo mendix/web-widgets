f896ff04d4ec73f5148379520725a50408f9686a
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2025-04-11 12:36:20 +0200

    feat: add new set of properties

---
+++ packages/shared/widget-plugin-filtering/src/custom-filter-api/index.ts
@@ -0,0 +1,6 @@
+export * from "./BaseStoreProvider";
+export * from "./DateStoreProvider";
+export * from "./DropdownStoreProvider";
+export * from "./NumberStoreProvider";
+export * from "./StringStoreProvider";
+export * from "./typings";
