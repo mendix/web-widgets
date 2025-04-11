9cbd69f4b15bad54ab92a1ce509766d615ecc013
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2025-04-11 12:36:20 +0200

    feat: add new set of properties

---
+++ packages/pluggableWidgets/datagrid-dropdown-filter-web/src/hocs/withDropdownLinkedAttributes.tsx
@@ -0,0 +1,71 @@
+import { createElement } from "react";
+import { AttributeMetaData } from "mendix";
+import { useFilterAPI } from "@mendix/widget-plugin-filtering/context";
+import { APIError } from "@mendix/widget-plugin-filtering/errors";
+import { error, value, Result } from "@mendix/widget-plugin-filtering/result-meta";
+import { PickerFilterStore } from "@mendix/widget-plugin-filtering/typings/PickerFilterStore";
+import { Alert } from "@mendix/widget-plugin-component-kit/Alert";
+import { useConst } from "@mendix/widget-plugin-mobx-kit/react/useConst";
+import { useSetup } from "@mendix/widget-plugin-mobx-kit/react/useSetup";
+import { ISetupable } from "@mendix/widget-plugin-mobx-kit/setupable";
+import { DropdownStoreProvider } from "@mendix/widget-plugin-filtering";
+
+interface RequiredProps {
+    attributes: Array<{
+        attribute: AttributeMetaData<string>;
+    }>;
+    name: string;
+}
+
+interface StoreProvider extends ISetupable {
+    store: PickerFilterStore;
+}
+
+type Component<P extends object> = (props: P) => React.ReactElement;
+
+export function withDropdownLinkedAttributes<P extends RequiredProps>(
+    component: Component<P & InjectableFilterAPI>
+): Component<P> {
+    const StoreInjector = withInjectedStore(component);
+
+    return function FilterAPIProvider(props) {
+        const api = useStoreProvider(props);
+
+        if (api.hasError) {
+            return <Alert bootstrapStyle="danger">{api.error.message}</Alert>;
+        }
+
+        return <StoreInjector {...props} {...api.value} />;
+    };
+}
+
+function withInjectedStore<P extends object>(
+    Component: Component<P & InjectableFilterAPI>
+): Component<P & { provider: StoreProvider; channel: string }> {
+    return function StoreInjector(props) {
+        const provider = useSetup(() => props.provider);
+        return <Component {...props} filterStore={provider.store} parentChannelName={props.channel} />;
+    };
+}
+
+interface InjectableFilterAPI {
+    filterStore: PickerFilterStore;
+    parentChannelName?: string;
+}
+
+function useStoreProvider(props: RequiredProps): Result<{ provider: StoreProvider; channel: string }, APIError> {
+    const filterAPI = useFilterAPI();
+    return useConst(() => {
+        if (filterAPI.hasError) {
+            return error(filterAPI.error);
+        }
+
+        return value({
+            provider: new DropdownStoreProvider(filterAPI.value, {
+                attributes: props.attributes.map(obj => obj.attribute),
+                dataKey: props.name
+            }),
+            channel: filterAPI.value.parentChannelName
+        });
+    });
+}
