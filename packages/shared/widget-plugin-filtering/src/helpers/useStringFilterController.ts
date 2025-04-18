3d87d75bf524cbf7fa32dc0ff7cd5287e389e026
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2025-04-18 14:31:03 +0200

    refactor(!): remove legacy provider

--- packages/shared/widget-plugin-filtering/src/helpers/useStringFilterController.ts
+++ packages/shared/widget-plugin-filtering/src/helpers/useStringFilterController.ts
@@ -1,17 +1,8 @@
-import { AllFunctions } from "@mendix/filter-commons/typings/FilterFunctions";
-import { useEffect, useState } from "react";
+import { useSetup } from "@mendix/widget-plugin-mobx-kit/react/useSetup";
 import { Params, StringFilterController } from "../controllers/input/StringInputController";
-import { ArgumentInterface } from "../typings/ArgumentInterface";
-import { FilterFn, InputFilterBaseInterface } from "../typings/InputFilterInterface";
 
-export function useStringFilterController<
-    F extends InputFilterBaseInterface<A, Fn>,
-    A extends ArgumentInterface,
-    Fn extends AllFunctions = FilterFn<F>
->(params: Params): StringFilterController {
-    const [ctrl] = useState(() => new StringFilterController(params));
-
-    useEffect(() => ctrl.setup(), [ctrl]);
+export function useStringFilterController(params: Params): StringFilterController {
+    const ctrl = useSetup(() => new StringFilterController(params));
 
     return ctrl;
 }
