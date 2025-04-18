e270d815721a2b722938d1017f8c9fdb8f68c685
gjulivan <grandjulivan@gmail.com>
2025-04-19 00:55:05 +0200

    feat(doc-viewer): add default text viewer and image viewer

---
+++ packages/pluggableWidgets/document-viewer-web/components/TextViewer.tsx
@@ -0,0 +1,39 @@
+import { createElement, useEffect, useState } from "react";
+import { BaseControlViewer } from "./BaseViewer";
+import { DocRendererElement, DocumentRendererProps, DocumentStatus } from "./documentRenderer";
+
+const TextViewer: DocRendererElement = (props: DocumentRendererProps) => {
+    const { file, setDocumentStatus } = props;
+    const [text, setText] = useState<string | null>(null);
+
+    useEffect(() => {
+        const controller = new AbortController();
+        const { signal } = controller;
+        if (file.status === "available" && file.value.uri) {
+            fetch(file.value.uri, { method: "GET", signal })
+                .then(res => res.text())
+                .then(response => {
+                    setText(response);
+                })
+                .catch(() => {
+                    setDocumentStatus(DocumentStatus.error);
+                });
+        }
+
+        return () => {
+            controller.abort();
+        };
+    }, [file, file.status, file.value?.uri, setDocumentStatus]);
+
+    return (
+        <BaseControlViewer {...props} file={file}>
+            <pre className="text-content">{text}</pre>
+        </BaseControlViewer>
+    );
+};
+
+TextViewer.contentTypes = ["text/plain", "text/csv", "application/json"];
+
+TextViewer.fileTypes = ["txt", "csv", "json"];
+
+export default TextViewer;
--- packages/pluggableWidgets/document-viewer-web/components/index.ts
+++ packages/pluggableWidgets/document-viewer-web/components/index.ts
@@ -1,5 +1,7 @@
 import DocxViewer from "./DocxViewer";
 import PDFViewer from "./PDFViewer";
 import ExcelViewer from "./ExcelViewer";
+import TextViewer from "./TextViewer";
+import ImageViewer from "./ImageViewer";
 
-export const DocumentRenderers = [DocxViewer, ExcelViewer, PDFViewer];
+export const DocumentRenderers = [DocxViewer, ExcelViewer, PDFViewer, TextViewer, ImageViewer];
