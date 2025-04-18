68cb2efbb01d6ba874e3c527217beacf5d572837
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2025-04-18 14:31:03 +0200

    refactor(!): remove legacy provider

--- packages/shared/widget-plugin-filtering/src/helpers/useNumberFilterController.ts
+++ packages/shared/widget-plugin-filtering/src/helpers/useNumberFilterController.ts
@@ -1,17 +1,7 @@
-import { AllFunctions } from "@mendix/filter-commons/typings/FilterFunctions";
-import { useEffect, useState } from "react";
+import { useSetup } from "@mendix/widget-plugin-mobx-kit/react/useSetup";
 import { NumberFilterController, Params } from "../controllers/input/NumberInputController";
-import { ArgumentInterface } from "../typings/ArgumentInterface";
-import { FilterFn, InputFilterBaseInterface } from "../typings/InputFilterInterface";
 
-export function useNumberFilterController<
-    F extends InputFilterBaseInterface<A, Fn>,
-    A extends ArgumentInterface,
-    Fn extends AllFunctions = FilterFn<F>
->(params: Params): NumberFilterController {
-    const [ctrl] = useState(() => new NumberFilterController(params));
-
-    useEffect(() => ctrl.setup(), [ctrl]);
-
+export function useNumberFilterController(params: Params): NumberFilterController {
+    const ctrl = useSetup(() => new NumberFilterController(params));
     return ctrl;
 }
