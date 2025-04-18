b7cc51256cd6620b8928b78601ab0c5d51ed6226
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2025-04-18 10:08:47 +0200

    chore: fix imports and remove old modules

--- packages/shared/widget-plugin-filtering/src/controllers/picker/StaticComboboxController.ts
+++
@@ -1,9 +0,0 @@
-import { StaticBaseController, StaticBaseControllerProps } from "./StaticBaseController";
-import { ComboboxControllerMixin } from "./mixins/ComboboxControllerMixin";
-
-export class StaticComboboxController extends ComboboxControllerMixin(StaticBaseController) {
-    constructor(props: StaticBaseControllerProps) {
-        super({ ...props, multiselect: false });
-        this.inputPlaceholder = props.placeholder ?? "Search";
-    }
-}
