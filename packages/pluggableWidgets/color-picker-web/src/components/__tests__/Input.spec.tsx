8ee965307bc58be3ee372ec94ad663f6e293d698
Samuel Reichert <samuelreichertt@gmail.com>
2025-05-06 17:03:16 +0200

    test(color-picker-web): migrate tests to react-testing-library and improve readability

--- packages/pluggableWidgets/color-picker-web/src/components/__tests__/Input.spec.tsx
+++ packages/pluggableWidgets/color-picker-web/src/components/__tests__/Input.spec.tsx
@@ -1,44 +1,52 @@
-import { shallow, ShallowWrapper } from "enzyme";
+import "@testing-library/jest-dom";
+import { render, RenderResult } from "@testing-library/react";
 import { createElement, ReactElement } from "react";
-import { render } from "@testing-library/react";
-
 import { Input, InputProps } from "../Input";
 
 describe("Input", () => {
-    const renderInput = (props: InputProps, children: ReactElement): ShallowWrapper<InputProps, any> =>
-        shallow(<Input {...props}>{children}</Input>);
-    const inputProps: InputProps = {
-        color: "#000000",
-        disabled: false,
-        children: null,
-        onChange: jest.fn()
-    };
-    const inputChildren = (
-        <div>
-            <button />
-        </div>
-    );
+    let inputProps: InputProps;
+
+    beforeEach(() => {
+        inputProps = {
+            color: "#000000",
+            disabled: false,
+            children: null,
+            onChange: jest.fn()
+        };
+    });
+
+    function renderInput(configs: Partial<InputProps> = {}, children: ReactElement): RenderResult {
+        return render(
+            <Input {...inputProps} {...configs}>
+                {children}
+            </Input>
+        );
+    }
 
     it("render DOM structure", () => {
         const { asFragment } = render(<Input {...inputProps} />);
         expect(asFragment()).toMatchSnapshot();
     });
-    it("renders the structure correctly", () => {
-        const inputComponent = renderInput(inputProps, inputChildren);
 
-        expect(inputComponent.getElement()).toEqual(
-            createElement(
-                "div",
-                { className: "widget-color-picker-input-container" },
-                createElement("input", {
-                    className: "form-control",
-                    disabled: inputProps.disabled,
-                    type: "text",
-                    value: inputProps.color,
-                    onChange: inputProps.onChange
-                }),
-                inputChildren
-            )
+    it("renders the structure correctly", () => {
+        const inputChildren = (
+            <div>
+                <button />
+            </div>
         );
+        const { container, getByRole } = renderInput(inputProps, inputChildren);
+
+        const containerDiv = container.firstChild;
+        expect(containerDiv).toHaveClass("widget-color-picker-input-container");
+
+        // Assert the input element
+        const inputElement = getByRole("textbox");
+        expect(inputElement).toHaveClass("form-control");
+        expect(inputElement).toBeEnabled();
+        expect(inputElement).toHaveValue(inputProps.color);
+
+        // Assert the children
+        const buttonElement = container.querySelector("button");
+        expect(buttonElement).toBeInTheDocument();
     });
 });
