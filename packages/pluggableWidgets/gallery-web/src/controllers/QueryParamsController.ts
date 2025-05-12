cec356b42533182087717ec3bb70000cff7e5ce6
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2025-05-12 14:53:08 +0200

    chore: add controller module

---
+++ packages/pluggableWidgets/gallery-web/src/controllers/QueryParamsController.ts
@@ -0,0 +1,57 @@
+import { compactArray, fromCompactArray, isAnd } from "@mendix/filter-commons/condition-utils";
+import { CustomFilterHost } from "@mendix/widget-plugin-filtering/stores/generic/CustomFilterHost";
+import { DatasourceController } from "@mendix/widget-plugin-grid/query/DatasourceController";
+import { disposeBatch } from "@mendix/widget-plugin-mobx-kit/disposeBatch";
+import { ReactiveController, ReactiveControllerHost } from "@mendix/widget-plugin-mobx-kit/reactive-controller";
+import { ListValue } from "mendix";
+import { FilterCondition } from "mendix/filters";
+import { makeAutoObservable, reaction } from "mobx";
+
+export class QueryParamsController implements ReactiveController {
+    private readonly _query: DatasourceController;
+    private readonly _filters: CustomFilterHost;
+
+    constructor(host: ReactiveControllerHost, query: DatasourceController, filters: CustomFilterHost) {
+        host.addController(this);
+
+        this._query = query;
+        this._filters = filters;
+
+        makeAutoObservable(this, { setup: false });
+    }
+
+    private get _derivedSortOrder(): ListValue["sortOrder"] {
+        return [];
+    }
+
+    private get _derivedFilter(): FilterCondition {
+        return compactArray(this._filters.conditions);
+    }
+
+    setup(): () => void {
+        const [add, disposeAll] = disposeBatch();
+        add(
+            reaction(
+                () => this._derivedSortOrder,
+                sortOrder => this._query.setSortOrder(sortOrder),
+                { fireImmediately: true }
+            )
+        );
+        add(
+            reaction(
+                () => this._derivedFilter,
+                filter => this._query.setFilter(filter),
+                { fireImmediately: true }
+            )
+        );
+
+        return disposeAll;
+    }
+
+    unzipFilter(filter?: FilterCondition): Array<FilterCondition | undefined> {
+        if (!filter || !isAnd(filter)) {
+            return [];
+        }
+        return fromCompactArray(filter);
+    }
+}
