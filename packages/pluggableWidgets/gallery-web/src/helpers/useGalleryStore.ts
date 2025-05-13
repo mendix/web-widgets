65839b1d1c2c75a63a23b192227048e67f1628fc
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2025-05-13 17:10:51 +0200

    chore: switch to new gallery store

--- packages/pluggableWidgets/gallery-web/src/helpers/useGalleryStore.ts
+++ packages/pluggableWidgets/gallery-web/src/helpers/useGalleryStore.ts
@@ -1,12 +1,13 @@
 import { GateProvider } from "@mendix/widget-plugin-mobx-kit/GateProvider";
 import { useConst } from "@mendix/widget-plugin-mobx-kit/react/useConst";
+import { useSetup } from "@mendix/widget-plugin-mobx-kit/react/useSetup";
 import { useEffect } from "react";
 import { GalleryContainerProps } from "../../typings/GalleryProps";
-import { GalleryPropsGate, GalleryStore } from "../stores/RootG2Store";
+import { GalleryPropsGate, GalleryStore } from "../stores/GalleryStore";
 
 export function useGalleryStore(props: GalleryContainerProps): GalleryStore {
     const gate = useGate(props);
-    const store = useConst(
+    const store = useSetup(
         () => new GalleryStore({ gate, ...props, showPagingButtons: "auto", showTotalCount: false })
     );
     return store;
