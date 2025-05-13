35de344afe1d3100db67b6151bb031bc9e007e99
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2025-05-13 14:21:03 +0200

    chore: fix build issues

--- packages/pluggableWidgets/gallery-web/src/components/HeaderContainer.tsx
+++ packages/pluggableWidgets/gallery-web/src/components/HeaderContainer.tsx
@@ -1,20 +1,13 @@
 import { createElement } from "react";
-import { getGlobalFilterContextObject } from "@mendix/widget-plugin-filtering/context";
 import { getGlobalSortContext } from "@mendix/widget-plugin-sorting/context";
 import { SortAPIProvider } from "@mendix/widget-plugin-sorting/providers/SortAPIProvider";
-import { HeaderFiltersStore } from "@mendix/widget-plugin-filtering/stores/generic/HeaderFiltersStore";
 import {
     getGlobalSelectionContext,
     SelectionHelper,
     useCreateSelectionContextValue
 } from "@mendix/widget-plugin-grid/selection";
 const SelectionContext = getGlobalSelectionContext();
-const FilterContext = getGlobalFilterContextObject();
 const SortAPIContext = getGlobalSortContext();
-
-function FilterAPIProvider(props: React.PropsWithChildren<{ filtersStore: HeaderFiltersStore }>): React.ReactElement {
-    return <FilterContext.Provider value={props.filtersStore.context}>{props.children}</FilterContext.Provider>;
-}
 
 function SelectionStatusProvider(
     props: React.PropsWithChildren<{ selectionHelper?: SelectionHelper }>
@@ -23,21 +16,15 @@ function SelectionStatusProvider(
     return <SelectionContext.Provider value={value}>{props.children}</SelectionContext.Provider>;
 }
 
-interface HeaderContainerProps
-    extends React.PropsWithChildren<{
-        filtersStore: HeaderFiltersStore;
+export function HeaderContainer(
+    props: React.PropsWithChildren<{
         sortProvider: SortAPIProvider;
         selectionHelper?: SelectionHelper;
-    }> {}
-
-export function HeaderContainer(props: HeaderContainerProps): React.ReactElement {
+    }>
+): React.ReactElement {
     return (
-        <FilterAPIProvider filtersStore={props.filtersStore}>
-            <SortAPIContext.Provider value={props.sortProvider.context}>
-                <SelectionStatusProvider selectionHelper={props.selectionHelper}>
-                    {props.children}
-                </SelectionStatusProvider>
-            </SortAPIContext.Provider>
-        </FilterAPIProvider>
+        <SortAPIContext.Provider value={props.sortProvider.context}>
+            <SelectionStatusProvider selectionHelper={props.selectionHelper}>{props.children}</SelectionStatusProvider>
+        </SortAPIContext.Provider>
     );
 }
---
+++ packages/pluggableWidgets/gallery-web/src/helpers/useGalleryStore.ts
@@ -0,0 +1,19 @@
+import { GateProvider } from "@mendix/widget-plugin-mobx-kit/GateProvider";
+import { useConst } from "@mendix/widget-plugin-mobx-kit/react/useConst";
+import { useEffect } from "react";
+import { GalleryContainerProps } from "../../typings/GalleryProps";
+import { GalleryPropsGate, GalleryStore } from "../stores/RootG2Store";
+
+export function useGalleryStore(props: GalleryContainerProps): GalleryStore {
+    const gate = useGate(props);
+    const store = useConst(
+        () => new GalleryStore({ gate, ...props, showPagingButtons: "auto", showTotalCount: false })
+    );
+    return store;
+}
+
+function useGate(props: GalleryContainerProps): GalleryPropsGate {
+    const gateProvider = useConst(() => new GateProvider(props));
+    useEffect(() => gateProvider.setProps(props));
+    return gateProvider.gate;
+}
