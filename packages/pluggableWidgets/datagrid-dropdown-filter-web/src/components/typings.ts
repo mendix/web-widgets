b7d6c2d576d7df12d612cf3eb5c74ca51af556cc
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2025-05-15 13:43:29 +0200

    chore: rename HOCs

--- packages/pluggableWidgets/datagrid-dropdown-filter-web/src/components/typings.ts
+++ packages/pluggableWidgets/datagrid-dropdown-filter-web/src/components/typings.ts
@@ -1,12 +1,12 @@
 import { RefFilterStore } from "@mendix/widget-plugin-dropdown-filter/stores/RefFilterStore";
 import { StaticSelectFilterStore } from "@mendix/widget-plugin-dropdown-filter/stores/StaticSelectFilterStore";
 
-export interface EnumFilterAPI {
+export interface EnumFilterProps {
     filterStore: StaticSelectFilterStore;
     parentChannelName?: string;
 }
 
-export interface RefFilterAPI {
+export interface RefFilterProps {
     filterStore: RefFilterStore;
     parentChannelName?: string;
 }
