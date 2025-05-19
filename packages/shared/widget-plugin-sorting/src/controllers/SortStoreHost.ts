9038c3fd63d4232b3bfb7baea311f0472fab28d1
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2025-05-19 14:37:28 +0200

    feat: change sort host implementation

--- packages/shared/widget-plugin-sorting/src/controllers/SortStoreHost.ts
+++ packages/shared/widget-plugin-sorting/src/controllers/SortStoreHost.ts
@@ -1,61 +1,110 @@
-import { autorun, makeAutoObservable, observable, reaction } from "mobx";
+import { autorun, makeAutoObservable, reaction } from "mobx";
 import { ObservableSortStore, ObservableSortStoreHost } from "../ObservableSortStoreHost";
-import { SortInstruction } from "../SortingStoreInterface";
+import { SortDirection, SortInstruction } from "../SortingStoreInterface";
+
+export interface SortRecord {
+    key: string;
+    attrId: string;
+    dir: SortDirection;
+}
 
 export class SortStoreHost implements ObservableSortStoreHost {
-    private readonly _stores: Map<string, [store: ObservableSortStore, dispose: () => void]> = new Map();
-    private readonly _storeOrder: string[] = [];
-    private readonly _sortOrder: Array<SortInstruction | null> = [];
+    private readonly _disposeMap: WeakMap<ObservableSortStore, () => void> = new WeakMap();
+    private readonly _state: SortRecord[] = [];
 
     constructor() {
-        makeAutoObservable<this, "_sortOrder">(this, {
-            _sortOrder: observable.shallow
+        makeAutoObservable<this, "_stores">(this, {
+            _stores: false
         });
     }
 
-    observe(key: string, store: ObservableSortStore): void {
-        this.unobserve(key);
+    observe(store: ObservableSortStore, key: string): void {
+        this.unobserve(store);
 
+        // We always initialize store from the host
         const clearAutorun = autorun(() => {
-            const index = this._storeOrder.indexOf(key);
-            store.sortOrder = this._sortOrder[index] ?? null;
+            const record = this._state.find(record => record.key === key);
+            store.sortOrder = record ? this._toSortInstruction(record) : null;
         });
 
+        // This reaction is used to update the host when the store changes
         const clearReaction = reaction(
-            () => store.sortOrder,
-            (sortOrder: SortInstruction | null) => {
-                const index = this._storeOrder.indexOf(key);
-                if (index === -1) {
+            () => (store.sortOrder ? this._toRecord(store.sortOrder, key) : null),
+            storeRecord => {
+                const index = this._state.findIndex(record => record.key === key);
+                // Update the record
+                if (index > -1 && storeRecord) {
+                    Object.assign(this._state[index], storeRecord);
                     return;
                 }
-                this._sortOrder[index] = sortOrder;
+                // Remove the record if it is null
+                if (index > -1 && storeRecord === null) {
+                    this._state.splice(index, 1);
+                    return;
+                }
+                // Add the record if it is not already present
+                if (index === -1 && storeRecord) {
+                    this._state.push(storeRecord);
+                }
             }
         );
 
         const dispose = (): void => {
             clearAutorun();
             clearReaction();
-            this._storeOrder.splice(this._storeOrder.indexOf(key), 1);
-            this._stores.delete(key);
+            this._disposeMap.delete(store);
+            const index = this._state.findIndex(record => record.key === key);
+            if (index !== -1) {
+                this._state.splice(index, 1);
+            }
         };
 
-        this._stores.set(key, [store, dispose]);
-        this._storeOrder.push(key);
+        this._disposeMap.set(store, dispose);
     }
 
-    unobserve(key: string): void {
-        const dispose = this._stores.get(key)?.[1];
+    unobserve(store: ObservableSortStore): void {
+        const dispose = this._disposeMap.get(store);
         if (dispose) {
             dispose();
         }
     }
 
     get sortOrder(): SortInstruction[] {
-        return this._sortOrder.flatMap(sortOrder => (sortOrder ? [sortOrder] : []));
+        return this._state.map(record => this._toSortInstruction(record));
     }
 
-    set sortOrder(sortOrder: SortInstruction[]) {
-        this._sortOrder.length = 0;
-        this._sortOrder.push(...sortOrder);
+    get state(): SortRecord[] {
+        return [...this._state];
+    }
+
+    set state(records: SortRecord[]) {
+        this._state.length = 0;
+
+        type ReduceRec = [acc: SortRecord[], indexMap: Map<string, number>, index: number];
+
+        const [deduplicatedRecords] = records.reduce<ReduceRec>(
+            ([acc, map, index], record: SortRecord) => {
+                if (map.has(record.key)) {
+                    acc[map.get(record.key)!] = record;
+                    return [acc, map, index];
+                } else {
+                    map.set(record.key, index);
+                    acc.push(record);
+                    return [acc, map, index + 1];
+                }
+            },
+            [[], new Map(), 0]
+        );
+
+        this._state.push(...deduplicatedRecords);
+    }
+
+    private _toSortInstruction(record: SortRecord): SortInstruction {
+        return [record.attrId, record.dir] as SortInstruction;
+    }
+
+    private _toRecord(instruction: SortInstruction, key: string): SortRecord | null {
+        const [attrId, dir] = instruction;
+        return { key, attrId, dir };
     }
 }
