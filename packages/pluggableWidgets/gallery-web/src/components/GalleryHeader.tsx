430c43e80f480cdaf4fa9e7671bbdf3111f79985
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2025-05-20 12:07:35 +0200

    fix: change header context

--- packages/pluggableWidgets/gallery-web/src/components/GalleryHeader.tsx
+++ packages/pluggableWidgets/gallery-web/src/components/GalleryHeader.tsx
@@ -1,22 +1,13 @@
 import { createElement, ReactElement } from "react";
-import { useGalleryRootScope } from "../helpers/root-context";
-import { getGlobalSortContext } from "@mendix/widget-plugin-sorting/context";
 
 type GalleryHeaderProps = Omit<JSX.IntrinsicElements["div"], "ref">;
 
-const SortAPI = getGlobalSortContext();
-
 export function GalleryHeader(props: GalleryHeaderProps): ReactElement | null {
     const { children } = props;
-    const scope = useGalleryRootScope();
 
     if (!children) {
         return null;
     }
 
-    return (
-        <SortAPI.Provider value={scope.rootStore.sortAPI}>
-            <section {...props} className="widget-gallery-header widget-gallery-filter" />
-        </SortAPI.Provider>
-    );
+    return <section {...props} className="widget-gallery-header widget-gallery-filter" />;
 }
--- packages/pluggableWidgets/gallery-web/src/components/HeaderWidgetsHost.tsx
+++ packages/pluggableWidgets/gallery-web/src/components/HeaderWidgetsHost.tsx
@@ -1,13 +1,18 @@
 import { createElement } from "react";
-// import { getGlobalSortContext } from "@mendix/widget-plugin-sorting/context";
+import { getGlobalSortContext } from "@mendix/widget-plugin-sorting/context";
 import { getGlobalSelectionContext, useCreateSelectionContextValue } from "@mendix/widget-plugin-grid/selection";
 import { useGalleryRootScope } from "../helpers/root-context";
+
 const SelectionContext = getGlobalSelectionContext();
-// const SortAPIContext = getGlobalSortContext();
+const SortAPI = getGlobalSortContext();
 
 export function HeaderWidgetsHost(props: { children?: React.ReactNode }): React.ReactElement {
-    const { selectionHelper } = useGalleryRootScope();
+    const { selectionHelper, rootStore } = useGalleryRootScope();
     const selectionContext = useCreateSelectionContextValue(selectionHelper);
 
-    return <SelectionContext.Provider value={selectionContext}>{props.children}</SelectionContext.Provider>;
+    return (
+        <SortAPI.Provider value={rootStore.sortAPI}>
+            <SelectionContext.Provider value={selectionContext}>{props.children}</SelectionContext.Provider>;
+        </SortAPI.Provider>
+    );
 }
