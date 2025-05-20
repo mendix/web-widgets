e32f0df6c82486a80b35cbebc279ba05fde51345
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2025-05-20 09:40:54 +0200

    chore: enable multisort

--- packages/pluggableWidgets/gallery-web/src/components/GalleryHeader.tsx
+++ packages/pluggableWidgets/gallery-web/src/components/GalleryHeader.tsx
@@ -1,13 +1,22 @@
 import { createElement, ReactElement } from "react";
+import { useGalleryRootScope } from "../helpers/root-context";
+import { getGlobalSortContext } from "@mendix/widget-plugin-sorting/context";
 
 type GalleryHeaderProps = Omit<JSX.IntrinsicElements["div"], "ref">;
 
+const SortAPI = getGlobalSortContext();
+
 export function GalleryHeader(props: GalleryHeaderProps): ReactElement | null {
     const { children } = props;
+    const scope = useGalleryRootScope();
 
     if (!children) {
         return null;
     }
 
-    return <section {...props} className="widget-gallery-header widget-gallery-filter" />;
+    return (
+        <SortAPI.Provider value={scope.rootStore.sortAPI}>
+            <section {...props} className="widget-gallery-header widget-gallery-filter" />
+        </SortAPI.Provider>
+    );
 }
