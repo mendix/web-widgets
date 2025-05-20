74057a9f45ab922bc941ac5b0e0d62564b8a0a72
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2025-05-20 13:04:18 +0200

    fix: add filter context

--- packages/pluggableWidgets/gallery-web/src/components/HeaderWidgetsHost.tsx
+++ packages/pluggableWidgets/gallery-web/src/components/HeaderWidgetsHost.tsx
@@ -2,17 +2,21 @@ import { createElement } from "react";
 import { getGlobalSortContext } from "@mendix/widget-plugin-sorting/context";
 import { getGlobalSelectionContext, useCreateSelectionContextValue } from "@mendix/widget-plugin-grid/selection";
 import { useGalleryRootScope } from "../helpers/root-context";
+import { getGlobalFilterContextObject } from "@mendix/widget-plugin-filtering/context";
 
 const SelectionContext = getGlobalSelectionContext();
 const SortAPI = getGlobalSortContext();
+const FilterAPI = getGlobalFilterContextObject();
 
 export function HeaderWidgetsHost(props: { children?: React.ReactNode }): React.ReactElement {
     const { selectionHelper, rootStore } = useGalleryRootScope();
     const selectionContext = useCreateSelectionContextValue(selectionHelper);
 
     return (
-        <SortAPI.Provider value={rootStore.sortAPI}>
-            <SelectionContext.Provider value={selectionContext}>{props.children}</SelectionContext.Provider>;
-        </SortAPI.Provider>
+        <FilterAPI.Provider value={rootStore.filterAPI}>
+            <SortAPI.Provider value={rootStore.sortAPI}>
+                <SelectionContext.Provider value={selectionContext}>{props.children}</SelectionContext.Provider>;
+            </SortAPI.Provider>
+        </FilterAPI.Provider>
     );
 }
