978adf5a35b823319ea553912c50bc8f5246243c
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2025-06-05 14:04:15 +0200

    chore: remove obsolete code

--- packages/shared/widget-plugin-filtering/src/helpers/useStringFilterController.ts
+++
@@ -1,8 +0,0 @@
-import { useSetup } from "@mendix/widget-plugin-mobx-kit/react/useSetup";
-import { Params, StringFilterController } from "../controllers/input/StringInputController";
-
-export function useStringFilterController(params: Params): StringFilterController {
-    const ctrl = useSetup(() => new StringFilterController(params));
-
-    return ctrl;
-}
