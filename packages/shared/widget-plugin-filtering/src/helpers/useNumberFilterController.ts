1424aaa4bd0e80464921cc620ec543fd5862d887
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2025-06-05 14:04:15 +0200

    chore: remove obsolete code

--- packages/shared/widget-plugin-filtering/src/helpers/useNumberFilterController.ts
+++
@@ -1,7 +0,0 @@
-import { useSetup } from "@mendix/widget-plugin-mobx-kit/react/useSetup";
-import { NumberFilterController, Params } from "../controllers/input/NumberInputController";
-
-export function useNumberFilterController(params: Params): NumberFilterController {
-    const ctrl = useSetup(() => new NumberFilterController(params));
-    return ctrl;
-}
