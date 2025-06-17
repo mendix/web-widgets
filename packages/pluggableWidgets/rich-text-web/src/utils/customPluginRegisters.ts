4334a8c2bad51bad77eacd95db6d7ec345b30420
gjulivan <grandjulivan@gmail.com>
2025-06-17 14:26:23 +0200

    feat(rte): add image upload handler on paste

--- packages/pluggableWidgets/rich-text-web/src/utils/customPluginRegisters.ts
+++ packages/pluggableWidgets/rich-text-web/src/utils/customPluginRegisters.ts
@@ -14,6 +14,7 @@ import { IndentLeftStyle, IndentRightStyle } from "./formats/indent";
 import Formula from "./formats/formula";
 import QuillResize from "quill-resize-module";
 import QuillTableBetter from "./formats/quill-table-better/quill-table-better";
+import MxUploader from "./modules/uploader";
 
 class Empty {
     doSomething(): string {
@@ -35,6 +36,7 @@ Quill.register(IndentLeftStyle, true);
 Quill.register(IndentRightStyle, true);
 Quill.register(Formula, true);
 Quill.register(Button, true);
+Quill.register({ "modules/uploader": MxUploader }, true);
 Quill.register("modules/resize", QuillResize, true);
 // add empty handler for view code, this format is handled by toolbar's custom config via ViewCodeDialog
 Quill.register({ "ui/view-code": Empty });
