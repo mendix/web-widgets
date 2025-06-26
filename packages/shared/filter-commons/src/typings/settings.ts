7bdd232631b5595dc8d5502c285f8a348b6067b5
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2025-06-26 15:49:36 +0200

    feat: add gallery persistent state controller

--- packages/shared/filter-commons/src/typings/settings.ts
+++ packages/shared/filter-commons/src/typings/settings.ts
@@ -7,3 +7,10 @@ export type SelectData = string[];
 export type FilterData = InputData | SelectData | null | undefined;
 
 export type FiltersSettingsMap<T> = Map<T, FilterData>;
+
+export type Json = string | number | boolean | null | Json[] | { [key: string]: Json };
+
+export interface Serializable {
+    toJSON(): Json;
+    fromJSON(data: Json): void;
+}
