461e3273536a04a04e86cbe4c7a767be2fdc3de0
Samuel Reichert <samuelreichertt@gmail.com>
2025-07-01 13:37:21 +0200

    fix(custom-chart-web): fix data and layout parsing to merge sample when attribute is empty

--- packages/pluggableWidgets/custom-chart-web/src/utils/utils.ts
+++ packages/pluggableWidgets/custom-chart-web/src/utils/utils.ts
@@ -3,14 +3,11 @@ export function parseData(staticData?: string, attributeData?: string, sampleDat
     let finalData: Data[] = [];
 
     try {
-        if (staticData) {
-            finalData = [...finalData, ...JSON.parse(staticData)];
-        }
-        if (attributeData) {
-            finalData = [...finalData, ...JSON.parse(attributeData)];
-        }
-        if (!finalData.length && sampleData) {
-            finalData = [...finalData, ...JSON.parse(sampleData)];
+        const dataAttribute = attributeData ? JSON.parse(attributeData) : [];
+        finalData = [...finalData, ...(staticData ? JSON.parse(staticData) : []), ...dataAttribute];
+
+        if (dataAttribute.length === 0) {
+            finalData = [...finalData, ...(sampleData ? JSON.parse(sampleData) : [])];
         }
     } catch (error) {
         console.error("Error parsing chart data:", error);
@@ -23,14 +20,11 @@ export function parseLayout(staticLayout?: string, attributeLayout?: string, sam
     let finalLayout: Partial<Layout> = {};
 
     try {
-        if (staticLayout) {
-            finalLayout = { ...finalLayout, ...JSON.parse(staticLayout) };
-        }
-        if (attributeLayout) {
-            finalLayout = { ...finalLayout, ...JSON.parse(attributeLayout) };
-        }
-        if (Object.keys(finalLayout).length === 0 && sampleLayout) {
-            finalLayout = { ...finalLayout, ...JSON.parse(sampleLayout) };
+        const layoutAttribute = attributeLayout ? JSON.parse(attributeLayout) : {};
+        finalLayout = { ...finalLayout, ...(staticLayout ? JSON.parse(staticLayout) : {}), ...layoutAttribute };
+
+        if (Object.keys(layoutAttribute).length === 0) {
+            finalLayout = { ...finalLayout, ...(sampleLayout ? JSON.parse(sampleLayout) : {}) };
         }
     } catch (error) {
         console.error("Error parsing chart layout:", error);
