605f57fc8f035731100e5bcc8c0790cbb6c50b18
Rahman Unver <rahmanunver@gmail.com>
2025-07-04 14:36:46 +0200

    test(calendar-web): update test with timezone set to UTC

--- packages/pluggableWidgets/calendar-web/src/__tests__/Calendar.spec.tsx
+++ packages/pluggableWidgets/calendar-web/src/__tests__/Calendar.spec.tsx
@@ -4,6 +4,13 @@ import { ListValueBuilder } from "@mendix/widget-plugin-test-utils";
 
 import Calendar from "../Calendar";
 import { CalendarContainerProps } from "../../typings/CalendarProps";
+
+// We capture any existing value, switch to UTC, and restore the original value
+// when the suite finishes.
+
+const ORIGINAL_TZ = process.env.TZ;
+process.env.TZ = "UTC";
+
 const customViewProps: CalendarContainerProps = {
     name: "calendar-test",
     class: "calendar-class",
@@ -49,6 +56,10 @@ beforeAll(() => {
 
 afterAll(() => {
     jest.useRealTimers();
+
+    // Restore whatever time-zone the process was originally using so other
+    // test files are unaffected.
+    process.env.TZ = ORIGINAL_TZ;
 });
 
 describe("Calendar", () => {
