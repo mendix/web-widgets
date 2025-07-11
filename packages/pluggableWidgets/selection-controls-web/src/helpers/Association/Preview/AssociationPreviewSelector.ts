e8f0c45d55126a4a1df6fbcc5bf7a34ded297133
gjulivan <grandjulivan@gmail.com>
2025-07-11 13:30:36 +0200

    feat: update dropzone for custom content

---
+++ packages/pluggableWidgets/selection-controls-web/src/helpers/Association/Preview/AssociationPreviewSelector.ts
@@ -0,0 +1,41 @@
+import { SingleSelector, Status, CaptionsProvider, OptionsProvider } from "../../types";
+import {
+    SelectionControlsPreviewProps,
+    OptionsSourceCustomContentTypeEnum
+} from "../../../../typings/SelectionControlsProps";
+import { generateUUID } from "@mendix/widget-plugin-platform/framework/generate-uuid";
+import { PreviewCaptionsProvider } from "../../Preview/PreviewCaptionsProvider";
+import { PreviewOptionsProvider } from "../../Preview/PreviewOptionsProvider";
+
+export class AssociationPreviewSelector implements SingleSelector {
+    type = "single" as const;
+    status: Status = "available";
+    attributeType?: "string" | "boolean" | "big" | "date" | undefined;
+    selectorType?: "context" | "database" | "static" | undefined;
+    // type: "single";
+    readOnly: boolean;
+    validation?: string | undefined;
+    clearable: boolean = false;
+    currentId: string | null;
+    customContentType: OptionsSourceCustomContentTypeEnum;
+    caption: CaptionsProvider;
+    options: OptionsProvider;
+
+    constructor(props: SelectionControlsPreviewProps) {
+        this.readOnly = props.readOnly;
+        this.currentId = `single-${generateUUID()}`;
+        this.customContentType = props.optionsSourceCustomContentType;
+        this.readOnly = props.readOnly;
+        this.caption = new PreviewCaptionsProvider(new Map());
+        this.options = new PreviewOptionsProvider(this.caption, new Map());
+        (this.caption as PreviewCaptionsProvider).updatePreviewProps({
+            customContentRenderer: props.optionsSourceAssociationCustomContent?.renderer,
+            customContentType: props.optionsSourceCustomContentType
+        });
+    }
+
+    updateProps() {}
+    setValue() {}
+    onEnterEvent() {}
+    onLeaveEvent() {}
+}
