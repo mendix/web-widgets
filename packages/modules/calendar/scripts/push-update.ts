b03170f5091e0a2b33daee542d21a0b453364f26
Rahman Unver <rahmanunver@gmail.com>
2025-07-15 19:02:12 +0200

    feat(calendar-web): update xml, useMemo, refactor

---
+++ packages/modules/calendar/scripts/push-update.ts
@@ -0,0 +1,15 @@
+#!/usr/bin/env ts-node-script
+
+import { pushUpdateToTestProject, runModuleSteps } from "@mendix/automation-utils/steps";
+
+async function main(): Promise<void> {
+    await runModuleSteps({
+        packagePath: process.cwd(),
+        steps: [pushUpdateToTestProject]
+    });
+}
+
+main().catch(err => {
+    console.error(err);
+    process.exit(1);
+});
