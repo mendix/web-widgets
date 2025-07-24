ccfd705f928cf928478c110d951a13ce3c77a28f
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2025-07-24 16:10:34 +0200

    feat: add combined filter store

---
+++ packages/shared/widget-plugin-filtering/src/utils/fnv-1a-hash.ts
@@ -0,0 +1,20 @@
+/* eslint-disable no-bitwise */
+
+/**
+ * Generates 32 bit FNV-1a hash from the given string.
+ * As explained here: http://isthe.com/chongo/tech/comp/fnv/
+ *
+ * @param s {string} String to generate hash from.
+ * @param [h] {number} FNV-1a hash generation init value.
+ * @returns {number} The result integer hash.
+ */
+export function fnv1aHash(s: string, h: number = 0x811c9dc5): number {
+    const l = s.length;
+
+    for (let i = 0; i < l; i++) {
+        h ^= s.charCodeAt(i);
+        h += (h << 1) + (h << 4) + (h << 7) + (h << 8) + (h << 24);
+    }
+
+    return h >>> 0;
+}
