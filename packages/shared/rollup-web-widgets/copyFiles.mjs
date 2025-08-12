c0549247d636c19de313de2b8c12e5d7ad5006a3
gjulivan <grandjulivan@gmail.com>
2025-08-12 09:50:27 +0200

    chore: prepare release adding OSS readme

--- packages/shared/rollup-web-widgets/copyFiles.mjs
+++ packages/shared/rollup-web-widgets/copyFiles.mjs
@@ -12,6 +12,12 @@ export function copyDefaultFilesPlugin() {
                 src: "../../../LICENSE",
                 dest: "dist/tmp/widgets",
                 rename: "License.txt"
+            },
+            {
+                // Matches files starting with 'Siemens', containing 'READMEOSS' somewhere, and ending with .html
+                src: "Siemens*READMEOSS*.html",
+                dest: "dist/tmp/widgets",
+                flatten: true
             }
         ]
     });
