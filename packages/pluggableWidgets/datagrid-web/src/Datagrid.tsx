c9dbd1e2f9992d7a369665b817fbad7259377057
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2025-08-13 12:22:33 +0200

    feat: add selection counter

--- packages/pluggableWidgets/datagrid-web/src/Datagrid.tsx
+++ packages/pluggableWidgets/datagrid-web/src/Datagrid.tsx
@@ -2,6 +2,7 @@ import { useOnResetFiltersEvent } from "@mendix/widget-plugin-external-events/ho
 import { useClickActionHelper } from "@mendix/widget-plugin-grid/helpers/ClickActionHelper";
 import { useFocusTargetController } from "@mendix/widget-plugin-grid/keyboard-navigation/useFocusTargetController";
 import { useSelectionHelper } from "@mendix/widget-plugin-grid/selection";
+import { useConst } from "@mendix/widget-plugin-mobx-kit/react/useConst";
 import { generateUUID } from "@mendix/widget-plugin-platform/framework/generate-uuid";
 import { observer } from "mobx-react-lite";
 import { ReactElement, ReactNode, createElement, useCallback, useMemo } from "react";
@@ -13,6 +14,7 @@ import { ProgressStore } from "./features/data-export/ProgressStore";
 import { useDataExport } from "./features/data-export/useDataExport";
 import { useCellEventsController } from "./features/row-interaction/CellEventsController";
 import { useCheckboxEventsController } from "./features/row-interaction/CheckboxEventsController";
+import { DatagridContext } from "./helpers/root-context";
 import { useSelectActionHelper } from "./helpers/SelectActionHelper";
 import { IColumnGroupStore } from "./helpers/state/ColumnGroupStore";
 import { RootGridStore } from "./helpers/state/RootGridStore";
@@ -57,74 +59,82 @@ const Container = observer((props: Props): ReactElement => {
 
     const checkboxEventsController = useCheckboxEventsController(selectActionHelper, focusController);
 
+    const ctx = useConst(() => {
+        rootStore.basicData.setSelectionHelper(selectionHelper);
+        return {
+            basicData: rootStore.basicData,
+            selectionHelper,
+            selectActionHelper,
+            cellEventsController,
+            checkboxEventsController,
+            focusController,
+            selectionCountStore: rootStore.selectionCountStore
+        };
+    });
+
     return (
-        <Widget
-            className={props.class}
-            CellComponent={Cell}
-            columnsDraggable={props.columnsDraggable}
-            columnsFilterable={props.columnsFilterable}
-            columnsHidable={props.columnsHidable}
-            columnsResizable={props.columnsResizable}
-            columnsSortable={props.columnsSortable}
-            data={items}
-            emptyPlaceholderRenderer={useCallback(
-                (renderWrapper: (children: ReactNode) => ReactElement) =>
-                    props.showEmptyPlaceholder === "custom" ? renderWrapper(props.emptyPlaceholder) : <div />,
-                [props.emptyPlaceholder, props.showEmptyPlaceholder]
-            )}
-            filterRenderer={useCallback(
-                (renderWrapper, columnIndex) => {
-                    const columnFilter = columnsStore.columnFilters[columnIndex];
-                    return renderWrapper(columnFilter.renderFilterWidgets());
-                },
-                [columnsStore.columnFilters]
-            )}
-            headerTitle={props.filterSectionTitle?.value}
-            headerContent={
-                props.filtersPlaceholder && (
-                    <WidgetHeaderContext selectionHelper={selectionHelper} rootStore={rootStore}>
-                        {props.filtersPlaceholder}
-                    </WidgetHeaderContext>
-                )
-            }
-            hasMoreItems={props.datasource.hasMoreItems ?? false}
-            headerWrapperRenderer={useCallback((_columnIndex: number, header: ReactElement) => header, [])}
-            id={useMemo(() => `DataGrid${generateUUID()}`, [])}
-            numberOfItems={props.datasource.totalCount}
-            onExportCancel={abortExport}
-            page={paginationCtrl.currentPage}
-            pageSize={props.pageSize}
-            paginationType={props.pagination}
-            loadMoreButtonCaption={props.loadMoreButtonCaption?.value}
-            paging={paginationCtrl.showPagination}
-            pagingPosition={props.pagingPosition}
-            showPagingButtons={props.showPagingButtons}
-            rowClass={useCallback((value: any) => props.rowClass?.get(value)?.value ?? "", [props.rowClass])}
-            gridInteractive={!!(props.itemSelection || props.onClick)}
-            setPage={paginationCtrl.setPage}
-            styles={props.style}
-            selectionStatus={selectionHelper?.type === "Multi" ? selectionHelper.selectionStatus : "unknown"}
-            exporting={exportProgress.exporting}
-            processedRows={exportProgress.loaded}
-            exportDialogLabel={props.exportDialogLabel?.value}
-            cancelExportLabel={props.cancelExportLabel?.value}
-            selectRowLabel={props.selectRowLabel?.value}
-            selectAllRowsLabel={props.selectAllRowsLabel?.value}
-            visibleColumns={columnsStore.visibleColumns}
-            availableColumns={columnsStore.availableColumns}
-            setIsResizing={(status: boolean) => columnsStore.setIsResizing(status)}
-            columnsSwap={(moved, [target, placement]) => columnsStore.swapColumns(moved, [target, placement])}
-            selectActionHelper={selectActionHelper}
-            cellEventsController={cellEventsController}
-            checkboxEventsController={checkboxEventsController}
-            focusController={focusController}
-            isFirstLoad={rootStore.loaderCtrl.isFirstLoad}
-            isFetchingNextBatch={rootStore.loaderCtrl.isFetchingNextBatch}
-            isLoading={props.datasource.status === "loading"}
-            loadingType={props.loadingType}
-            columnsLoading={!columnsStore.loaded}
-            showRefreshIndicator={rootStore.loaderCtrl.showRefreshIndicator}
-        />
+        <DatagridContext.Provider value={ctx}>
+            <Widget
+                className={props.class}
+                CellComponent={Cell}
+                columnsDraggable={props.columnsDraggable}
+                columnsFilterable={props.columnsFilterable}
+                columnsHidable={props.columnsHidable}
+                columnsResizable={props.columnsResizable}
+                columnsSortable={props.columnsSortable}
+                data={items}
+                emptyPlaceholderRenderer={useCallback(
+                    (renderWrapper: (children: ReactNode) => ReactElement) =>
+                        props.showEmptyPlaceholder === "custom" ? renderWrapper(props.emptyPlaceholder) : <div />,
+                    [props.emptyPlaceholder, props.showEmptyPlaceholder]
+                )}
+                filterRenderer={useCallback(
+                    (renderWrapper, columnIndex) => {
+                        const columnFilter = columnsStore.columnFilters[columnIndex];
+                        return renderWrapper(columnFilter.renderFilterWidgets());
+                    },
+                    [columnsStore.columnFilters]
+                )}
+                headerTitle={props.filterSectionTitle?.value}
+                headerContent={
+                    props.filtersPlaceholder && (
+                        <WidgetHeaderContext selectionHelper={selectionHelper} rootStore={rootStore}>
+                            {props.filtersPlaceholder}
+                        </WidgetHeaderContext>
+                    )
+                }
+                hasMoreItems={props.datasource.hasMoreItems ?? false}
+                headerWrapperRenderer={useCallback((_columnIndex: number, header: ReactElement) => header, [])}
+                id={useMemo(() => `DataGrid${generateUUID()}`, [])}
+                numberOfItems={props.datasource.totalCount}
+                onExportCancel={abortExport}
+                page={paginationCtrl.currentPage}
+                pageSize={props.pageSize}
+                paginationType={props.pagination}
+                loadMoreButtonCaption={props.loadMoreButtonCaption?.value}
+                paging={paginationCtrl.showPagination}
+                pagingPosition={props.pagingPosition}
+                showPagingButtons={props.showPagingButtons}
+                rowClass={useCallback((value: any) => props.rowClass?.get(value)?.value ?? "", [props.rowClass])}
+                setPage={paginationCtrl.setPage}
+                styles={props.style}
+                exporting={exportProgress.exporting}
+                processedRows={exportProgress.loaded}
+                visibleColumns={columnsStore.visibleColumns}
+                availableColumns={columnsStore.availableColumns}
+                setIsResizing={(status: boolean) => columnsStore.setIsResizing(status)}
+                columnsSwap={(moved, [target, placement]) => columnsStore.swapColumns(moved, [target, placement])}
+                selectActionHelper={selectActionHelper}
+                cellEventsController={cellEventsController}
+                checkboxEventsController={checkboxEventsController}
+                focusController={focusController}
+                isFirstLoad={rootStore.loaderCtrl.isFirstLoad}
+                isFetchingNextBatch={rootStore.loaderCtrl.isFetchingNextBatch}
+                showRefreshIndicator={rootStore.loaderCtrl.showRefreshIndicator}
+                loadingType={props.loadingType}
+                columnsLoading={!columnsStore.loaded}
+            />
+        </DatagridContext.Provider>
     );
 });
 
