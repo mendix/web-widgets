fa4a0b61fe986a0e7d8fc4d7e88628850679803c
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2025-08-27 11:15:53 +0200

    feat: add clear selection hook

--- packages/shared/widget-plugin-external-events/src/hooks.ts
+++ packages/shared/widget-plugin-external-events/src/hooks.ts
@@ -1,4 +1,5 @@
+export { useListenChannelEvents } from "./hooks/useListenChannelEvents";
+export { useOnClearSelectionEvent } from "./hooks/useOnClearSelectionEvent";
+export { useOnResetFiltersEvent } from "./hooks/useOnResetFiltersEvent";
 export { useOnResetValueEvent } from "./hooks/useOnResetValueEvent";
 export { useOnSetValueEvent } from "./hooks/useOnSetValueEvent";
-export { useOnResetFiltersEvent } from "./hooks/useOnResetFiltersEvent";
-export { useListenChannelEvents } from "./hooks/useListenChannelEvents";
