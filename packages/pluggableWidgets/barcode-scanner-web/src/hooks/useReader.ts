21829e9b79144e13abcd178d00b6875b0025ad3e
gjulivan <grandjulivan@gmail.com>
2025-09-03 00:35:50 +0200

    fix: update based on review feedbacks

--- packages/pluggableWidgets/barcode-scanner-web/src/hooks/useReader.ts
+++ packages/pluggableWidgets/barcode-scanner-web/src/hooks/useReader.ts
@@ -1,19 +1,10 @@
 import { useEventCallback } from "@mendix/widget-plugin-hooks/useEventCallback";
-import { RefObject, useEffect, useMemo, useRef } from "react";
+import { useEffect, useMemo, useRef } from "react";
 import { MxBarcodeReader } from "src/helpers/barcode-detector";
-import { BarcodeFormatsType } from "../../typings/BarcodeScannerProps";
 import { isBarcodeDetectorSupported } from "../helpers/barcode-detector-utils";
+import { UseReaderHook } from "../helpers/utils";
 import { Reader as NativeReader } from "./nativeReader";
 import { Reader as ZxReader } from "./zxReader";
-
-type UseReaderHook = (args: {
-    onSuccess?: (data: string) => void;
-    onError?: (e: Error) => void;
-    useCrop: boolean;
-    barcodeFormats?: BarcodeFormatsType[];
-    useAllFormats: boolean;
-    canvasMiddleRef: RefObject<HTMLDivElement>;
-}) => { ref: RefObject<HTMLVideoElement>; useBrowserAPI: boolean };
 
 export const useReader: UseReaderHook = args => {
     const videoRef = useRef<HTMLVideoElement>(null);
@@ -23,14 +14,14 @@ export const useReader: UseReaderHook = args => {
 
     const reader: MxBarcodeReader = useMemo(() => {
         return enableBrowserAPI ? new NativeReader(args, videoRef) : new ZxReader(args, videoRef);
-    }, [enableBrowserAPI, args, videoRef]);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [enableBrowserAPI, args.barcodeFormats, args.useAllFormats, args.useCrop, videoRef]);
 
     useEffect(() => {
         reader.start(onSuccess, onError);
 
         return reader.stop;
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+    }, [onSuccess, onError, reader]);
 
     return { ref: videoRef, useBrowserAPI: enableBrowserAPI };
 };
