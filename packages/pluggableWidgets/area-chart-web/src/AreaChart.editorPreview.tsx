bc1adea11ce3a89a5882ed8c10b758ed5f148e10
Samuel Reichert <samuelreichertt@gmail.com>
2025-09-10 18:35:10 +0200

    refactor: remove createElement imports and update tsconfig for React 19 JSX transform

--- packages/pluggableWidgets/area-chart-web/src/AreaChart.editorPreview.tsx
+++ packages/pluggableWidgets/area-chart-web/src/AreaChart.editorPreview.tsx
@@ -1,5 +1,5 @@
 import { ChartPreview } from "@mendix/shared-charts/preview";
-import { createElement, ReactNode } from "react";
+import { ReactNode } from "react";
 import { AreaChartPreviewProps } from "../typings/AreaChartProps";
 import AreaChartLegend from "./assets/AreaChart-legend.light.svg";
 import AreaChartImage from "./assets/AreaChart.light.svg";
