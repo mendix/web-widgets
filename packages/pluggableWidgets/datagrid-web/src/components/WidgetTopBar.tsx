1f40adce2611d84b2cb5ea32fc4683eb5be78c61
Yordan <yordan.stoyanov@mendix.com>
2025-09-15 17:06:25 +0200

    feat: add selection count visibility to config

--- packages/pluggableWidgets/datagrid-web/src/components/WidgetTopBar.tsx
+++ packages/pluggableWidgets/datagrid-web/src/components/WidgetTopBar.tsx
@@ -1,8 +1,22 @@
 import { createElement, ReactElement } from "react";
+import { SelectionCountVisibilityEnum } from "../../typings/DatagridProps";
+import { SelectionCounter } from "./SelectionCounter";
 
-export function WidgetTopBar(props: JSX.IntrinsicElements["div"]): ReactElement {
+type WidgetTopBarProps = {
+    selectionCountVisibility?: SelectionCountVisibilityEnum;
+    clearSelectionButtonLabel?: string;
+} & JSX.IntrinsicElements["div"];
+
+export function WidgetTopBar(props: WidgetTopBarProps): ReactElement {
+    const { selectionCountVisibility, clearSelectionButtonLabel } = props;
+
+    console.warn(selectionCountVisibility);
+
     return (
         <div {...props} className="widget-datagrid-top-bar table-header">
+            {selectionCountVisibility === "top" && (
+                <SelectionCounter clearSelectionButtonLabel={clearSelectionButtonLabel} />
+            )}
             {props.children}
         </div>
     );
