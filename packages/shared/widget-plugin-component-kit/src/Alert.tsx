abffab83f1b521c092e39855d203079e15eb9549
Hedwig Doets <hedwig.doets@mendix.com>
2025-09-19 13:28:11 +0200

    fix: connect combobox input to validation message, set aria-invalid

--- packages/shared/widget-plugin-component-kit/src/Alert.tsx
+++ packages/shared/widget-plugin-component-kit/src/Alert.tsx
@@ -5,24 +5,35 @@ export interface AlertProps {
     children?: ReactNode;
     className?: string;
     bootstrapStyle: "default" | "primary" | "success" | "info" | "warning" | "danger";
+    inputElementId?: string;
     role?: string;
 }
 
 export interface ValidationAlertProps {
     children?: ReactNode;
     className?: string;
+    referenceId?: string;
 }
 
 // cloning from https://gitlab.rnd.mendix.com/appdev/appdev/-/blob/master/client/src/widgets/web/helpers/Alert.tsx
-export const ValidationAlert = ({ className, children }: ValidationAlertProps): ReactElement => (
-    <Alert className={classNames("mx-validation-message", className)} bootstrapStyle="danger" role="alert">
+export const ValidationAlert = ({ className, children, referenceId }: ValidationAlertProps): ReactElement => (
+    <Alert
+        className={classNames("mx-validation-message", className)}
+        bootstrapStyle="danger"
+        role="alert"
+        inputElementId={referenceId}
+    >
         {children}
     </Alert>
 );
 
-export const Alert = ({ className, bootstrapStyle, children, role }: AlertProps): ReactNode =>
+export const Alert = ({ className, bootstrapStyle, children, role, inputElementId }: AlertProps): ReactNode =>
     Children.count(children) > 0 ? (
-        <div className={classNames(`alert alert-${bootstrapStyle}`, className)} role={role}>
+        <div
+            className={classNames(`alert alert-${bootstrapStyle}`, className)}
+            role={role}
+            id={inputElementId + "-error"}
+        >
             {children}
         </div>
     ) : null;
