72923397d2626987959654a28b98f80604ddb788
Samuel Reichert <samuelreichertt@gmail.com>
2025-09-23 10:13:31 +0200

    chore(checkbox-radio-selection-web): remove unused imports and update JSX settings for React 19

--- packages/pluggableWidgets/checkbox-radio-selection-web/src/components/Placeholder.tsx
+++ packages/pluggableWidgets/checkbox-radio-selection-web/src/components/Placeholder.tsx
@@ -1,4 +1,4 @@
-import { createElement, ReactElement } from "react";
+import { ReactElement } from "react";
 
 export function Placeholder({ noOptionsText }: { noOptionsText: string }): ReactElement {
     return (
