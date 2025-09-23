8dfaba7dcf16dff58b16a80b59dd6e377493aee7
Samuel Reichert <samuelreichertt@gmail.com>
2025-09-23 15:06:56 +0200

    chore(widget-plugin-mobx-kit): update Jest configuration and dependencies for improved testing

--- packages/shared/widget-plugin-mobx-kit/test/useSubscribe.test.ts
+++ packages/shared/widget-plugin-mobx-kit/test/useSubscribe.test.ts
@@ -1,4 +1,4 @@
-import { act, renderHook } from "@testing-library/react-hooks";
+import { act, renderHook } from "@testing-library/react";
 import { configure, isObservable, observable } from "mobx";
 import { useSubscribe } from "../src/react/useSubscribe";
 
