ec74e1a20e6bfaa4e52891bfc7b0e28a91494110
Samuel Reichert <samuelreichertt@gmail.com>
2025-09-24 15:01:17 +0200

    chore(document-viewer-web): refactor imports for improved consistency and type safety

--- packages/pluggableWidgets/document-viewer-web/components/PDFViewer.tsx
+++ packages/pluggableWidgets/document-viewer-web/components/PDFViewer.tsx
@@ -1,4 +1,4 @@
-import { createElement, Fragment, useCallback, useEffect, useMemo, useState } from "react";
+import { Fragment, useCallback, useEffect, useMemo, useState } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/Page/AnnotationLayer.css";
 import "react-pdf/dist/Page/TextLayer.css";
