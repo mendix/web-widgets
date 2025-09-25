54ad672562cba85393f5735cd81cbdd9bc697af8
Samuel Reichert <samuelreichertt@gmail.com>
2025-09-25 15:42:25 +0200

    chore(timeline-web): remove unused imports and update tsConfig for React 19

--- packages/pluggableWidgets/timeline-web/src/Timeline.tsx
+++ packages/pluggableWidgets/timeline-web/src/Timeline.tsx
@@ -1,4 +1,4 @@
-import { createElement, ReactElement } from "react";
+import { ReactElement } from "react";
 import { TimelineContainerProps } from "../typings/TimelineProps";
 import "./ui/Timeline.scss";
 import TimelineComponent from "./components/TimelineComponent";
