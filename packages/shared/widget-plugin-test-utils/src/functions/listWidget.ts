106b2cd94d34c5593b56c84dbc213b0b15827598
Samuel Reichert <samuelreichertt@gmail.com>
2025-09-26 17:16:38 +0200

    fix(shared): fix eslint errors

--- packages/shared/widget-plugin-test-utils/src/functions/listWidget.ts
+++ packages/shared/widget-plugin-test-utils/src/functions/listWidget.ts
@@ -1,9 +1,10 @@
 import { ListWidgetValue, ObjectItem } from "mendix";
+import { ReactNode } from "react";
 
 /**
  * Returns `ListWidgetValue` mock.
  * @param get - function to use as `.get`. Should map item to React node.
  */
-export function listWidget(get: (item: ObjectItem) => React.ReactNode): ListWidgetValue {
+export function listWidget(get: (item: ObjectItem) => ReactNode): ListWidgetValue {
     return { get } as ListWidgetValue;
 }
