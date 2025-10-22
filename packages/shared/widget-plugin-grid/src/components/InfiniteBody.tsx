e12ff2cef5fc13923ffb8a4ba6417e0cfa879418
Roman Vyakhirev <roman.vyakhirev@mendix.com>
2025-10-22 11:57:12 +0200

    chore: improve virtual scrolling

--- packages/shared/widget-plugin-grid/src/components/InfiniteBody.tsx
+++ packages/shared/widget-plugin-grid/src/components/InfiniteBody.tsx
@@ -1,36 +1,45 @@
-import {
-    CSSProperties,
-    PropsWithChildren,
-    ReactElement,
-    RefObject,
-    useCallback,
-    useLayoutEffect,
-    useRef,
-    useState
-} from "react";
-import classNames from "classnames";
+import { PropsWithChildren, RefObject, useCallback, useLayoutEffect, useRef, useState } from "react";
 import { useOnScreen } from "@mendix/widget-plugin-hooks/useOnScreen";
 
 export interface InfiniteBodyProps {
-    className?: string;
     hasMoreItems: boolean;
     isInfinite: boolean;
-    role?: string;
     setPage?: (computePage: (prevPage: number) => number) => void;
-    style?: CSSProperties;
 }
 const offsetBottom = 30;
 
 export function useInfiniteControl(
     props: PropsWithChildren<InfiniteBodyProps>
-): [trackScrolling: (e: any) => void, bodySize: number, containerRef: RefObject<HTMLDivElement | null>] {
+): [
+    trackBodyScrolling: ((e: any) => void) | undefined,
+    bodyHeight: string | undefined,
+    gridWidth: string | undefined,
+    scrollBarSize: string | undefined,
+    gridBodyRef: RefObject<HTMLDivElement | null>,
+    gridContainerRef: RefObject<HTMLDivElement | null>,
+    gridHeaderRef: RefObject<HTMLDivElement | null>
+] {
     const { setPage, hasMoreItems, isInfinite } = props;
-    const [bodySize, setBodySize] = useState<number>(0);
-    const containerRef = useRef<HTMLDivElement>(null);
-    const isVisible = useOnScreen(containerRef as RefObject<HTMLElement>);
+    const [gridWidth, setGridWidth] = useState<string | undefined>();
+    const [bodyHeight, setBodyHeight] = useState<string | undefined>();
+    const [scrollBarSize, setScrollBarSize] = useState<string | undefined>();
+    const gridContainerRef = useRef<HTMLDivElement | null>(null);
+    const gridBodyRef = useRef<HTMLDivElement>(null);
+    const gridHeaderRef = useRef<HTMLDivElement | null>(null);
+    const isVisible = useOnScreen(gridBodyRef as RefObject<HTMLElement>);
 
-    const trackScrolling = useCallback(
+    const trackBodyScrolling = useCallback(
         (e: any) => {
+            const head = gridHeaderRef.current;
+            if (head) {
+                head.scrollTo({ left: e.target.scrollLeft });
+                head.dataset.scrolledY = e.target.scrollTop > 0 ? "true" : "false";
+                head.dataset.scrolledX = e.target.scrollLeft > 0 ? "true" : "false";
+            }
+
+            const scrollBarSize = e.target.offsetWidth - e.target.clientWidth;
+            setScrollBarSize(`${scrollBarSize}px`);
+
             /**
              * In Windows OS the result of first expression returns a non integer and result in never loading more, require floor to solve.
              * note: Math floor sometimes result in incorrect integer value,
@@ -48,31 +57,40 @@ export function useInfiniteControl(
         [hasMoreItems, setPage]
     );
 
-    const calculateBodyHeight = useCallback((): void => {
-        if (isVisible && isInfinite && hasMoreItems && bodySize <= 0 && containerRef.current) {
-            setBodySize(containerRef.current.clientHeight - offsetBottom);
+    const lockGridBodyHeight = useCallback((): void => {
+        if (isVisible && isInfinite && hasMoreItems && bodyHeight === undefined && gridBodyRef.current) {
+            setBodyHeight(`${gridBodyRef.current.clientHeight - offsetBottom}px`);
         }
-    }, [isInfinite, hasMoreItems, bodySize, isVisible]);
+    }, [isInfinite, hasMoreItems, bodyHeight, isVisible]);
 
     useLayoutEffect(() => {
-        setTimeout(() => calculateBodyHeight(), 100);
-    }, [calculateBodyHeight]);
+        setTimeout(() => lockGridBodyHeight(), 100);
+    }, [lockGridBodyHeight]);
 
-    return [trackScrolling, bodySize, containerRef];
-}
+    useLayoutEffect(() => {
+        const observeTarget = gridContainerRef.current;
+        if (!isInfinite || !observeTarget) return;
 
-export function InfiniteBody(props: PropsWithChildren<InfiniteBodyProps>): ReactElement {
-    const { className, isInfinite } = props;
-    const [trackScrolling, bodySize, containerRef] = useInfiniteControl(props);
-    return (
-        <div
-            ref={containerRef}
-            className={classNames(className, isInfinite ? "infinite-loading" : "")}
-            onScroll={isInfinite ? trackScrolling : undefined}
-            role={props.role}
-            style={isInfinite && bodySize > 0 ? { ...props.style, maxHeight: bodySize } : props.style}
-        >
-            {props.children}
-        </div>
-    );
+        const resizeObserver = new ResizeObserver(entries => {
+            for (const entry of entries) {
+                setGridWidth(entry.target.clientWidth ? `${entry.target.clientWidth}px` : undefined);
+            }
+        });
+
+        resizeObserver.observe(observeTarget);
+
+        return () => {
+            resizeObserver.unobserve(observeTarget);
+        };
+    }, [isInfinite]);
+
+    return [
+        isInfinite ? trackBodyScrolling : undefined,
+        bodyHeight,
+        gridWidth,
+        scrollBarSize,
+        gridBodyRef,
+        gridContainerRef,
+        gridHeaderRef
+    ];
 }
