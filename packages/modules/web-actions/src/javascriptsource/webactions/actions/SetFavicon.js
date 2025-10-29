3988d2333dcec44454f48b82e4856ce682560544
Samuel Reichert <samuelreichertt@gmail.com>
2025-10-29 16:01:16 +0100

    chore: add mx-global import

--- packages/modules/web-actions/src/javascriptsource/webactions/actions/SetFavicon.js
+++ packages/modules/web-actions/src/javascriptsource/webactions/actions/SetFavicon.js
@@ -5,6 +5,7 @@
 // - the code between BEGIN USER CODE and END USER CODE
 // - the code between BEGIN EXTRA CODE and END EXTRA CODE
 // Other code you write will be lost the next time you deploy the project.
+import "mx-global";
 import { Big } from "big.js";
 
 // BEGIN EXTRA CODE
