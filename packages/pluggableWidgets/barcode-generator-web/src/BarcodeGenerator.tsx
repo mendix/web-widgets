51ca766d660f839f482fdcb3dc05563e0b35ac9d
Yordan <yordan.stoyanov@mendix.com>
2025-11-11 15:00:03 +0100

    fix: improve property hiding logic

--- packages/pluggableWidgets/barcode-generator-web/src/BarcodeGenerator.tsx
+++ packages/pluggableWidgets/barcode-generator-web/src/BarcodeGenerator.tsx
@@ -1,109 +1,21 @@
-import { ReactElement, useRef } from "react";
+import { ReactElement } from "react";
 import { BarcodeGeneratorContainerProps } from "../typings/BarcodeGeneratorProps";
-import { useDownload } from "./hooks/useDownload";
-import { CodeRenderer } from "./components/CodeRenderer";
+import { barcodeConfig } from "./config/Barcode.config";
+import { BarcodeConfigProvider } from "./config/BarcodeConfigContext";
+import { BarcodeGeneratorInner } from "./components/BarcodeGeneratorInner";
 
 import "./ui/BarcodeGenerator.scss";
 
-export default function BarcodeGenerator({
-    codeValue,
-    codeWidth,
-    codeHeight,
-    codeFormat,
-    customCodeFormat,
-    codeMargin,
-    displayValue,
-    enableEan128,
-    enableFlat,
-    lastChar,
-    enableMod43,
-    addonValue,
-    addonFormat,
-    addonSpacing,
-    qrSize,
-    qrMargin,
-    qrTitle,
-    qrLevel,
-    qrImageSrc,
-    qrImageX,
-    qrImageY,
-    qrImageHeight,
-    qrImageWidth,
-    qrImageOpacity,
-    qrImageExcavate,
-    tabIndex,
-    allowDownload
-}: BarcodeGeneratorContainerProps): ReactElement {
-    const svgRef = useRef<SVGSVGElement>(null);
-    const qrContainerRef = useRef<HTMLDivElement>(null);
+export default function BarcodeGenerator(props: BarcodeGeneratorContainerProps): ReactElement {
+    const config = barcodeConfig(props);
 
-    const value = codeValue?.status === "available" ? codeValue.value : "";
-    const width = codeWidth ?? 128;
-    const height = codeHeight ?? 128;
-    const format = codeFormat === "Custom" ? (customCodeFormat ?? "CODE128") : (codeFormat ?? "CODE128");
-    const margin = codeMargin ?? 2;
-    const showValue = displayValue ?? false;
-    const download = allowDownload ?? false;
-    const qrsize = qrSize ?? 128;
-    const qrmargin = qrMargin ?? 2;
-    const qrtitle = qrTitle ?? "";
-    const qrlevel = qrLevel ?? "L";
-    const qrimageSrc = qrImageSrc?.status === "available" && qrImageSrc.value ? qrImageSrc.value.uri : "";
-    const qrimageX = qrImageX === 0 ? undefined : qrImageX;
-    const qrimageY = qrImageY === 0 ? undefined : qrImageY;
-    const qrimageHeight = qrImageHeight ?? 24;
-    const qrimageWidth = qrImageWidth ?? 24;
-    const qrimageOpacity = qrImageOpacity?.toNumber() ?? 1;
-    const qrimageExcavate = qrImageExcavate ?? true;
-    const supportsEan128 = enableEan128 ?? false;
-    const supportsFlat = enableFlat ?? false;
-    const lastCharacter = lastChar ?? "";
-    const supportsMod43 = enableMod43 ?? false;
-    const processedAddonValue = addonValue?.status === "available" ? addonValue.value : "";
-    const processedAddonFormat = addonFormat ?? "EAN5";
-    const processedAddonSpacing = addonSpacing ?? 20;
-
-    const { downloadSVG } = useDownload({ format, svgRef, qrContainerRef });
-
-    if (!value) {
+    if (!config.value) {
         return <span>No barcode value provided</span>;
     }
 
     return (
-        <div className="barcode-generator" tabIndex={tabIndex}>
-            <CodeRenderer
-                value={value}
-                format={format}
-                width={width}
-                height={height}
-                margin={margin}
-                displayValue={showValue}
-                enableEan128={supportsEan128}
-                enableFlat={supportsFlat}
-                lastChar={lastCharacter}
-                enableMod43={supportsMod43}
-                addonValue={processedAddonValue}
-                addonFormat={processedAddonFormat}
-                addonSpacing={processedAddonSpacing}
-                qrsize={qrsize}
-                qrmargin={qrmargin}
-                qrlevel={qrlevel}
-                qrtitle={qrtitle}
-                qrimageSrc={qrimageSrc}
-                qrimageX={qrimageX}
-                qrimageY={qrimageY}
-                qrimageWidth={qrimageWidth}
-                qrimageHeight={qrimageHeight}
-                qrimageOpacity={qrimageOpacity}
-                qrimageExcavate={qrimageExcavate}
-                svgRef={svgRef}
-                qrContainerRef={qrContainerRef}
-            />
-            {download && (
-                <button type="button" onClick={downloadSVG} className="btn btn-default">
-                    Download {format === "QRCode" ? "QR Code" : "Barcode"}
-                </button>
-            )}
-        </div>
+        <BarcodeConfigProvider config={config}>
+            <BarcodeGeneratorInner tabIndex={props.tabIndex} />
+        </BarcodeConfigProvider>
     );
 }
---
+++ packages/pluggableWidgets/barcode-generator-web/src/config/Barcode.config.ts
@@ -0,0 +1,75 @@
+import { BarcodeGeneratorContainerProps } from "../../typings/BarcodeGeneratorProps";
+
+/** Configuration for static values that don't change at runtime. */
+export interface BarcodeConfig {
+    // Basic barcode properties
+    value: string;
+    width: number;
+    height: number;
+    format: string;
+    margin: number;
+    displayValue: boolean;
+    allowDownload: boolean;
+
+    // Advanced barcode options
+    enableEan128: boolean;
+    enableFlat: boolean;
+    lastChar: string;
+    enableMod43: boolean;
+    addonValue: string;
+    addonFormat: string;
+    addonSpacing: number;
+
+    // QR Code properties
+    qrSize: number;
+    qrMargin: number;
+    qrTitle: string;
+    qrLevel: string;
+    qrImageSrc: string;
+    qrImageX: number | undefined;
+    qrImageY: number | undefined;
+    qrImageHeight: number;
+    qrImageWidth: number;
+    qrImageOpacity: number;
+    qrImageExcavate: boolean;
+}
+
+export function barcodeConfig(props: BarcodeGeneratorContainerProps): BarcodeConfig {
+    const value = props.codeValue?.status === "available" ? (props.codeValue.value ?? "") : "";
+    const format =
+        props.codeFormat === "Custom" ? (props.customCodeFormat ?? "CODE128") : (props.codeFormat ?? "CODE128");
+
+    return Object.freeze({
+        // Basic barcode properties
+        value,
+        width: props.codeWidth ?? 128,
+        height: props.codeHeight ?? 128,
+        format,
+        margin: props.codeMargin ?? 2,
+        displayValue: props.displayValue ?? false,
+        allowDownload: props.allowDownload ?? false,
+
+        // Advanced barcode options
+        enableEan128: props.enableEan128 ?? false,
+        enableFlat: props.enableFlat ?? false,
+        lastChar: props.lastChar ?? "",
+        enableMod43: props.enableMod43 ?? false,
+        addonValue: props.addonValue?.status === "available" ? (props.addonValue.value ?? "") : "",
+        addonFormat: props.addonFormat,
+        addonSpacing: props.addonSpacing ?? 20,
+
+        // QR Code properties
+        qrSize: props.qrSize ?? 128,
+        qrMargin: props.qrMargin ?? 2,
+        qrTitle: props.qrTitle ?? "",
+        qrLevel: props.qrLevel ?? "L",
+        qrImageSrc:
+            props.qrImageSrc?.status === "available" && props.qrImageSrc.value ? props.qrImageSrc.value.uri : "",
+        qrImageX: props.qrImageX === 0 ? undefined : props.qrImageX,
+        qrImageY: props.qrImageY === 0 ? undefined : props.qrImageY,
+        qrImageHeight: props.qrImageHeight ?? 24,
+        qrImageWidth: props.qrImageWidth ?? 24,
+        qrImageOpacity: props.qrImageOpacity?.toNumber() ?? 1,
+        qrImageExcavate: props.qrImageExcavate ?? true
+    });
+}
