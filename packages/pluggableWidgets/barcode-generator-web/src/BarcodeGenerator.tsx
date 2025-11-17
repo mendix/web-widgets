5d9e04d6f565ccdcbeb0b0dd8244221e720e5b78
Yordan <yordan.stoyanov@mendix.com>
2025-11-17 14:30:34 +0100

    fix: update @types/react version in package.json

--- packages/pluggableWidgets/barcode-generator-web/src/BarcodeGenerator.tsx
+++ packages/pluggableWidgets/barcode-generator-web/src/BarcodeGenerator.tsx
@@ -1,10 +1,21 @@
 import { ReactElement } from "react";
 import { BarcodeGeneratorContainerProps } from "../typings/BarcodeGeneratorProps";
 import { barcodeConfig } from "./config/Barcode.config";
-import { BarcodeConfigProvider } from "./config/BarcodeConfigContext";
-import { BarcodeGeneratorInner } from "./components/BarcodeGeneratorInner";
+import { BarcodeContextProvider, useBarcodeConfig } from "./config/BarcodeContext";
+import { QRCodeRenderer } from "./components/QRCode";
+import { BarcodeRenderer } from "./components/Barcode";
 
 import "./ui/BarcodeGenerator.scss";
+
+function BarcodeContainer({ tabIndex }: { tabIndex?: number }): ReactElement {
+    const config = useBarcodeConfig();
+
+    return (
+        <div className="barcode-generator" tabIndex={tabIndex}>
+            {config.isQRCode ? <QRCodeRenderer /> : <BarcodeRenderer />}
+        </div>
+    );
+}
 
 export default function BarcodeGenerator(props: BarcodeGeneratorContainerProps): ReactElement {
     const config = barcodeConfig(props);
@@ -14,8 +25,8 @@ export default function BarcodeGenerator(props: BarcodeGeneratorContainerProps):
     }
 
     return (
-        <BarcodeConfigProvider config={config}>
-            <BarcodeGeneratorInner tabIndex={props.tabIndex} />
-        </BarcodeConfigProvider>
+        <BarcodeContextProvider config={config}>
+            <BarcodeContainer tabIndex={props.tabIndex} />
+        </BarcodeContextProvider>
     );
 }
--- packages/pluggableWidgets/barcode-generator-web/src/hooks/useRenderBarcode.ts
+++ packages/pluggableWidgets/barcode-generator-web/src/hooks/useRenderBarcode.ts
@@ -1,115 +1,31 @@
-import JsBarcode from "jsbarcode";
-import { type ForwardedRef, useEffect } from "react";
+import { useBarcodeConfig } from "../config/BarcodeContext";
+import { RefObject, useEffect, useRef } from "react";
+import { type BarcodeRenderOptions, renderBarcode } from "../utils/barcodeRenderer-utils";
 
-interface BarcodeMethodOptions {
-    width?: number;
-    height?: number;
-    margin?: number;
-    displayValue?: boolean;
-}
+export const useRenderBarcode = (): RefObject<SVGSVGElement | null> => {
+    const ref = useRef<SVGSVGElement>(null);
 
-interface BarcodeInstance {
-    EAN13: (value: string, options: BarcodeMethodOptions) => BarcodeInstance;
-    EAN8: (value: string, options: BarcodeMethodOptions) => BarcodeInstance;
-    EAN5: (value: string, options: BarcodeMethodOptions) => BarcodeInstance;
-    EAN2: (value: string, options: BarcodeMethodOptions) => BarcodeInstance;
-    blank: (spacing: number) => BarcodeInstance;
-    render: () => void;
-    [key: string]: any;
-}
+    const {
+        value,
+        width,
+        height,
+        format,
+        margin,
+        displayValue,
+        addonValue,
+        enableEan128,
+        enableFlat,
+        lastChar,
+        enableMod43,
+        addonFormat,
+        addonSpacing
+    } = useBarcodeConfig();
 
-interface BarcodeOptions {
-    format: string;
-    width: number;
-    height: number;
-    margin: number;
-    displayValue: boolean;
-    ean128?: boolean;
-    flat?: boolean;
-    lastChar?: string;
-    mod43?: boolean;
-}
-
-const createBarcodeWithAddon = (
-    ref: ForwardedRef<SVGSVGElement>,
-    value: string,
-    mainFormat: string,
-    addonValue: string,
-    addonFormat: string,
-    options: BarcodeOptions,
-    addonSpacing: number
-): void => {
-    if (ref && typeof ref !== "function" && ref.current) {
-        const barcodeInstance = JsBarcode(ref.current) as BarcodeInstance;
-
-        // Generate main barcode dynamically
-        barcodeInstance[mainFormat](value, {
-            width: options.width,
-            height: options.height,
-            margin: options.margin,
-            displayValue: options.displayValue
-        });
-
-        // Add spacing
-        barcodeInstance.blank(addonSpacing);
-
-        // Add addon dynamically
-        barcodeInstance[addonFormat](addonValue, { width: 1 });
-
-        barcodeInstance.render();
-    }
-};
-
-const createStandardBarcode = (ref: ForwardedRef<SVGSVGElement>, value: string, options: BarcodeOptions): void => {
-    if (ref && typeof ref !== "function" && ref.current) {
-        JsBarcode(ref.current, value, options);
-    }
-};
-
-const renderBarcode = (
-    ref: ForwardedRef<SVGSVGElement>,
-    value: string,
-    format: string,
-    addonValue: string | undefined,
-    addonFormat: string | undefined,
-    addonSpacing: number,
-    options: BarcodeOptions
-): void => {
-    switch (addonFormat) {
-        case "EAN5":
-            createBarcodeWithAddon(ref, value, format, addonValue!, addonFormat, options, addonSpacing);
-            break;
-
-        case "EAN2":
-            createBarcodeWithAddon(ref, value, format, addonValue!, addonFormat, options, addonSpacing);
-            break;
-
-        default:
-            createStandardBarcode(ref, value, options);
-            break;
-    }
-};
-
-export const useRenderBarcode = (
-    ref: ForwardedRef<SVGSVGElement>,
-    value: string,
-    width: number,
-    height: number,
-    format: string,
-    margin: number,
-    displayValue: boolean,
-    enableEan128: boolean,
-    enableFlat: boolean,
-    lastChar: string,
-    enableMod43: boolean,
-    addonValue?: string,
-    addonFormat?: string,
-    addonSpacing?: number
-): void => {
     useEffect(() => {
         if (ref && typeof ref !== "function" && ref.current && value) {
             try {
-                const options: BarcodeOptions = {
+                const renderOptions: BarcodeRenderOptions = {
+                    value,
                     format,
                     width,
                     height,
@@ -118,28 +34,18 @@ export const useRenderBarcode = (
                     ean128: enableEan128,
                     flat: enableFlat,
                     lastChar,
-                    mod43: enableMod43
+                    mod43: enableMod43,
+                    addonValue,
+                    addonFormat,
+                    addonSpacing
                 };
 
-                renderBarcode(ref, value, format, addonValue, addonFormat, addonSpacing || 20, options);
+                renderBarcode(ref, renderOptions);
             } catch (error) {
                 console.error("Error generating barcode:", error);
             }
         }
-    }, [
-        value,
-        width,
-        height,
-        format,
-        margin,
-        displayValue,
-        enableEan128,
-        enableFlat,
-        enableMod43,
-        lastChar,
-        addonValue,
-        addonFormat,
-        addonSpacing,
-        ref
-    ]);
+    }, [value, addonValue]);
+
+    return ref;
 };
