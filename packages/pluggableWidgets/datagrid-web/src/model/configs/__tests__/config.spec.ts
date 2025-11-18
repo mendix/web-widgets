9c080c79b12206136627a480bb05e58c8e8cae4e
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2025-11-18 11:12:38 +0100

    test: add new tests

---
+++ packages/pluggableWidgets/datagrid-web/src/model/configs/__tests__/config.spec.ts
@@ -0,0 +1,84 @@
+import { mockContainerProps } from "../../../utils/test-utils";
+import { datagridConfig } from "../Datagrid.config";
+
+describe("datagridConfig", () => {
+    it("should generate config with default values", () => {
+        const props = mockContainerProps();
+        const config = datagridConfig(props);
+
+        expect(config).toMatchObject({
+            checkboxColumnEnabled: false,
+            columnsDraggable: true,
+            columnsFilterable: true,
+            columnsHidable: true,
+            columnsResizable: true,
+            columnsSortable: true,
+            enableSelectAll: false,
+            filtersChannelName: expect.stringMatching(/^datagrid2_1:Datagrid@\w+:events$/),
+            id: expect.stringMatching(/^datagrid2_1:Datagrid@\w+$/),
+            keepSelection: false,
+            loadingType: "spinner",
+            multiselectable: undefined,
+            name: "datagrid2_1",
+            pagingPosition: "bottom",
+            refreshIntervalMs: 0,
+            selectAllCheckboxEnabled: true,
+            selectionEnabled: false,
+            selectorColumnEnabled: true,
+            settingsStorageEnabled: false
+        });
+    });
+
+    it("should generate config with multi-selection enabled", () => {
+        const props = {
+            ...mockContainerProps(),
+            itemSelection: { type: "Multi" } as any,
+            itemSelectionMethod: "checkbox" as const,
+            enableSelectAll: true,
+            keepSelection: true,
+            refreshInterval: 5
+        };
+        const config = datagridConfig(props);
+
+        expect(config).toMatchObject({
+            checkboxColumnEnabled: true,
+            enableSelectAll: true,
+            keepSelection: true,
+            multiselectable: true,
+            refreshIntervalMs: 5000,
+            selectionEnabled: true
+        });
+    });
+
+    it("should enable settings storage with localStorage", () => {
+        const props = {
+            ...mockContainerProps(),
+            configurationStorageType: "localStorage" as const
+        };
+        const config = datagridConfig(props);
+
+        expect(config.settingsStorageEnabled).toBe(true);
+    });
+
+    it("should enable settings storage with attribute when configurationAttribute is provided", () => {
+        const props = {
+            ...mockContainerProps(),
+            configurationStorageType: "attribute" as const,
+            configurationAttribute: { type: "attribute" } as any
+        };
+        const config = datagridConfig(props);
+
+        expect(config.settingsStorageEnabled).toBe(true);
+    });
+
+    it("should disable settings storage with attribute when configurationAttribute is not provided", () => {
+        const props = {
+            ...mockContainerProps(),
+            configurationStorageType: "attribute" as const,
+            configurationAttribute: undefined
+        };
+        const config = datagridConfig(props);
+
+        expect(config.settingsStorageEnabled).toBe(false);
+    });
+});
