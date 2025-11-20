77b819cabb8bc683ad7c894d6445e39f85db3e51
Yordan <yordan.stoyanov@mendix.com>
2025-11-20 13:04:03 +0100

    refactor: update components to use new state management

--- packages/pluggableWidgets/datagrid-web/src/components/GridHeader.tsx
+++ packages/pluggableWidgets/datagrid-web/src/components/GridHeader.tsx
@@ -1,11 +1,10 @@
-import { ReactElement, useState } from "react";
+import { ReactElement } from "react";
 import { useColumnsStore, useDatagridConfig, useGridSizeStore } from "../model/hooks/injection-hooks";
-import { ColumnId } from "../typings/GridColumn";
 import { CheckboxColumnHeader } from "./CheckboxColumnHeader";
 import { ColumnProvider } from "./ColumnProvider";
 import { ColumnResizer } from "./ColumnResizer";
 import { ColumnSelector } from "./ColumnSelector";
-import { Header } from "./Header";
+import { ColumnContainer } from "./ColumnContainer";
 import { HeaderSkeletonLoader } from "./loader/HeaderSkeletonLoader";
 
 export function GridHeader(): ReactElement {
@@ -13,8 +12,6 @@ export function GridHeader(): ReactElement {
     const columnsStore = useColumnsStore();
     const gridSizeStore = useGridSizeStore();
     const columns = columnsStore.visibleColumns;
-    const [dragOver, setDragOver] = useState<[ColumnId, "before" | "after"] | undefined>(undefined);
-    const [isDragging, setIsDragging] = useState<[ColumnId | undefined, ColumnId, ColumnId | undefined] | undefined>();
 
     if (!columnsStore.loaded) {
         return <HeaderSkeletonLoader size={columns.length} />;
@@ -26,9 +23,7 @@ export function GridHeader(): ReactElement {
                 <CheckboxColumnHeader key="headers_column_select_all" />
                 {columns.map(column => (
                     <ColumnProvider column={column} key={`${column.columnId}`}>
-                        <Header
-                            dropTarget={dragOver}
-                            isDragging={isDragging}
+                        <ColumnContainer
                             resizer={
                                 <ColumnResizer
                                     onResizeStart={() => columnsStore.setIsResizing(true)}
@@ -36,8 +31,6 @@ export function GridHeader(): ReactElement {
                                     setColumnWidth={(width: number) => column.setSize(width)}
                                 />
                             }
-                            setDropTarget={setDragOver}
-                            setIsDragging={setIsDragging}
                         />
                     </ColumnProvider>
                 ))}
