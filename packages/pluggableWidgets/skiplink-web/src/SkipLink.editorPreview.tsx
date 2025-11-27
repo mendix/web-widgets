ded729aec8a5238af9a5741da5de67e08214f045
hedwig.doets <hedwig.doets@mendix.com>
2025-11-27 13:23:45 +0100

    fix: remove react import

--- packages/pluggableWidgets/skiplink-web/src/SkipLink.editorPreview.tsx
+++ packages/pluggableWidgets/skiplink-web/src/SkipLink.editorPreview.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from "react";
+import { ReactElement } from "react";
 import { SkipLinkPreviewProps } from "../typings/SkipLinkProps";
 
 export const preview = (props: SkipLinkPreviewProps): ReactElement => {
