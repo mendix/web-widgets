825d6fbf12e8346f1047bb882a8d75aa8aa0fa84
Samuel Reichert <samuelreichertt@gmail.com>
2025-12-01 17:30:39 +0100

    refactor(date-time-picker-web): remove hardcodded label from DateTimePicker and update styles

--- packages/pluggableWidgets/date-time-picker-web/src/components/DateTimePickerContainer.tsx
+++ packages/pluggableWidgets/date-time-picker-web/src/components/DateTimePickerContainer.tsx
@@ -1,4 +1,3 @@
-import classNames from "classnames";
 import ReactDatePicker from "react-datepicker";
 import { DateTimePickerContainerProps } from "typings/DateTimePickerProps";
 import { useController } from "../hooks/useController";
@@ -12,15 +11,12 @@ export function DateTimePickerContainer(props: DateTimePickerContainerProps) {
         onChange: props.onChange
     });
     const pickerProps = useSetupProps(props, controller);
-    const label = props.showLabel && props.label?.status === "available" ? props.label.value : null;
     const portalId = `datepicker_` + Math.random();
 
     // still have to add validation for max and min time and validation message
     console.info("unused props", {
         name: props.name,
-        editabilityCondition: props.editabilityCondition,
-        readOnlyStyle: props.readOnlyStyle,
-        visible: props.visible,
+        id: props.id,
         validationType: props.validationType,
         customValidation: props.customValidation,
         validationMessage: props.validationMessage
@@ -30,42 +26,22 @@ export function DateTimePickerContainer(props: DateTimePickerContainerProps) {
         props.validationMessage?.status === "available" && props.validationMessage.value.length > 0;
 
     return (
-        <div
-            className={classNames("widget-datetimepicker", props.class)}
-            data-focusindex={props.tabIndex}
-            style={props.style}
-        >
-            {label ? (
-                <div className="widget-datetimepicker-label-wrapper">
-                    <div id={portalId} />
+        <div className="widget-datetimepicker" data-focusindex={props.tabIndex}>
+            <ReactDatePicker {...pickerProps} ref={controller.pickerRef} />
 
-                    <label className="widget-datetimepicker-label" id={pickerProps.ariaLabelledBy}>
-                        {label}
-                    </label>
-                </div>
-            ) : (
-                <span className="sr-only" id={pickerProps.ariaLabelledBy}>
-                    Date picker
-                </span>
-            )}
-
-            <div className="widget-datetimepicker-wrapper">
-                <ReactDatePicker {...pickerProps} ref={controller.pickerRef} />
-
-                <button
-                    aria-controls={portalId}
-                    aria-expanded={controller.expanded}
-                    aria-haspopup
-                    aria-label="Show calendar"
-                    className="widget-datetimepicker-input-button"
-                    onKeyDown={controller.handleButtonKeyDown}
-                    onMouseDown={controller.handleButtonMouseDown}
-                    type="button"
-                >
-                    <span className="mx-icon-filled mx-icon-calendar" />
-                </button>
-                {hasValidationMessage && <div role="alert">{props.validationMessage?.value}</div>}
-            </div>
+            <button
+                aria-controls={portalId}
+                aria-expanded={controller.expanded}
+                aria-haspopup
+                aria-label="Show calendar"
+                className="widget-datetimepicker-input-button"
+                onKeyDown={controller.handleButtonKeyDown}
+                onMouseDown={controller.handleButtonMouseDown}
+                type="button"
+            >
+                <span className="mx-icon-filled mx-icon-calendar" />
+            </button>
+            {hasValidationMessage && <div role="alert">{props.validationMessage?.value}</div>}
         </div>
     );
 }
