f7fbcf37cca45deda9f2846e04fe170a9fe360da
gjulivan <grandjulivan@gmail.com>
2025-12-03 11:06:43 +0100

    fix: multiple clipboard instance exist

--- packages/pluggableWidgets/rich-text-web/src/components/EditorWrapper.tsx
+++ packages/pluggableWidgets/rich-text-web/src/components/EditorWrapper.tsx
@@ -120,7 +120,6 @@ function EditorWrapperInner(props: EditorWrapperProps): ReactElement {
     }, [quillRef.current, onChange?.isExecuting]);
 
     const onTextChange = useCallback(() => {
-        console.log("onTextChange called", quillRef?.current?.getContents());
         if (stringAttribute.value !== quillRef?.current?.getSemanticHTML()) {
             setAttributeValueDebounce(quillRef?.current?.getSemanticHTML());
         }
