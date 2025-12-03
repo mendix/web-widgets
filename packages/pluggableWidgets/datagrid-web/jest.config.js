8be7556b17df7c7878a56f67852010837f7f8d31
Samuel Reichert <samuelreichertt@gmail.com>
2025-12-03 09:17:26 +0100

    fix(datagrid-web): refactor jest.config.js to use base variable and add setuoFilesAfterEnv

--- packages/pluggableWidgets/datagrid-web/jest.config.js
+++ packages/pluggableWidgets/datagrid-web/jest.config.js
@@ -1,8 +1,11 @@
+const base = require("@mendix/pluggable-widgets-tools/test-config/jest.enzyme-free.config.js");
+
 module.exports = {
-    ...require("@mendix/pluggable-widgets-tools/test-config/jest.enzyme-free.config.js"),
+    ...base,
     /**
      * `nanoevents` package is ESM module and because ESM is not supported by Jest yet
      * we mark `nanoevents` as a module that should be transformed by ts-jest.
      */
-    transformIgnorePatterns: ["node_modules/(?!nanoevents)/"]
+    transformIgnorePatterns: ["node_modules/(?!nanoevents)/"],
+    setupFilesAfterEnv: [...base.setupFilesAfterEnv, "<rootDir>/jest.setup.ts"]
 };
