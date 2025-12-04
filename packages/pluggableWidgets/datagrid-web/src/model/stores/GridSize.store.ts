c9c2128d19485ddce027bcfde0a5f055f3ca4765
Roman Vyakhirev <roman.vyakhirev@mendix.com>
2025-12-04 15:46:21 +0100

    chore: reorganize code

--- packages/pluggableWidgets/datagrid-web/src/model/stores/GridSize.store.ts
+++ packages/pluggableWidgets/datagrid-web/src/model/stores/GridSize.store.ts
@@ -4,6 +4,8 @@ import { ComputedAtom } from "@mendix/widget-plugin-mobx-kit/src/interfaces/Comp
 import { SetPageAction } from "@mendix/widget-plugin-grid/src/pagination/pagination.model.ts";
 import { PaginationConfig } from "../../features/pagination/pagination.config";
 
+export const VIRTUAL_SCROLLING_OFFSET = 30;
+
 export class GridSizeStore {
     gridContainerRef = createRef<HTMLDivElement>();
     gridBodyRef = createRef<HTMLDivElement>();
@@ -39,7 +41,7 @@ export class GridSizeStore {
             setGridWidth: action,
 
             gridBodyHeight: observable,
-            setGridBodyHeight: action,
+            lockGridBodyHeight: action,
 
             columnSizes: observable,
             updateColumnSizes: action,
@@ -74,11 +76,19 @@ export class GridSizeStore {
         this.gridWidth = size;
     }
 
-    setGridBodyHeight(size: number): void {
-        this.gridBodyHeight = size;
-    }
-
     updateColumnSizes(sizes: number[]): void {
         this.columnSizes = sizes;
+    }
+
+    lockGridBodyHeight(): void {
+        if (!this.hasVirtualScrolling || !this.paging.hasMoreItems) {
+            return;
+        }
+        const gridBody = this.gridBodyRef.current;
+        if (!gridBody || this.gridBodyHeight !== undefined) {
+            return;
+        }
+
+        this.gridBodyHeight = gridBody.clientHeight - VIRTUAL_SCROLLING_OFFSET;
     }
 }
