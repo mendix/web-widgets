593f8e17867e0eb0cd54ab24971ca604106e3ff5
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2025-12-29 15:06:49 +0100

    feat: start pagination

---
+++ packages/pluggableWidgets/gallery-web/src/components/Pagination.tsx
@@ -0,0 +1,6 @@
+import { observer } from "mobx-react-lite";
+import { ReactElement } from "react";
+
+export const Pagination = observer(function Pagination(): ReactElement {
+    return <div className="widget-gallery-pagination">Pagination</div>;
+});
