906ba8ece1d335c94a80f2847def6dd981c38c86
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2025-12-30 12:05:57 +0100

    chore: update changelog & small fix in refresh status

--- packages/pluggableWidgets/datagrid-web/src/components/RefreshStatus.tsx
+++ packages/pluggableWidgets/datagrid-web/src/components/RefreshStatus.tsx
@@ -1,11 +1,12 @@
 import { RefreshIndicator } from "@mendix/widget-plugin-component-kit/RefreshIndicator";
+import { observer } from "mobx-react-lite";
 import { ReactNode } from "react";
 import { useLoaderViewModel } from "../model/hooks/injection-hooks";
 
-export const RefreshStatus = function RefreshStatus(): ReactNode {
+export const RefreshStatus = observer(function RefreshStatus(): ReactNode {
     const loaderVM = useLoaderViewModel();
 
     if (!loaderVM.showRefreshIndicator) return null;
 
     return loaderVM.isRefreshing ? <RefreshIndicator /> : null;
-};
+});
