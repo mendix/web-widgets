fba58f13e29fb9397b1f469980b73b9afd5aa276
gjulivan <grandjulivan@gmail.com>
2026-01-05 13:48:33 +0100

    fix: styling cleanup

--- packages/pluggableWidgets/range-slider-web/src/RangeSlider.tsx
+++ packages/pluggableWidgets/range-slider-web/src/RangeSlider.tsx
@@ -1,7 +1,5 @@
 import { ReactNode } from "react";
 import { RangeSliderContainerProps } from "../typings/RangeSliderProps";
-import "@rc-component/slider/assets/index.css";
-import "@rc-component/tooltip/assets/bootstrap.css";
 import "./ui/RangeSlider.scss";
 import { Container } from "./components/Container";
 
