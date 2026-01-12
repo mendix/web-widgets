94bf360ebf8c0fcf26c0a9481a2e8dd839b7bd7e
Roman Vyakhirev <roman.vyakhirev@mendix.com>
2026-01-12 14:26:50 +0100

    chore: bump babel

--- packages/pluggableWidgets/combobox-web/src/helpers/utils.ts
+++ packages/pluggableWidgets/combobox-web/src/helpers/utils.ts
@@ -40,10 +40,10 @@ export function CaptionContent(props: CaptionContentProps): ReactElement {
         onClick: onClick
             ? onClick
             : htmlFor
-              ? (e: MouseEvent) => {
-                    e.preventDefault();
-                }
-              : undefined
+            ? (e: MouseEvent) => {
+                  e.preventDefault();
+              }
+            : undefined
     });
 }
 
