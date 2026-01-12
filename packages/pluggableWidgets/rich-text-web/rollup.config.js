d6d42ad25404624601d9b5d53c32c865a73de524
Roman Vyakhirev <roman.vyakhirev@mendix.com>
2026-01-12 14:19:26 +0100

    chore: cleanup

--- packages/pluggableWidgets/rich-text-web/rollup.config.js
+++
@@ -1,9 +0,0 @@
-import preserveDirectives from "rollup-preserve-directives";
-
-export default args => {
-    const result = args.configDefaultConfig;
-    return result.map((config, _index) => {
-        config.plugins = [...config.plugins, preserveDirectives()];
-        return config;
-    });
-};
