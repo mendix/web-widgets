3932a3ce776e3ff48c66f9a1f43272bb919e8153
Yordan Stoyanov <yordan.stoyanov@mendix.com>
2026-02-11 14:25:27 +0100

    feat: enhance config and styling - hide ean128 for qrcode - change code value to expression - add button position and card view config - implement layout, styling and icon

---
+++ packages/pluggableWidgets/barcode-generator-web/src/components/icons/DownloadIcon.tsx
@@ -0,0 +1,21 @@
+import { ReactElement } from "react";
+
+export function DownloadIcon(): ReactElement {
+    return (
+        <>
+            <svg
+                width="12"
+                height="12"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+            >
+                <path
+                    d="M18.29 9.29L19.71 10.71L12 18.41L4.29004 10.71L5.71004 9.29L11 14.59V0H13V14.59L18.29 9.29ZM2 22H22V18H24V22C24 23.1 23.1 24 22 24H2C0.9 24 0 23.1 0 22V18H2V22Z"
+                    fill="currentColor"
+                />
+            </svg>
+        </>
+    );
+}
