63aa65a83f6181b8d0915a0fb06c8502d8aff63a
Diljohn Singh <diljohn.chauhan@gmail.com>
2026-02-20 15:24:23 +0100

    fix: adding multiselectable attribute

--- packages/pluggableWidgets/datagrid-web/src/components/ColumnSelector.tsx
+++ packages/pluggableWidgets/datagrid-web/src/components/ColumnSelector.tsx
@@ -126,6 +126,7 @@ export function ColumnSelector(props: ColumnSelectorProps): ReactElement {
             className={`column-selectors`}
             data-focusindex={0}
             role="menu"
+            aria-multiselectable="true"
             style={{ ...correctedFloatingStyles, maxHeight }}
             {...getFloatingProps()}
         >
