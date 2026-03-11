a911a7fc021db300565ad1956cd4b586b3dfe789
gjulivan <grandjulivan@gmail.com>
2026-03-11 15:00:56 +0100

    chore: update jest config after pwt update

--- packages/pluggableWidgets/tree-node-web/jest.config.js
+++ packages/pluggableWidgets/tree-node-web/jest.config.js
@@ -1,5 +1,5 @@
 const { join } = require("path");
-const base = require("@mendix/pluggable-widgets-tools/test-config/jest.enzyme-free.config.js");
+const base = require("@mendix/pluggable-widgets-tools/test-config/jest.config.js");
 
 module.exports = {
     ...base,
