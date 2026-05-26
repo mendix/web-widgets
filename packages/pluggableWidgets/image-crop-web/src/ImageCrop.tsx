050445e3583088588dc095c6754c0912af7f4ba1
Rahman <rahmanunver@gmail.com>
2026-05-26 09:45:10 +0200

    feat(image-crop-web): bundle ReactCrop CSS via SCSS import

--- packages/pluggableWidgets/image-crop-web/src/ImageCrop.tsx
+++ packages/pluggableWidgets/image-crop-web/src/ImageCrop.tsx
@@ -1,6 +1,7 @@
 import { ReactElement } from "react";
 import { ImageCropContainerProps } from "../typings/ImageCropProps";
 import { ImageCropContainer } from "./components/ImageCropContainer";
+import "./ui/ImageCrop.scss";
 
 export function ImageCrop(props: ImageCropContainerProps): ReactElement | null {
     return <ImageCropContainer {...props} />;
