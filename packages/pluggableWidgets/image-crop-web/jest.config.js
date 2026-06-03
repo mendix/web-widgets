ccb861862877dc0a3c4ccedbade107bcf8e860b8
Rahman <rahmanunver@gmail.com>
2026-06-03 17:21:14 +0200

    feat(image-cropper-web): auto-apply crop on change, remove Crop button

--- packages/pluggableWidgets/image-crop-web/jest.config.js
+++
@@ -1,6 +0,0 @@
-const base = require("@mendix/pluggable-widgets-tools/test-config/jest.config.js");
-
-module.exports = {
-    ...base,
-    setupFilesAfterEnv: [...(base.setupFilesAfterEnv ?? []), require("path").join(__dirname, "jest.setup.ts")]
-};
