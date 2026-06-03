c889d54b8bacd873a452dc9f3c0710d1b22e8da2
Rahman <rahmanunver@gmail.com>
2026-06-03 17:21:14 +0200

    feat(image-cropper-web): auto-apply crop on change, remove Crop button

--- packages/pluggableWidgets/image-crop-web/README.md
+++
@@ -1,5 +0,0 @@
-# Image Crop
-
-Crops images bound to a Mendix image attribute. The cropped result is written back to the same attribute.
-
-See the [Mendix Marketplace listing](https://marketplace.mendix.com/) for usage docs.
--- packages/pluggableWidgets/image-crop-web/src/ImageCrop.tsx
+++
@@ -1,8 +0,0 @@
-import { ReactElement } from "react";
-import { ImageCropContainerProps } from "../typings/ImageCropProps";
-import { ImageCropContainer } from "./components/ImageCropContainer";
-import "./ui/ImageCrop.scss";
-
-export function ImageCrop(props: ImageCropContainerProps): ReactElement | null {
-    return <ImageCropContainer {...props} />;
-}
