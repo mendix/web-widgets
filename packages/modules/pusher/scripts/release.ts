6bd1d8bb951d9432eaaf5afb49516d78d3b764e1
Roman Vyakhirev <roman.vyakhirev@mendix.com>
2026-06-12 17:02:30 +0200

    chore: remove legacy widget as it is committed to the test project

--- packages/modules/pusher/scripts/release.ts
+++ packages/modules/pusher/scripts/release.ts
@@ -1,20 +1,16 @@
 #!/usr/bin/env ts-node-script
 
-import { join } from "path";
 import {
     addWidgetsToMpk,
     cloneTestProject,
     copyModuleLicense,
     copyWidgetsToProject,
     createModuleMpk,
-    logStep,
-    ModuleStepParams,
     moveModuleToDist,
     removeDist,
     runModuleSteps,
     writeModuleVersion
 } from "@mendix/automation-utils/steps";
-import { cp } from "@mendix/automation-utils/shell";
 
 async function main(): Promise<void> {
     await runModuleSteps({
@@ -25,20 +21,11 @@ async function main(): Promise<void> {
             writeModuleVersion,
             copyModuleLicense,
             copyWidgetsToProject,
-            copyLegacyPusherWidget,
             createModuleMpk,
             addWidgetsToMpk,
             moveModuleToDist
         ]
     });
-}
-
-async function copyLegacyPusherWidget({ config }: ModuleStepParams) {
-    logStep("Copying legacy Pusher widget to the module");
-    cp(
-        join(config.paths.package, "assets", "Pusher_widget_legacy_1.2.0.mpk"),
-        join(config.output.dirs.widgets, "Pusher.mpk")
-    );
 }
 
 main().catch(err => {
