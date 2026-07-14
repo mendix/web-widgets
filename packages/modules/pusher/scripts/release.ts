e13be5d7201b3f195b91b697acfc2fcfb329a694
Roman Vyakhirev <roman.vyakhirev@mendix.com>
2026-07-14 11:47:40 +0200

    chore: add copying old widget step to Pusher

--- packages/modules/pusher/scripts/release.ts
+++ packages/modules/pusher/scripts/release.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env ts-node-script
 
 import {
+    addTestProjectWidgetsToMpk,
     addWidgetsToMpk,
     cloneTestProject,
     copyModuleLicense,
@@ -23,6 +24,7 @@ async function main(): Promise<void> {
             copyWidgetsToProject,
             createModuleMpk,
             addWidgetsToMpk,
+            addTestProjectWidgetsToMpk(["Pusher.mpk"]),
             moveModuleToDist
         ]
     });
