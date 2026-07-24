aa93cdf8a831c9a4241f6ee5b0f6f748d801e541
hedwig.doets <hedwig.doets@mendix.com>
2026-07-24 10:53:30 +0200

    fix: prettier fix

--- packages/pluggableWidgets/combobox-web/src/components/ComboboxMenuWrapper.tsx
+++ packages/pluggableWidgets/combobox-web/src/components/ComboboxMenuWrapper.tsx
@@ -91,7 +91,8 @@ export function ComboboxMenuWrapper(props: ComboboxMenuWrapperProps): ReactEleme
                 aria-hidden={!isOpen || undefined}
                 {...menuProps}
             >
-                {isOpen && (isEmpty && !isLoading ? <NoOptionsPlaceholder>{noOptionsText}</NoOptionsPlaceholder> : children)}
+                {isOpen &&
+                    (isEmpty && !isLoading ? <NoOptionsPlaceholder>{noOptionsText}</NoOptionsPlaceholder> : children)}
                 {isOpen && loader}
             </ul>
             {isOpen && menuFooterContent && (
